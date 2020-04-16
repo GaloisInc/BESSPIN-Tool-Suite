852b33d87cf8732290a29d6c159aebc5684e913a
rtadros <rtadros@galois.com>
2020-04-16 16:41:38 -0700

    more work towards building apps and fetching os Images

--- fett/target/build.py
+++ fett/target/build.py
@@ -1,11 +1,31 @@
 #! /usr/bin/env python3
 """ 
-Building OS images, apps, any other needed files
+Building OS images any other needed files
 """
 
 from fett.base.utils.misc import *
 
 @decorate.debugWrap
-@decorate.timeWrap
 def prepareOsImage ():
+    if(isEqSetting('osImage','FreeRTOS')):
+        prepareFreeRTOS ()
+    elif(isEqSetting('osImage','debian')):
+        prepareDebian ()
+    elif(isEqSetting('osImage','FreeBSD')):
+        prepareFreeBSD ()
+
+@decorate.debugWrap
+@decorate.timeWrap
+def prepareFreeRTOS():
+    pass
+
+@decorate.debugWrap
+@decorate.timeWrap
+def prepareDebian():
+    # copy the crngOnDebian.riscv
+    pass
+
+@decorate.debugWrap
+@decorate.timeWrap
+def prepareFreeBSD():
     pass
