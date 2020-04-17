753d80bc21aa3d9190e3b5cd47886d3340314d01
rtadros <rtadros@galois.com>
2020-04-17 14:03:06 -0700

    done with emulateDir.sh -- preparing for emulateTests.py

---
+++ fett/target/fpga.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+""" 
+Main fpga class + misc fpga functions
+"""
+
+from fett.base.utils.misc import *
+from fett.target.common import *
+
+@decorate.debugWrap
+@decorate.timeWrap
+def programBifile ():
+    logAndExit (f"<fpga.programBifile> is not yet implemented.",exitCode=EXIT.Implementation)
+
+@decorate.debugWrap
+def resetEthAdaptor ():
+    logAndExit (f"<fpga.resetEthAdaptor> is not yet implemented.",exitCode=EXIT.Implementation)
---
+++ fett/target/qemu.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+""" 
+Main qemu class + misc qemu functions
+"""
+
+from fett.base.utils.misc import *
+from fett.target.common import *
