1718f439ad8a9ee25be5e79a65fce13137411ff7
rtadros <rtadros@galois.com>
2020-04-17 14:53:49 -0700

    finishing the classesHierarchy

--- fett/target/fpga.py
+++ fett/target/fpga.py
@@ -6,6 +6,16 @@ Main fpga class + misc fpga functions
 from fett.base.utils.misc import *
 from fett.target.common import *
 
+class fpgaTarget (commonTarget):
+    def __init__ (self):
+
+        # For FreeBSD
+        self.uartAttempts = 0
+        self.limitUartAttempts = 8 if(isEqSetting('procFlavor','bluespec')) else 4
+
+        super().__init__()
+        return
+
 @decorate.debugWrap
 @decorate.timeWrap
 def programBifile ():
--- fett/target/qemu.py
+++ fett/target/qemu.py
@@ -5,3 +5,9 @@ Main qemu class + misc qemu functions
 
 from fett.base.utils.misc import *
 from fett.target.common import *
+
+class qemuTarget (commonTarget):
+    def __init__ (self):
+
+        super().__init__()
+        return
