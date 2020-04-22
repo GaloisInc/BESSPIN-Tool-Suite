a19cf3e60199a0c4c75006bb8e51689f68eb8124
rtadros <rtadros@galois.com>
2020-04-22 08:00:17 -0700

    adding interrupt signals handler

--- fett/base/utils/misc.py
+++ fett/base/utils/misc.py
@@ -24,6 +24,7 @@ class EXIT (enum.Enum):
     External = enum.auto()
     Run = enum.auto()
     Network = enum.auto()
+    Interrupted = enum.auto()
 
     def __str__ (self): #to replace '_' by ' ' when printing
         return f"{self.name.replace('_',' ')}"
@@ -33,6 +34,9 @@ def exitFett (exitCode):
         exitCode = EXIT.Unspecified
     printAndLog(f"End of FETT! [Exit code {exitCode.value}:{exitCode}]")
     exit(exitCode.value)
+
+def exitOnInterrupt (xSig,xFrame):
+    exitFett(EXIT.Interrupted)
 
 def formatExc (exc):
     """ format the exception for printing """
