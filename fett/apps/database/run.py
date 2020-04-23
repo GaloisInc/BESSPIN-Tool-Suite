98add162f4832a77cc54a97a402ad4a2392348ee
Alexander Bakst <abakst@galois.com>
2020-04-23 15:28:58 -0700

    Fix comment formatting

--- fett/apps/database/run.py
+++ fett/apps/database/run.py
@@ -11,7 +11,7 @@ def runApp (target):
     # target is a fett target object
     outLog = ''
     # All we need to do is install sqlite into a suitable location,
+    # like /usr/bin
     outLog += target.runCommand("echo \"Installing sqlite into /usr/bin...\"")[1]
     outLog += target.runCommand("install sqlite /usr/bin")[1]
-    # like /usr/bin
     return outLog
