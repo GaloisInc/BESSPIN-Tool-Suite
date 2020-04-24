e91196ff699db1c81f2ff8447d5cbfe8a2c84358
Isaiah Mindich <isaiah@galois.com>
2020-04-24 15:06:50 -0700

    Runner for nginx

--- fett/apps/webserver/run.py
+++ fett/apps/webserver/run.py
@@ -10,5 +10,22 @@ from fett.base.utils.misc import *
 def runApp (target):
     # target is a fett target object
     outLog = ''
-    outLog += target.runCommand("echo \"Executing app...\"")
+    outLog += target.runCommand("echo \"Installing nginx...\"")[1]
+    outLog += target.runCommand("mkdir -p /usr/local/sbin && install nginx /usr/local/sbin/nginx")[1]
+    outLog += target.runCommand("mkdir -p /usr/local/nginx/logs")[1]
+    outLog += target.runCommand("cp -r conf /usr/local/nginx/conf")[1]
+    outLog += target.runCommand("cp -r html /usr/local/nginx/html")[1]
+    outLog += target.runCommand("echo \"Starting nginx service...\"")[1]
+    if getSetting('osImage') == 'debian':
+        outLog += target.runCommand("install nginx.service /lib/systemd/system/nginx.service")[1]
+        outLog += target.runCommand("systemctl enable nginx.service")[1]
+        outLog += target.runCommand("systemctl start nginx.service")[1]
+    elif getSetting('osImage') == 'FreeBSD':
+        outLog += target.runCommand("mkdir -p /usr/local/etc/rc.d")[1]
+        outLog += target.runCommand("install rcfile /usr/local/etc/rc.d/nginx")[1]
+        outLog += target.runCommand("service nginx enable")[1]
+        outLog += target.runCommand("service nginx start")[1]
+    else:
+        logAndExit (f"Can't start nginx service on <{getSetting('osImage')}>",
+                     exitCode=EXIT.Configurations)
     return outLog
