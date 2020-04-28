65893528d83f4851a2ede8cfb426f4991871c26f
Ramy Tadros <rtadros@galois.com>
2020-04-28 12:54:46 -0700

    adding initial .mk env files

---
+++ fett/target/srcFreeRTOS/envFett.mk
@@ -0,0 +1,12 @@
+# This file has the necessary includes to run Fett on FreeRTOS
+
+CFLAGS += -DmainDEMO_TYPE=12 
+CFLAGS += -DtestgenOnFreeRTOS -DtestgenFPGA
+DEMO_SRC = main.c \
+	$(INC_TESTGEN)/*.c \
+	$(wildcard $(INC_TESTGEN)/lib/*.c)
+INCLUDES += -I$(INC_TESTGEN)/lib
+INCLUDES += -I$(INC_TESTGEN)
+CFLAGS := $(filter-out -Werror,$(CFLAGS))
+
+include $(INC_TESTGEN)/appEnv.mk
