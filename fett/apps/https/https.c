56f69ed68dbd8d689a63fb86cf4c38e20dffab8b
Ramy Tadros <rtadros@galois.com>
2020-05-01 07:47:25 -0700

    starting the network from the apps tasks

--- fett/apps/https/https.c
+++ fett/apps/https/https.c
@@ -6,9 +6,19 @@ void vHttps (void *pvParameters);
 
 void vHttps (void *pvParameters) {
     (void) pvParameters;
+    BaseType_t funcReturn;
+    uint32_t recvNotification;
     xMainTask = xTaskGetCurrentTaskHandle();
 
     fettPrintf("vHTTPS: Starting HTTPS...\r\n");
-    
+
+    funcReturn = xTaskCreate(vStartNetwork, "vHttps:startNetwork", configMINIMAL_STACK_SIZE * STACKSIZEMUL, NULL, xMainPriority, NULL);
+    vERROR_IF_NEQ(funcReturn, pdPASS, "vHttps: Creating vStartNetwork task.");
+
+    recvNotification = NOTIFY_FAIL;
+    funcReturn = xTaskNotifyWait(0xffffffff, 0, &recvNotification, pdMS_TO_TICKS(20000)); //it usually takes 10-15 seconds
+    vERROR_IF_NEQ(funcReturn, pdPASS, "vHttps: Receive notification from vStartNetwork.");
+    vERROR_IF_NEQ(recvNotification, NOTIFY_SUCCESS, "vHttps: Expected notification value from vStartNetwork.");
+
     vEXIT(0);
 }
--- fett/apps/ota/ota.c
+++ fett/apps/ota/ota.c
@@ -6,9 +6,19 @@ void vOta (void *pvParameters);
 
 void vOta (void *pvParameters) {
     (void) pvParameters;
+    BaseType_t funcReturn;
+    uint32_t recvNotification;
     xMainTask = xTaskGetCurrentTaskHandle();
 
     fettPrintf ("vOTA: Starting OTA...\r\n");
+
+    funcReturn = xTaskCreate(vStartNetwork, "vOta:startNetwork", configMINIMAL_STACK_SIZE * STACKSIZEMUL, NULL, xMainPriority, NULL);
+    vERROR_IF_NEQ(funcReturn, pdPASS, "vOta: Creating vStartNetwork task.");
+
+    recvNotification = NOTIFY_FAIL;
+    funcReturn = xTaskNotifyWait(0xffffffff, 0, &recvNotification, pdMS_TO_TICKS(20000)); //it usually takes 10-15 seconds
+    vERROR_IF_NEQ(funcReturn, pdPASS, "vOta: Receive notification from vStartNetwork.");
+    vERROR_IF_NEQ(recvNotification, NOTIFY_SUCCESS, "vOta: Expected notification value from vStartNetwork.");
 
     vEXIT(0);
 }
