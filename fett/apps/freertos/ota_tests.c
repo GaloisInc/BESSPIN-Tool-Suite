2915aa0d3adddd127a57597c4d5b81c019762d1a
Rod Chapman <rod@proteancode.com>
2020-05-03 12:41:05 +0100

    Add test cases for bad sig, bad message, and both sig and message bad.

--- fett/apps/freertos/ota_tests.c
+++ fett/apps/freertos/ota_tests.c
@@ -9,8 +9,9 @@ static const byte raw_pk[ED25519_KEY_SIZE] =
    0xE0, 0x16, 0xE3, 0x0F, 0x27, 0x0B, 0xAE, 0x74,
    0xB3, 0x0B, 0xED, 0xAC, 0x33, 0x47, 0x01, 0xFA};
 
-#define GOOD_SIG_SIZE 70
-static const byte good_signature[GOOD_SIG_SIZE] = {
+#define PAYLOAD_SIZE 70
+// Signature and message match
+static const byte good_one[PAYLOAD_SIZE] = {
   0xac, 0xa3, 0x99, 0x24, 0x9d, 0xe4, 0x94, 0x20, 0x0f, 0x11, 0xc4, 0xf2,
   0x52, 0x3e, 0xef, 0x90, 0xc3, 0x9e, 0x1f, 0xdb, 0xe3, 0xe5, 0x32, 0x33,
   0x67, 0x23, 0x00, 0xa2, 0xd3, 0x1d, 0xbf, 0x41, 0x4c, 0xcb, 0x2e, 0xb3,
@@ -18,7 +19,36 @@ static const byte good_signature[GOOD_SIG_SIZE] = {
   0x74, 0x07, 0x12, 0xc2, 0xf6, 0x64, 0x5b, 0x34, 0x5b, 0xe9, 0x85, 0x8b,
   0x32, 0xcb, 0x0f, 0x01, 0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x0a
 };
-unsigned int good_txt_sig_len = 70;
+
+// first byte of signature corrupt, message OK
+static const byte bad_sig[PAYLOAD_SIZE] = {
+  0xFF, 0xa3, 0x99, 0x24, 0x9d, 0xe4, 0x94, 0x20, 0x0f, 0x11, 0xc4, 0xf2,
+  0x52, 0x3e, 0xef, 0x90, 0xc3, 0x9e, 0x1f, 0xdb, 0xe3, 0xe5, 0x32, 0x33,
+  0x67, 0x23, 0x00, 0xa2, 0xd3, 0x1d, 0xbf, 0x41, 0x4c, 0xcb, 0x2e, 0xb3,
+  0x2b, 0xf6, 0x28, 0xef, 0x01, 0x48, 0xe4, 0x81, 0xca, 0x3b, 0xb0, 0xde,
+  0x74, 0x07, 0x12, 0xc2, 0xf6, 0x64, 0x5b, 0x34, 0x5b, 0xe9, 0x85, 0x8b,
+  0x32, 0xcb, 0x0f, 0x01, 0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x0a
+};
+
+// signature OK, first byte of message corrupt
+static const byte bad_msg[PAYLOAD_SIZE] = {
+  0xac, 0xa3, 0x99, 0x24, 0x9d, 0xe4, 0x94, 0x20, 0x0f, 0x11, 0xc4, 0xf2,
+  0x52, 0x3e, 0xef, 0x90, 0xc3, 0x9e, 0x1f, 0xdb, 0xe3, 0xe5, 0x32, 0x33,
+  0x67, 0x23, 0x00, 0xa2, 0xd3, 0x1d, 0xbf, 0x41, 0x4c, 0xcb, 0x2e, 0xb3,
+  0x2b, 0xf6, 0x28, 0xef, 0x01, 0x48, 0xe4, 0x81, 0xca, 0x3b, 0xb0, 0xde,
+  0x74, 0x07, 0x12, 0xc2, 0xf6, 0x64, 0x5b, 0x34, 0x5b, 0xe9, 0x85, 0x8b,
+  0x32, 0xcb, 0x0f, 0x01, 0xFF, 0x65, 0x6c, 0x6c, 0x6f, 0x0a
+};
+
+// first byte of signature corrupt, first byte of message corrupt
+static const byte bad_both[PAYLOAD_SIZE] = {
+  0xFF, 0xa3, 0x99, 0x24, 0x9d, 0xe4, 0x94, 0x20, 0x0f, 0x11, 0xc4, 0xf2,
+  0x52, 0x3e, 0xef, 0x90, 0xc3, 0x9e, 0x1f, 0xdb, 0xe3, 0xe5, 0x32, 0x33,
+  0x67, 0x23, 0x00, 0xa2, 0xd3, 0x1d, 0xbf, 0x41, 0x4c, 0xcb, 0x2e, 0xb3,
+  0x2b, 0xf6, 0x28, 0xef, 0x01, 0x48, 0xe4, 0x81, 0xca, 0x3b, 0xb0, 0xde,
+  0x74, 0x07, 0x12, 0xc2, 0xf6, 0x64, 0x5b, 0x34, 0x5b, 0xe9, 0x85, 0x8b,
+  0x32, 0xcb, 0x0f, 0x01, 0xFF, 0x65, 0x6c, 0x6c, 0x6f, 0x0a
+};
 
 void test_ed25519_verify(void)
 {
@@ -41,25 +71,81 @@ void test_ed25519_verify(void)
     }
 
   // TEST CASE 1 - GOOD SIGNATURE
-
   fettPrintf ("Test case 1 - expected good signature\n");
 
-  // sm points at the signature (64 bytes) followed directly by the message
-  r = wc_ed25519_verify_msg((byte *) good_signature,  // ptr to first byte of signature
-                            ED25519_SIG_SIZE,         // size of signature
+  // good_one points at the signature (64 bytes) followed directly by the message
+  r = wc_ed25519_verify_msg((byte *) good_one,  // ptr to first byte of signature
+                            ED25519_SIG_SIZE,   // size of signature
 
-                            good_signature + ED25519_SIG_SIZE, // ptr to first byte of message
-                            GOOD_SIG_SIZE - ED25519_SIG_SIZE,  // size of message
+                            good_one + ED25519_SIG_SIZE,     // ptr to first byte of message
+                            PAYLOAD_SIZE - ED25519_SIG_SIZE, // size of message
 
                             &signature_ok,            // Returned status
                             &pk);                     // public key
-  if (r == 0 && signature_ok == 1)
+  if (signature_ok == 1)
     {
-      fettPrintf ("Signature is OK\n");
+      fettPrintf ("Signature is OK - PASS\n");
     }
   else
     {
-      fettPrintf ("Signature is NOT OK\n");
+      fettPrintf ("Signature is NOT OK - FAIL\n");
+    }
+
+  fettPrintf ("Test case 2 - corrupt signature\n");
+
+  r = wc_ed25519_verify_msg((byte *) bad_sig,  // ptr to first byte of signature
+                            ED25519_SIG_SIZE,  // size of signature
+
+                            bad_sig + ED25519_SIG_SIZE,     // ptr to first byte of message
+                            PAYLOAD_SIZE - ED25519_SIG_SIZE, // size of message
+
+                            &signature_ok,            // Returned status
+                            &pk);                     // public key
+  if (signature_ok == 1)
+    {
+      fettPrintf ("Signature is OK - FAIL\n");
+    }
+  else
+    {
+      fettPrintf ("Signature is NOT OK - PASS\n");
+    }
+
+  fettPrintf ("Test case 3 - corrupt message\n");
+
+  r = wc_ed25519_verify_msg((byte *) bad_msg,  // ptr to first byte of signature
+                            ED25519_SIG_SIZE,  // size of signature
+
+                            bad_msg + ED25519_SIG_SIZE,     // ptr to first byte of message
+                            PAYLOAD_SIZE - ED25519_SIG_SIZE, // size of message
+
+                            &signature_ok,            // Returned status
+                            &pk);                     // public key
+  if (signature_ok == 1)
+    {
+      fettPrintf ("Signature is OK - FAIL\n");
+    }
+  else
+    {
+      fettPrintf ("Signature is NOT OK - PASS\n");
+    }
+
+  fettPrintf ("Test case 4 - corrupt message and signature\n");
+
+  r = wc_ed25519_verify_msg((byte *) bad_both,  // ptr to first byte of signature
+                            ED25519_SIG_SIZE,  // size of signature
+
+                            bad_both + ED25519_SIG_SIZE,     // ptr to first byte of message
+                            PAYLOAD_SIZE - ED25519_SIG_SIZE, // size of message
+
+                            &signature_ok,            // Returned status
+                            &pk);                     // public key
+  if (signature_ok == 1)
+    {
+      fettPrintf ("Signature is OK - FAIL\n");
+    }
+  else
+    {
+      fettPrintf ("Signature is NOT OK - PASS\n");
     }
 
   wc_ed25519_free (&pk);
