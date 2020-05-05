5816aa87c2c8fb3b883794b4167e81d6f7441c55
Rod Chapman <rod@proteancode.com>
2020-05-05 16:47:28 +0100

    Create HTTP server at the top-level of this task.

--- fett/apps/freertos/http.c
+++ fett/apps/freertos/http.c
@@ -1,8 +1,37 @@
 // This is the main task executed by http
 
 #include "fettFreeRTOS.h"
+#include "tcp_server.h"
 
 void vHttp (void *pvParameters);
+void Http_Worker (void);
+
+#define configHTTP_ROOT "/"
+
+static const struct xSERVER_CONFIG xServerConfiguration[] =
+  {
+   /* Server type, port number, backlog, root dir. */
+   { eSERVER_HTTP, 80,          1,       configHTTP_ROOT },
+  };
+
+void Http_Worker (void)
+{
+    TCPServer_t *pxTCPServer = NULL;
+    const TickType_t xInitialBlockTime = pdMS_TO_TICKS( 200UL );
+
+    /* Create the servers defined by the xServerConfiguration array above. */
+    pxTCPServer = FreeRTOS_CreateTCPServer
+      (xServerConfiguration, sizeof( xServerConfiguration ) / sizeof( xServerConfiguration[ 0 ] ) );
+
+    vERROR_IF_EQ(pxTCPServer, NULL, "vHttp: Failed to create TCP Server.");
+
+    for( ;; )
+      {
+        FreeRTOS_TCPServerWork( pxTCPServer, xInitialBlockTime );
+      }
+}
+
+
 
 void vHttp (void *pvParameters) {
     (void) pvParameters;
@@ -10,7 +39,7 @@ void vHttp (void *pvParameters) {
 
     fettPrintf("(Info)~  vHttp: Starting HTTP...\r\n");
 
-    /* Code goes here */
+    Http_Worker();
 
     fettPrintf("(Info)~  vHttp: Exitting HTTP...\r\n");
 
