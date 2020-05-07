41bf34757785b5952057ff04293d57c2b1636eca
Dragan Stosic <dragan.stosic@gmail.com>
2020-05-07 11:27:34 +0200

    removed check of the os-systems

--- fett/apps/unix/database.py
+++ fett/apps/unix/database.py
@@ -35,91 +35,84 @@ def deploymentTest(target):
     xDb = 'test.db'
     target.switchUser()
 
-    def create_database_and_table(pathToFile='/usr/bin', xFile='sqlite', xTable='food'):
+    def create_database_and_table(xTable='food'):
         printAndLog(f"Test[create_database_and_table]: Create sqlite {xDb} database and {xTable} table", doPrint=False)
-        if getSetting('osImage') in ['debian', 'FreeBSD']:
-            target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
-                              endsWith="sqlite>")
-            target.runCommand(f"CREATE VIRTUAL TABLE IF NOT EXISTS {xTable} USING fts3(title);",
-                              endsWith="sqlite>")
-            target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>")
-            target.runCommand(".exit")
-            printAndLog(f"Test[create_database_and_table]: The {xDb} database and {xTable} table created successfully!",
-                        doPrint=False)
+        target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
+                          endsWith="sqlite>")
+        target.runCommand(f"CREATE VIRTUAL TABLE IF NOT EXISTS {xTable} USING fts3(title);",
+                          endsWith="sqlite>")
+        target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>")
+        target.runCommand(".exit")
+        printAndLog(f"Test[create_database_and_table]: The {xDb} database and {xTable} table created successfully!",
+                    doPrint=False)
 
     def insert_record(xTable='food', title_val='Pancakes'):
         printAndLog(f"Test[insert_record]: Insert into  {xTable} table value {title_val}.", doPrint=False)
-        if getSetting('osImage') in ['debian', 'FreeBSD']:
-            target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
-                              endsWith="sqlite>")
-            target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>")
-            target.runCommand(f"INSERT INTO {xTable}(title) VALUES('{title_val}');",
-                              endsWith="sqlite>")
-            target.runCommand(f"SELECT * from {xTable};", expectedContents=[f"{title_val}"], expectExact=True,
-                              endsWith="sqlite>")
-            target.runCommand(".exit")
-            printAndLog(
-                f"Test[insert_record]: The value {title_val} has been successfully inserted into {xTable} table!",
-                doPrint=False)
+        target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
+                          endsWith="sqlite>")
+        target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>")
+        target.runCommand(f"INSERT INTO {xTable}(title) VALUES('{title_val}');",
+                          endsWith="sqlite>")
+        target.runCommand(f"SELECT * from {xTable};", expectedContents=[f"{title_val}"], expectExact=True,
+                          endsWith="sqlite>")
+        target.runCommand(".exit")
+        printAndLog(
+            f"Test[insert_record]: The value {title_val} has been successfully inserted into {xTable} table!",
+            doPrint=False)
 
     def update_record(xTable='food', title_val='Pizza'):
         printAndLog(f"Test[update_record]: Update the first record in the table {xTable}  - value {title_val}.",
                     doPrint=False)
-        if getSetting('osImage') in ['debian', 'FreeBSD']:
-            target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
-                              endsWith="sqlite>")
-            target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>")
-            target.runCommand(f"SELECT * from {xTable};", expectExact='Pancakes', endsWith="sqlite>")
+        target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
+                          endsWith="sqlite>")
+        target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>")
+        target.runCommand(f"SELECT * from {xTable};", expectExact='Pancakes', endsWith="sqlite>")
 
-            target.runCommand(f"Update {xTable} SET title='{title_val}' WHERE title ='Pancakes';",
-                              endsWith="sqlite>")
-            target.runCommand(f"SELECT * from {xTable};", expectExact=True, expectedContents=[f"{title_val}"],
-                              endsWith="sqlite>")
-            target.runCommand(".exit")
-            printAndLog(f"Test[update_record]: The first record has been successfully updated - value {title_val}.",
-                        doPrint=False)
+        target.runCommand(f"Update {xTable} SET title='{title_val}' WHERE title ='Pancakes';",
+                          endsWith="sqlite>")
+        target.runCommand(f"SELECT * from {xTable};", expectExact=True, expectedContents=[f"{title_val}"],
+                          endsWith="sqlite>")
+        target.runCommand(".exit")
+        printAndLog(f"Test[update_record]: The first record has been successfully updated - value {title_val}.",
+                    doPrint=False)
 
     def delete_record(xTable='food', title_val='Pizza'):
         printAndLog(f"Test[delete_record]: Delete {title_val} from the {xTable} table.", doPrint=False)
-        if getSetting('osImage') in ['debian', 'FreeBSD']:
-            target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
-                              endsWith="sqlite>")
-            target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>")
-            target.runCommand(f"SELECT * from {xTable};", expectExact=True, expectedContents=[f"{title_val}"],
-                              endsWith="sqlite>")
-            target.runCommand(f"DELETE FROM {xTable} WHERE title='{title_val}';",
-                              endsWith="sqlite>")
-            target.runCommand(f"SELECT * from {xTable};", expectedContents=[], endsWith="sqlite>")
-            target.runCommand(".exit")
-            printAndLog(
-                f"Test[delete_record]: The value {title_val} has been successfully deleted from the {xTable} table!",
-                doPrint=False)
+        target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
+                          endsWith="sqlite>")
+        target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>")
+        target.runCommand(f"SELECT * from {xTable};", expectExact=True, expectedContents=[f"{title_val}"],
+                          endsWith="sqlite>")
+        target.runCommand(f"DELETE FROM {xTable} WHERE title='{title_val}';",
+                          endsWith="sqlite>")
+        target.runCommand(f"SELECT * from {xTable};", expectedContents=[], endsWith="sqlite>")
+        target.runCommand(".exit")
+        printAndLog(
+            f"Test[delete_record]: The value {title_val} has been successfully deleted from the {xTable} table!",
+            doPrint=False)
 
     def drop_table(xTable='food'):
         printAndLog(f"Test[drop_table]: Drop {xTable} table", doPrint=False)
         retText = ''
-        if getSetting('osImage') in ['debian', 'FreeBSD']:
-            target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
-                              endsWith="sqlite>")
-            retText += \
-                target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>", shutdownOnError=False,
-                                  suppressErrors=True)[1]
-            if not (f"{xTable}" in retText):
-                target.runCommand(".exit")
-                logging.info(f"Test: drop_table: Invalid input parameter table {xTable}. Provide valid table name.")
-            else:
-                target.runCommand(f"DROP TABLE IF EXISTS {xTable};", endsWith="sqlite>")
-                target.runCommand(".tables", expectedContents=[], endsWith="sqlite>")
-                target.runCommand(".exit")
-                printAndLog(f"Test[drop_table]: The {xTable} table has been dropped successfully!", doPrint=False)
+        target.runCommand(f"{sqlite_bin} {xDb}", expectedContents=["SQLite version", ".help"],
+                          endsWith="sqlite>")
+        retText += \
+            target.runCommand(".tables", expectedContents=[f"{xTable}"], endsWith="sqlite>", shutdownOnError=False,
+                              suppressErrors=True)[1]
+        if not (f"{xTable}" in retText):
+            target.runCommand(".exit")
+            logging.info(f"Test: drop_table: Invalid input parameter table {xTable}. Provide valid table name.")
+        else:
+            target.runCommand(f"DROP TABLE IF EXISTS {xTable};", endsWith="sqlite>")
+            target.runCommand(".tables", expectedContents=[], endsWith="sqlite>")
+            target.runCommand(".exit")
+            printAndLog(f"Test[drop_table]: The {xTable} table has been dropped successfully!", doPrint=False)
 
     def drop_database(pathToFile='~'):
-
         printAndLog(f"Test[drop_database]: Drop sqlite {xDb} database", doPrint=False)
         if target.doesFileExist(xFile=xDb, pathToFile=pathToFile, shutdownOnError=False):
-            if getSetting('osImage') in ['debian', 'FreeBSD']:
-                target.runCommand(f"rm -f {pathToFile}/{xDb}")
-                printAndLog(f"Test[drop_database]: Database {xDb} dropped successfully!", doPrint=False)
+            target.runCommand(f"rm -f {pathToFile}/{xDb}")
+            printAndLog(f"Test[drop_database]: Database {xDb} dropped successfully!", doPrint=False)
         else:
             target.shutdownAndExit(f"\nTest[drop_database]: Failed to find <{pathToFile}/{xDb}> on target.", exitCode=EXIT.Run)
 
