eeaf9c6765e8116c51aa321bc641f61497bd79ab
Rod Chapman <rod@proteancode.com>
2020-05-09 14:24:19 +0100

    Document meaning of DUMMY_TFTP

--- fett/apps/freertos/tftp_server.h
+++ fett/apps/freertos/tftp_server.h
@@ -2,13 +2,21 @@
 
 #define DUMMY_TFTP 0
 
-// Waits for an incoming connection on TFTP_PORT and receives
-// a file, writing the contents to buffer[0 .. buffer_len-1] and
-// the indicated file name into file_name[0 .. file_name_len-1].
-// file_name will be zero-terminated.
+// IF DUMMY_TFTP == 1
 //
-// Returns the number of bytes received on success.
-// Returns 0 on error.
+//  Places a known, constant signed payload into buffer[0 .. 69]
+//  Places "f" into file_name[0 .. 1]
+//  Suitable as a dumb known-answer test, needing no TFTP client
+//
+// ELSE
+//
+//  Waits for an incoming connection on TFTP_PORT and receives
+//  a file, writing the contents to buffer[0 .. buffer_len-1] and
+//  the indicated file name into file_name[0 .. file_name_len-1].
+//  file_name will be zero-terminated.
+//
+//  Returns the number of bytes received on success.
+//  Returns 0 on error.
 uint32_t TFTP_Receive_One_File (uint8_t *buffer,         // out
                                 uint32_t buffer_len,     // in
                                 char    *file_name,      // out
