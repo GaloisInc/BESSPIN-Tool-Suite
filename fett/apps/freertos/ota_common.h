5c3ad967825c55e02413d3873fada01fe3ba88f7
Rod Chapman <rod@proteancode.com>
2020-05-09 11:12:00 +0100

    Add extern declarations for file_buffer and filename_buffer here.

--- fett/apps/freertos/ota_common.h
+++ fett/apps/freertos/ota_common.h
@@ -3,4 +3,7 @@
 // The known public key used for signature verification.
 // Completed in ota_common.c
 extern const byte raw_pk[ED25519_KEY_SIZE];
-#
+
+// The incoming TFTP file buffer and file name. Completed in ota.c
+extern uint8_t file_buffer[TFTP_FILE_BUFFER_LEN];
+extern char    filename_buffer[ffconfigMAX_FILENAME];
