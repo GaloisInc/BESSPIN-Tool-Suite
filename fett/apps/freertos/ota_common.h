d04970665905ca01c8df44d78a1c7e4347abd038
Rod Chapman <rod@proteancode.com>
2020-05-09 15:55:29 +0100

    Move raw_pk constant into ota.c where it belongs.

--- fett/apps/freertos/ota_common.h
+++ fett/apps/freertos/ota_common.h
@@ -1,9 +1,5 @@
 #include "fettFreeRTOS.h"
 
-// The known public key used for signature verification.
-// Completed in ota_common.c
-extern const byte raw_pk[ED25519_KEY_SIZE];
-
 // The incoming TFTP file buffer and file name. Completed in ota.c
 extern uint8_t file_buffer[OTA_MAX_SIGNED_PAYLOAD_SIZE]; // SIZE set in setupEnv.json
 extern char    filename_buffer[ffconfigMAX_FILENAME];
