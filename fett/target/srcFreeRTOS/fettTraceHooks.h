c0b50507f2e9ff8257bb424af389fdfd59319949
Ramy Tadros <rtadros@galois.com>
2020-05-11 14:20:22 -0700

    adding some initial hooks

--- fett/target/srcFreeRTOS/fettTraceHooks.h
+++ fett/target/srcFreeRTOS/fettTraceHooks.h
@@ -1,11 +1,11 @@
 /*
 - This file has the definitions of trace hooks as defined in https://www.freertos.org/rtos-trace-macros.html
-- It is currently left empty, but should be augmented with debug hooks in case of fett.py is executed with the `-d` flag.
+- These hooks are activated only if `fett.py` is executed with the `-d` flag
 */
 
-/*
-#ifdef DEBUG_MACRO
-    #define traceTASK_SWITCHED_IN() doThisOnTaskEntering (pxCurrentTCB);
-    #define traceQUEUE_SEND(xQueue) doThisWhenSendingOnTheQueue (xQueue);
-#endif
-*/
+extern void hook_TaskCreateSuccess (void * xTask);
+extern void hook_TaskSwitchedIn (void * xTask);
+
+#define traceTASK_CREATE(xTask) hook_TaskCreateSuccess(xTask)
+//#define traceTASK_SWITCHED_IN() hook_TaskSwitchedIn(pxCurrentTCB)
+
