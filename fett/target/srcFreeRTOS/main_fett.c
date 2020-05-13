a6f432c17b2d2fcb62528cf47c7109972eb9e03c
Rod Chapman <rod@proteancode.com>
2020-05-13 20:27:16 +0100

    PR#239 review: initialize WolfSSL in vMain() not in main_fett()

--- fett/target/srcFreeRTOS/main_fett.c
+++ fett/target/srcFreeRTOS/main_fett.c
@@ -11,14 +11,7 @@ UBaseType_t xMainPriority = tskIDLE_PRIORITY+100; //100 is chosen arbitrarily.
 //This is the entry point function
 void main_fett () {
 
-    int r;
-    
     fettPrintf ("\n>>>Beginning of Fett<<<\n");
-
-    // Initialize WolfSLL - this needs to be done before any other call to
-    // any other WolfSSL or Wolfcrypt API
-    r = wolfSSL_Init();
-    prERROR_IF_NEQ(r, SSL_SUCCESS, "main_fett: Initializing WolfSSL.");
 
     BaseType_t funcReturn = xTaskCreate(vMain, "main:vMain", configMINIMAL_STACK_SIZE * STACKSIZEMUL, NULL, xMainPriority, NULL);
 
@@ -33,9 +26,16 @@ void vMain (void *pvParameters) {
     (void) pvParameters;
     BaseType_t funcReturn;
     uint32_t recvNotification;
+    int r;
+    
     xMainTask = xTaskGetCurrentTaskHandle();
 
     fettPrintf("vMain: Main task started...\r\n");
+
+    // Initialize WolfSLL - this needs to be done before any other call to
+    // any other WolfSSL or Wolfcrypt API
+    r = wolfSSL_Init();
+    vERROR_IF_NEQ(r, SSL_SUCCESS, "main_fett: Initializing WolfSSL.");
 
     //Start the FAT filesystem
     funcReturn = ff_init();
