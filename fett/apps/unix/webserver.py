fc20401e5e1e436b28041a7e7405d277d266db06
rtadros <rtadros@galois.com>
2020-05-14 11:12:05 -0700

    moving deploy contents to install for webserver

--- fett/apps/unix/webserver.py
+++ fett/apps/unix/webserver.py
@@ -25,6 +25,19 @@ def install (target):
     outLog += target.runCommand("cp -r certs/* /etc/ssl/certs")[1]
     outLog += target.runCommand("cp -r keys/* /etc/ssl/private")[1]
 
+    outLog += target.runCommand("echo \"Starting nginx service...\"")[1]
+    if isEqSetting('osImage','debian'):
+        outLog += target.runCommand("install nginx.service /lib/systemd/system/nginx.service", erroneousContents="install:")[1]
+        outLog += target.runCommand("systemctl start nginx.service", erroneousContents=["Failed to start", "error code"])[1]
+    elif isEqSetting('osImage','FreeBSD'):
+        outLog += target.runCommand("install -d /usr/local/etc/rc.d")[1]
+        outLog += target.runCommand("install rcfile /usr/local/etc/rc.d/nginx", erroneousContents="install:")[1]
+        outLog += target.runCommand("service nginx enable", erroneousContents="nginx does not exist")[1]
+        outLog += target.runCommand("service nginx start")[1]
+    else:
+        target.shutdownAndExit (f"Can't start nginx service on <{getSetting('osImage')}>",
+                     exitCode=EXIT.Dev_Bug)
+
     # TODO add errors above
 
     printAndLog("Nginx installed successfully.")
@@ -34,21 +47,10 @@ def install (target):
 @decorate.timeWrap
 def deploy (target):
     # target is a fett target object
-    outLog = ''
-    outLog += target.runCommand("echo \"Starting nginx service...\"")[1]
-    if getSetting('osImage') == 'debian':
-        outLog += target.runCommand("install nginx.service /lib/systemd/system/nginx.service", erroneousContents="install:")[1]
-        outLog += target.runCommand("systemctl start nginx.service", erroneousContents=["Failed to start", "error code"])[1]
-    elif getSetting('osImage') == 'FreeBSD':
-        outLog += target.runCommand("install -d /usr/local/etc/rc.d")[1]
-        outLog += target.runCommand("install rcfile /usr/local/etc/rc.d/nginx", erroneousContents="install:")[1]
-        outLog += target.runCommand("service nginx enable", erroneousContents="nginx does not exist")[1]
-        outLog += target.runCommand("service nginx start")[1]
-    else:
-        logAndExit (f"Can't start nginx service on <{getSetting('osImage')}>",
-                     exitCode=EXIT.Configuration)
-    return outLog
+    return ''
 
+@decorate.debugWrap
+@decorate.timeWrap
 def curlTest(target, url, extra=[], http2=False):
     out = curlRequest(url, http2=http2, extra=extra)
     try:
