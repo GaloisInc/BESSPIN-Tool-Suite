5de4f12a292e6594e0822f0aa9d86a0066c2af4d
rtadros <rtadros@galois.com>
2020-05-19 09:45:36 -0700

    adding an error check to sqlite installation

--- fett/apps/unix/database.py
+++ fett/apps/unix/database.py
@@ -15,7 +15,7 @@ def install(target):
     # All we need to do is install sqlite into a suitable location,
     # like /usr/bin
     outLog += target.runCommand("echo \"Installing sqlite into /usr/bin...\"")[1]
-    outLog += target.runCommand("install sqlite /usr/bin")[1]
+    outLog += target.runCommand("install sqlite /usr/bin",erroneousContents=["install:"])[1]
     printAndLog("Sqlite installed successfully.")
     return outLog
 
