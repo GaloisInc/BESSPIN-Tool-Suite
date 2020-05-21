f0aa7243dd477a8560741b3c3f23aa684ded5c81
rtadros <rtadros@galois.com>
2020-05-21 09:47:14 -0700

    removing bluespec_p1 from figuring out xlen and proc flavor part

--- fett/base/config.py
+++ fett/base/config.py
@@ -42,7 +42,7 @@ def loadConfiguration(configFile):
         loadConfigSection (xConfig,configData,xSection)
 
     # Get the XLEN and processor flavor
-    if (getSetting('processor') in ['chisel_p1', 'bluespec_p1']):
+    if (getSetting('processor') in ['chisel_p1']):
         setSetting('xlen',32)
     elif (getSetting('processor') in ['chisel_p2', 'bluespec_p2']):
         setSetting('xlen',64)
@@ -50,7 +50,7 @@ def loadConfiguration(configFile):
         logAndExit(f"Failed to determine xlen from <{getSetting('processor')}>.",exitCode=EXIT.Dev_Bug)
     if (getSetting('processor') in ['chisel_p1', 'chisel_p2']):
         setSetting('procFlavor', 'chisel')
-    elif (getSetting('processor') in ['bluespec_p1', 'bluespec_p2']):
+    elif (getSetting('processor') in ['bluespec_p2']):
         setSetting('procFlavor', 'bluespec')
     else:
         logAndExit(f"Failed to determine the processor flavor <chisel or bluespec>.",exitCode=EXIT.Dev_Bug)
