75c78b480b754e9eff35cd2c14498992b9c75bd5
Jared Weakly <jweakly@galois.com>
2020-05-21 11:54:03 -0700

    Propagate return code correctly

--- .github/action/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-
-sudo chown -R besspinuser /github/workspace
-
-exec "$@"
--- .github/action/index.js
+++ .github/action/index.js
@@ -1,44 +1,39 @@
 const child_process = require("child_process");
 const process = require("process");
 
-const getInput = (name) =>
-  (process.env[`INPUT_${name.replace(/ /g, "_").toUpperCase()}`] || "").trim();
+const toInpt = (n) => `INPUT_${n.replace(/ /g, "_").toUpperCase()}`;
+const getInput = (n) => (process.env[toInpt(n)] || "").trim();
 
-const env = [
-  "-e INPUT_SSH-KEY",
-  "-e INPUT_RUN",
-  "-e HOME",
-  "-e CI=true",
-  "-e GITHUB_JOB",
-  "-e GITHUB_REF",
-  "-e GITHUB_SHA",
-  "-e GITHUB_REPOSITORY",
-  "-e GITHUB_REPOSITORY_OWNER",
-  "-e GITHUB_RUN_ID",
-  "-e GITHUB_RUN_NUMBER",
-  "-e GITHUB_ACTOR",
-  "-e GITHUB_WORKFLOW",
-  "-e GITHUB_HEAD_REF",
-  "-e GITHUB_BASE_REF",
-  "-e GITHUB_EVENT_NAME",
-  "-e GITHUB_WORKSPACE",
-  "-e GITHUB_ACTION",
-  "-e GITHUB_EVENT_PATH",
-  "-e GITHUB_ACTIONS=true",
-  "-e ACTIONS_RUNTIME_URL",
-  "-e ACTIONS_RUNTIME_TOKEN",
-  "-e ACTIONS_CACHE_URL",
-  "-e RUNNER_OS",
-  "-e RUNNER_TOOL_CACHE",
-  "-e RUNNER_TEMP",
-  "-e RUNNER_WORKSPACE",
-];
+const env = ["-e HOME=/home/besspinuser", "-e CI=true"].concat(
+  [
+    "JOB",
+    "REF",
+    "SHA",
+    "REPOSITORY",
+    "REPOSITORY_OWNER",
+    "RUN_ID",
+    "RUN_NUMBER",
+    "ACTOR",
+    "WORKFLOW",
+    "HEAD_REF",
+    "BASE_REF",
+    "EVENT_NAME",
+    "WORKSPACE",
+    "ACTION",
+    "EVENT_PATH",
+    "ACTIONS=true",
+  ].map((e) => `-e GITHUB_${e}`),
+  ["input", "run"].map((e) => `-e ${toInpt(e)}`),
+  ["RUNTIME_URL", "RUNTIME_TOKEN", "CACHE_URL"].map((e) => `-e ACTIONS_${e}`),
+  ["OS", "TOOL_CACHE", "TEMP", "WORKSPACE"].map((e) => `-e RUNNER_${e}`)
+);
 
 try {
   const sshKey = getInput("ssh-key");
   const run = getInput("run");
   // This works because the VM with the self hosted runner has already pulled the image.
-  const image = "artifactory.galois.com:5008/fett-target:ci";
+  const image =
+    getInput("image") || "artifactory.galois.com:5008/fett-target:ci";
 
   const work = "/home/gitlab-runner/actions-runner/_work";
   const docker = child_process.spawn(
@@ -51,15 +46,14 @@ try {
       "--network=host",
       "--workdir /github/workspace",
       ...env,
-      '-v "/var/run/docker.sock":"/var/run/docker.sock" ',
-      `-v "${work}/_temp/_github_home":"/github/home"        `,
-      `-v "${work}/_temp/_github_workflow":"/github/workflow" `,
-      `-v "${work}/SSITH-FETT-Target/SSITH-FETT-Target":"/github/workspace"`,
+      "-v /var/run/docker.sock:/var/run/docker.sock",
+      `-v ${work}/_temp/_github_home:/github/home`,
+      `-v ${work}/_temp/_github_workflow:/github/workflow`,
+      `-v ${work}/SSITH-FETT-Target/SSITH-FETT-Target:/github/workspace`,
       image,
       "bash",
       "-c",
-      `"export HOME=/home/besspinuser
-      export PATH=/opt/Xilinx/Vivado/2019.1/bin:/opt/Xilinx/Vivado_Lab/2019.1/bin:$PATH
+      `"export PATH=/opt/Xilinx/Vivado/2019.1/bin:/opt/Xilinx/Vivado_Lab/2019.1/bin:$PATH
       . /opt/Xilinx/Vivado_Lab/2019.1/settings64.sh
       eval $(ssh-agent -s)
       ssh-add <(echo \\"${sshKey}\\")
@@ -68,7 +62,7 @@ try {
     ],
     { shell: true, stdio: "inherit" }
   );
-  process.exitCode = docker.exitCode;
+  docker.on("exit", (c) => process.exit(c));
 } catch (error) {
   process.stderr.write(error.message);
   process.exitCode = 1;
