832349fdf6f76c5680168431139e43aa41ec1fb9
rtadros <rtadros@galois.com>
2020-06-04 22:19:14 +0000

    error if not notifications are received

--- fett/target/srcFreeRTOS/main_fett.c
+++ fett/target/srcFreeRTOS/main_fett.c
@@ -121,6 +121,10 @@ void vMain (void *pvParameters) {
         } //else: notification received
     } while ((iNotif<nTasksNotif) && (exitCode==0) && ((xTaskGetTickCount() - xStartTime) < pdMS_TO_TICKS(1000*APP_TIMEOUT)));
 
+    if ((exitCode==0) && (iNotif<nTasksNotif)) {
+        fettPrintf("(Error)~  vMain: Missing notifications. [received %d/%d]\r\n",iNotif,nTasksNotif);
+        exitCode = 1;
+    }
     exitFett(exitCode);
     vTaskDelete(NULL);
 }
