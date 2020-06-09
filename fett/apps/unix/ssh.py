8ff78fbd60c320a4f120c159a546c8bdd55dc41b
Dragan Stosic <dragan@freeandfair.us>
2020-06-09 02:17:52 +0200

    not ssh conn closed

--- fett/apps/unix/ssh.py
+++ fett/apps/unix/ssh.py
@@ -39,6 +39,6 @@ def extensiveTest (target):
     # target.sendFile (pathToFile=getSetting('repoDir'),xFile='README.md',timeout=120)
     # printAndLog("Test scp passed successfully!", doPrint=False, tee=getSetting('appLog'))
     printAndLog("Ssh and scp test OK!", tee=getSetting('appLog'))
-    target.closeSshConn()
+    # target.closeSshConn()
 
     return
