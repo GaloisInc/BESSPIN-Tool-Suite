72171fe6d9b325675f083c01984a2625b916e98e
rtadros <rtadros@galois.com>
2020-06-10 01:06:18 +0000

    Adding instance ID and Ips and such

--- fett/base/utils/aws.py
+++ fett/base/utils/aws.py
@@ -4,10 +4,10 @@ Functions required to use SQS and S3
 NOTE: This module is shared between fett.py and fett-ci.py, so DO NOT IMPORT any functions
 """
 
-def sendSQS (urlQueue, exitFunc, status, jobId, nodeId, reason='fett'):
+def sendSQS (urlQueue, exitFunc, status, jobId, nodeId, reason='fett', hostIp='None', fpgaIp='None'):
 
     try:
-        import boto3, json
+        import boto3, json, urllib.request
     except Exception as exc:
         exitFunc(message=f"Failed to <import boto3, json>.",exc=exc)
 
@@ -16,12 +16,23 @@ def sendSQS (urlQueue, exitFunc, status, jobId, nodeId, reason='fett'):
     except Exception as exc:
         exitFunc(message=f"Failed to create the SQS client.",exc=exc)
 
+    # instanceID:
+    try:
+        instanceId = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
+    except Exception as exc:
+        exitFunc(message=f"Failed to obtain the instance ID.",exc=exc)
+
     msg = json.dumps({
             "job": {
               "id": str(jobId),
               "status": status, 
               "reason": reason,
               "node": str(nodeId)
+            },
+            "instance": {
+                "id": instanceId,
+                "instance-ip": hostIp,
+                "fpga-ip": fpgaIp
             }
         })
 
