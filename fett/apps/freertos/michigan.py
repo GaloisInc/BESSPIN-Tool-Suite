6a470ab60ca38273a6bc03736f006b0bfbe2041d
rtadros <rtadros@galois.com>
2020-06-19 23:24:08 +0000

    reformat tests + add lynx tests

--- fett/apps/freertos/michigan.py
+++ fett/apps/freertos/michigan.py
@@ -4,10 +4,11 @@ This is executed after loading the app on the target to execute FreeRTOS app for
 """
 
 from fett.base.utils.misc import *
+import time, subprocess
 
 @decorate.debugWrap
 @decorate.timeWrap
-def deploymentTest(target, curlTest, WEB_REPLY_OK):
+def deploymentTest(target):
     if (not isEqSetting('target','aws')):
         target.shutdownAndExit (f"<deploymentTest> is not implemented for Michigan on <{getSetting('target')}>",exitCode=EXIT.Implementation)
 
@@ -22,10 +23,23 @@ def deploymentTest(target, curlTest, WEB_REPLY_OK):
             target.shutdownAndExit (f"Test [{testName}] - FAILED!",exitCode=EXIT.Run)
 
     # Curl the help page
-    contentLength,code = curlTest(target, f"http://{serverUrl}/{curlTestPath}")
-    reportTestResult("Curl HTTP test page", code==WEB_REPLY_OK)
-
+    curlOut = subprocess.getoutput(f"curl -L -s -X GET -m 10 -I http://{serverUrl}/{curlTestPath}")
+    reportTestResult ("Curl HTTP test page",("HTTP/1.0 200 OK" in curlOut))
+    
     # lynx tests
+    def lynxTest (query, testNum):
+        command = ['lynx', '-source', f"\"{query}\""]
+        printAndLog(f"Lynx test-{testNum}: {' '.join(command)}",doPrint=False,tee=getSetting('appLog'))
+        try:
+            outLynx = str(subprocess.check_output(command, stderr=subprocess.STDOUT, timeout=30),'utf-8')
+            printAndLog(outLynx,doPrint=False,tee=getSetting('appLog'))
+        except Exception as exc:
+            target.shutdownAndExit (f"Test [query-{testNum}] - FAILED!",exc=exc,exitCode=EXIT.Run)
+
+        reportTestResult (f"query-{testNum}",(('error' not in outLynx) and ('ERROR' not in outLynx)))
+
+    for iQuery,xQuery in enumerate(lynxTestQueries):
+        lynxTest(xQuery,iQuery)
 
     return 
 
