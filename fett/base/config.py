6caaf62cb7f1520a74b322559fad0d403b23985d
Brian McCall <brian.mccall@fivetalent.com>
2020-06-19 09:40:14 -0700

    base64 decode password before setting it in the config file

--- fett/base/config.py
+++ fett/base/config.py
@@ -5,6 +5,7 @@ json details are in: ./utils/configData.json
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # """
 
 import configparser, json, os, re
+import base64
 from fett.base.utils.misc import *
 
 CONFIG_SECTIONS = ['backend', 'applications', 'build']
@@ -249,9 +250,11 @@ def genProdConfig(configFileSerialized, configFile):
         wasSet = False
         for xSection in xConfig:
             if (xSetting in xConfig[xSection]):
-                xConfig.set(xSection,xSetting,str(xValue))
-                wasSet = True
-                break
+              if(xSetting == "userPasswordHash"):
+                xValue = base64.b64decode(xValue).decode("utf-8")
+              xConfig.set(xSection,xSetting,str(xValue))
+              wasSet = True
+              break
         if (not wasSet):
             logAndExit(f"Failed to find the production setting <{xSetting}> in <{templateConfigPath}>.",exitCode=EXIT.Configuration)
 
