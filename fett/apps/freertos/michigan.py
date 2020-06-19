fed31a445b8ff9a678129f802766a249b3c8e6b1
rtadros <rtadros@galois.com>
2020-06-19 14:53:39 +0000

    first curl test

--- fett/apps/freertos/michigan.py
+++ fett/apps/freertos/michigan.py
@@ -13,6 +13,22 @@ def deploymentTest(target):
     if (not isEqSetting('target','aws')):
         target.shutdownAndExit (f"<deploymentTest> is not implemented for Michigan on <{getSetting('target')}>",exitCode=EXIT.Implementation)
 
+    serverUrl = f"{target.ipTarget}:{getSettingDict('michiganInfo',['httpPort'])}"
+    curlTestPath = getSettingDict('michiganInfo',['curlTestPath'])
+    lynxTestQueries = getSettingDict('michiganInfo',['lynxTestQueries'])
+
+    def reportTestResult (testName,isSuccess):
+        if (isSuccess):
+            printAndLog(f"Test [{testName}] - OK!",doPrint=True,tee=getSetting('appLog'))
+        else:
+            target.shutdownAndExit (f"Test [{testName}] - FAILED!",exitCode=EXIT.Run)
+
+    # Curl the help page
+    contentLength,code = freertos.curlTest(target, f"http://{serverUrl}/{curlTestPath}")
+    reportTestResult("Curl HTTP test page", code==freertos.WEB_REPLY_OK)
+
+    # lynx tests
+        
     return 
 
 
