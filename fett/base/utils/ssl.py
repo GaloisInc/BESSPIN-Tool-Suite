eac7851f4d38e0ccb8e594d35f05c5416ca66613
Steven Osborn <steven@lolsborn.com>
2020-07-02 00:10:26 -0700

    add serial files to working directory

--- fett/base/utils/ssl.py
+++ fett/base/utils/ssl.py
@@ -1,4 +1,5 @@
 import os
+import random
 import subprocess
 from fett.base.utils.misc import *
 
@@ -9,15 +10,22 @@ def gen_cert(name, key_path, cert_path):
     ca_path = os.path.join(getSetting('repoDir'), 'apps', 'ssl')
     ca_key = os.path.join(ca_path, "fettCA.key")
     ca_pem = os.path.join(ca_path, "fettCA.pem")
-
+    serial = os.path.join(cert_path, f"{name}.srl")
     subj = f"/C=US/ST=OR/L=Portland/O=Galois, Inc/CN={name}"
     # Private Key
     shellCommand(['openssl', 'genrsa', '-out', key_name, '2048'])
     # Certificate Signing Request
     shellCommand(['openssl', 'req', '-new', '-key', key_name, '-out', csr_name,
     '-subj', subj])
+
+    # Serial file
+    hash = random.getrandbits(128)
+    serfd = open(serial, "w+")
+    serfd.write(format(hash, 'x'))
+    serfd.close()
+
     # Certificate
     shellCommand(['openssl', 'x509', '-req', '-in', csr_name, '-CA', ca_pem,
-        '-CAkey', ca_key, '-CAcreateserial', '-out', crt_name, '-days', '825',
+        '-CAkey', ca_key, '-CAserial', serial, '-out', crt_name, '-days', '825',
          '-sha256']
     )
