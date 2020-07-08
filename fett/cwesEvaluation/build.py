487f251370a61743b848dc241d709443daed41d2
Brett Boston <boston@galois.com>
2020-07-07 17:50:46 -0700

    Get resource management tests building

--- fett/cwesEvaluation/build.py
+++ fett/cwesEvaluation/build.py
@@ -39,4 +39,35 @@ def buildCwesEvaluation():
                     'emulation',
                     'defaultEnvLinux.mk'),
         buildDir)
+
+    # Write configuration header file
+    # TODO: Write this based on values in the config file.  You'll also have to
+    # change it based on the suite being run.  Lastly, it doesn't seem to exist
+    # for all classes of vulnerabilities.
+    with open(os.path.join(buildDir, "testsParameters.h"), 'w') as f:
+        f.write("#define nResourceLimit 10\n")
+
+
+    crossCompileUnix()
+
     logAndExit(f"<buildCwesEvaluation> not implemented", exitCode=EXIT.Dev_Bug)
+
+@decorate.debugWrap
+@decorate.timeWrap
+def crossCompileUnix():
+    #cross-compiling sanity checks
+    if ((not isEqSetting('cross-compiler','Clang')) and isEqSetting('linker','LLD')):
+        # TODO: Is this true for testgen?
+        warnAndLog (f"Linking using <{getSetting('linker')}> while cross-compiling with <{getSetting('cross-compiler')} is not supported. Linking using <GCC> instead.>.")
+        setSetting('linker','GCC')
+
+    printAndLog (f"Cross-compiling ...")
+    envLinux = []
+    osImageCap1 = getSetting('osImage')[0].upper() + getSetting('osImage')[1:]
+    envLinux.append(f"OS_IMAGE={osImageCap1}")
+    envLinux.append(f"BACKEND={getSetting('target').upper()}")
+    envLinux.append(f"COMPILER={getSetting('cross-compiler').upper()}")
+    envLinux.append(f"LINKER={getSetting('linker').upper()}")
+    logging.debug(f"going to make using {envLinux}")
+    make (envLinux,getSetting('buildDir'))
+    printAndLog(f"Files cross-compiled successfully.")
