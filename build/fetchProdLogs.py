d159b1843cee98237e2eb8c5e43b20906678fef5
rtadros <rtadros@galois.com>
2020-07-24 16:13:52 -0700

    features list

---
+++ build/fetchProdLogs.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+"""
+--- fetchProdLogs.py is a utility to fetch logs from FETT production.
+--- usage:
+"""
+
+import sys,os,boto3
+
+prodBucket = 'master-ssith-fett-target-researcher-artifacts'
+
+
+
+def main():
+    try:
+        s3 = boto3.client('s3', region_name='us-west-2')
+    except Exception as exc:
+        exitFunc(message=f"Failed to create the S3 client.",exc=exc)
+
+
+
+
+if __name__ == '__main__':
+    # Reading the bash arguments
+    xArgParser = argparse.ArgumentParser (description='Fetch FETT logs from production')
+    xGroupConfig = xArgParser.add_mutually_exclusive_group(required=True)
+    xGroupConfig.add_argument ('-a', '--allLogs', help='Download all production artifacts.', action='store_true')
+    xGroupConfig.add_argument ('-g', '--grepFilter', help='Download the tarballs containing the search string.')
+    xArgParser.add_argument ('-ts', '--startTime', help='Download the tarballs created after start time.')
+    xArgParser.add_argument ('-tf', '--endTime', help='Download the tarballs created before end time.')
+    xArgParser.add_argument ('-o', '--outputDirectory', help='Overwrites the default output directory: ./logsDir/')
+    xArgs = xArgParser.parse_args()
+
+    main(xArgs)
