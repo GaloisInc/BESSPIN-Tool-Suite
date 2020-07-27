729421f30c30f30c2b64a74a972e539c20d037ec
rtadros <rtadros@galois.com>
2020-07-27 08:10:09 -0700

    better structure

--- build/fetchProdLogs.py
+++ build/fetchProdLogs.py
@@ -5,19 +5,42 @@
 --- usage:
 """
 
-import sys,os,boto3
+import sys,os,traceback
+import boto3
 
 prodBucket = 'master-ssith-fett-target-researcher-artifacts'
+artifactsPath = 'fett-target/production/artifacts'
 
+def tryFunc (func,errorMessage,*args,**kwargs):
+    try:
+        return func(*args,**kwargs)
+    except Exception as exc:
+        errorExit(errorMessage,exc=exc)
 
+def formatExc (exc):
+    """ format the exception for printing """
+    try:
+        return f"<{exc.__class__.__name__}>: {exc}"
+    except:
+        return '<Non-recognized Exception>'
+
+def errorExit(message,exc=None):
+    if (exc):
+        message += f"\n{formatExc(exc)}."
+    print(f"(ERROR)~ {message}")
+    if (exc):
+        print(traceback.format_exc())
+    exit(1)
 
 def main():
-    try:
-        s3 = boto3.client('s3', region_name='us-west-2')
-    except Exception as exc:
-        exitFunc(message=f"Failed to create the S3 client.",exc=exc)
+    s3 = tryFunc(boto3.client,
+                "Failed to create the S3 client.",
+                's3',region_name='us-west-2')
 
-
+    # fetch a list of all files
+    listFiles = tryFunc(s3.list_objects_v2,
+                    "Failed to list all files in artifacts path.",
+                    Bucket=prodBucket, MaxKeys=10, Prefix=artifactsPath)
 
 
 if __name__ == '__main__':
