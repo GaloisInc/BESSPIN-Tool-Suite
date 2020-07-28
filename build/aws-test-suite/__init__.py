79c129d4e63a0373deafbb07fae761dcefe37063
Tristan Peng <pengt@catlin.edu>
2020-07-28 12:43:37 -0700

    Import into __init__

--- build/aws-test-suite/__init__.py
+++ build/aws-test-suite/__init__.py
@@ -2,6 +2,8 @@
 FETT CI Library
 """
 from .workgroup import *
+from .aws_credentials import AWSCredentials
+from .userdata_creator import UserdataCreator
 
 
 def setup_logger(log_fname="aws-test-suite.log", level="info"):
@@ -19,5 +21,5 @@ def setup_logger(log_fname="aws-test-suite.log", level="info"):
     }
     log_level = llevel[level]
     logging.basicConfig(format='%(asctime)s: (%(levelname)s)  %(message)s', datefmt='%I:%M:%S %p', level=log_level,
-                            handlers=[logging.FileHandler(log_fname), logging.StreamHandler()])
+                        handlers=[logging.FileHandler(log_fname), logging.StreamHandler()])
     logging.info(f"AWS Test Suite Logger Initialized\nLog File: {log_fname}\nLog Level: {level}")
