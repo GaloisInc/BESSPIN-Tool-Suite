04bdc75bb26ce8235a52bf795ba942e09c0120e1
rtadros <rtadros@galois.com>
2020-07-28 09:57:42 -0700

    remove irrelevant changes from the PR

--- fett/target/srcFreeRTOS/fettFreeRTOS.h
+++ fett/target/srcFreeRTOS/fettFreeRTOS.h
@@ -33,17 +33,15 @@ fett header for includes, externs, and global variables
 #define MSGBUF_SIZE 32
 #define STACKSIZEMUL 12 //For tasks. Needs to be larger on 64-bit (not for FETT)
 #include "fettUserConfig.h"
-
 /* Task notifications values */
-#define NOTIFY_SUCCESS      0x0000FFFF // This allows us 16 distinct successes
-#define NOTIFY_SUCCESS_NTK  0x00000001
+#define NOTIFY_SUCCESS 0x0000FFFF //This allows us 16 distinct successes
+#define NOTIFY_SUCCESS_NTK 0x00000001
 #define NOTIFY_SUCCESS_HTTP 0x00000002
-#define NOTIFY_SUCCESS_OTA  0x00000004
-
-#define NOTIFY_FAIL      0xFFFF0000 // This allows us 16 distinct failures
-#define NOTIFY_FAIL_NTK  0x00010000
+#define NOTIFY_SUCCESS_OTA 0x00000004
+#define NOTIFY_FAIL 0xFFFF0000 //This allows us 16 distinct failures
+#define NOTIFY_FAIL_NTK 0x00010000
 #define NOTIFY_FAIL_HTTP 0x00020000
-#define NOTIFY_FAIL_OTA  0x00040000
+#define NOTIFY_FAIL_OTA 0x00040000
 /* True and False values */
 #define uTRUE 1
 #define uFALSE 0
@@ -103,6 +101,7 @@ extern bool StopRequested(void);
 // Debug printing and exit
 extern void fettPrintf (const char * textToPrint, ...);
 extern void exitFett (uint8_t exitCode);
+
 
 extern void _open (const char * dump1, int dump2, ...); //Needed for WolfSSL to compile -- should never be called
 extern void _gettimeofday (struct timeval *__p, void *__tz); //Needed for WolfSSL to compile -- should never be called
