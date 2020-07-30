db477c260a1cd5e426e3222ac852e025f6f7a705
Tristan Peng <pengt@catlin.edu>
2020-07-29 19:00:02 -0700

    Add an Instance class that can be managed by InstanceManager

--- build/aws_test_suite/instance_manager.py
+++ build/aws_test_suite/instance_manager.py
@@ -1,3 +1,8 @@
+import re
+
+from .aws_tools import *
+
+
 class InstanceManager:
     def __init__(self, cap=1, instances=None):
         if instances is None:
@@ -11,3 +16,12 @@ class InstanceManager:
 
     def terminate_instance(self, name):
         pass
+
+
+class Instance:
+    def __init__(self, ami, name, **kwargs):
+        if not re.fullmatch('ami-[A-Za-z0-9]+', ami):
+            ami = get_ami_id_from_name(ami)
+
+        self._ami = ami
+        self._name = name
