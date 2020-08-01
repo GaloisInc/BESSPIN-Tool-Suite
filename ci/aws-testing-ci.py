42abccfd855cfaf0e4d25084e18742fd14e2d96a
Tristan Peng <pengt@catlin.edu>
2020-07-31 17:14:23 -0700

    Fix instance name

--- ci/aws-testing-ci.py
+++ ci/aws-testing-ci.py
@@ -10,7 +10,6 @@ import os
 sys.path.insert(1, os.path.dirname(os.getcwd()))
 
 import argparse
-from datetime import datetime
 from build.aws_test_suite import *
 
 h = "[AWS Testing CI] : "
@@ -47,6 +46,7 @@ def main(args):
             b = f"-{args.branch}" if args.branch else ""
             bb = f"-{args.binaries_branch}" if args.binaries_branch else ""
             jn = f"{args.name}-r{j}-i{k}{b}{bb}-{r[k]}"
+            n = args.name if args.name else jn
 
             u = UserdataCreator.default(
                 a, args.branch, args.binaries_branch, args.key_path
@@ -56,9 +56,9 @@ def main(args):
                            nix-shell --command "ci/fett-ci.py -ep AWSTesting runDevPR -job { jn } -i {str(k)}"' """
             )
             i.add_instance(
-                Instance(args.ami, f"{args.name}-{str(k)}", userdata=u.userdata)
+                Instance(args.ami, f"{n}", userdata=u.userdata)
             )
-            console.log(f"{h}Queueing {r[k]}, with job name {jn}.")
+            console.log(f"{h}Queueing {r[k]}, with job name {jn} and instance name {n}.")
 
     console.log(f"{h}Starting instances and running tests.")
     while not i.done:
@@ -71,8 +71,6 @@ def main(args):
 
 
 if __name__ == "__main__":
-    today = datetime.now().strftime("%m%d%y")
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "ami", type=str, help="AWS AMI ID to use, i.e. 'ami-xxxxxxxxxxxxxxxxxx'"
@@ -82,68 +80,67 @@ if __name__ == "__main__":
         "--branch",
         type=str,
         help="The branch of FETT-Target to check out on the AWS instance, and run tests on. Defaults to whatever is "
-        "present on AMI",
+        "present on AMI"
     )
     parser.add_argument(
         "-bb",
         "--binaries-branch",
         type=str,
         help="The branch of FETT-Bineries to check out on the AWS instance, and run tests on. Defaults to whatever is "
-        "present on AMI",
+        "present on AMI"
     )
     parser.add_argument(
         "-c",
         "--count",
         type=int,
         help="Number of possible configurations (same as length of the output of a dry run of the CI script).",
-        default=-1,
+        default=-1
     )
     parser.add_argument(
         "-cp",
         "--cap",
         type=int,
         help="The maximum number of instances running at once (default is 1)",
-        default=1,
+        default=1
     )
     parser.add_argument(
         "-cd",
         "--credentials",
         type=str,
-        help="AWS credentials file (Use either --init (-i) or --credentials (-cd))",
+        help="AWS credentials file (Use either --init (-i) or --credentials (-cd))"
     )
     parser.add_argument(
         "-i",
         "--init",
         help="Initialize (first run or if tokens have expired) (Use either --init (-i) or --credentials (-cd))",
-        action="store_true",
+        action="store_true"
     )
     parser.add_argument(
         "-idx",
         "--instance-index",
         type=int,
         help="Specify a specific index of target to run - if entered, this program will run $RUNS worth of this "
-        "instance index only.",
+        "instance index only."
     )
     parser.add_argument(
         "-k",
         "--key-path",
         type=str,
         help='Path to the SSH key to be used with -b || -bb flags. Default: ["~/.ssh/aws-ci-gh"]',
-        default="~/.ssh/aws-ci-gh",
+        default="~/.ssh/aws-ci-gh"
     )
     parser.add_argument(
         "-n",
         "--name",
         type=str,
         help="Base name to assign to instances created by this script (default is <current-date>-aws-testing)",
-        default=f"{today}-aws-testing",
     )
     parser.add_argument(
         "-r",
         "--runs",
         type=int,
         help="How many complete runs of all targets to make. Default 1.",
-        default=1,
+        default=1
     )
 
     main(parser.parse_args())
