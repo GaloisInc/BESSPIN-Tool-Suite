6b5701e1bb943cb508b1ede0807a28f2c966a1df
lyellread <lyellread@gmail.com>
2020-08-14 08:18:33 -0700

    Adding optional logs in ci/utils.py
    This adds an additional list of log filepaths that will only be collected
    if they exist, and ignored (with a warning message) if they are not present
    This allows some logs that are not always present (i.e. /var/log/user-
    data.log) to be included in the .tar.gz of all logs.

--- ci/utils.py
+++ ci/utils.py
@@ -148,9 +148,17 @@ def prepareArtifact(
     # This is assuming fett-ci uses the default workDir and logFile
     workDir = os.path.join(repoDir, "workDir")
     logFile = os.path.join(workDir, "fett.log")
+    userDataLogFile = os.path.join("/var", "log", "user-data.log")
     outFiles = glob.glob(os.path.join(workDir, "*.out"))
-    listArtifacts = [configFile, logFile] + outFiles
-    for xArtifact in listArtifacts:
+
+    # List all artifacts that generate a termination if they cannot be captured
+    listEssentialArtifacts = [configFile, logFile] + outFiles
+
+    # List all artifacts that are fine to ignore if they do not exist.
+    listArtifacts = [userDataLogFile]
+
+    # Collect the essential artifacts, exit fett-ci if it cannot be gotten
+    for xArtifact in listEssentialArtifacts:
         try:
             shutil.copy2(xArtifact, artifactsPath)
         except Exception as exc:
@@ -158,6 +166,14 @@ def prepareArtifact(
                 message=f"Failed to copy <{xArtifact}> to <{artifactsPath}>.", exc=exc
             )
 
+    # Collect logs that will not generate termination if they cannot be found.
+    for xArtifact in listArtifacts:
+        try:
+            shutil.copy2(xArtifact, artifactsPath)
+        except:
+            print(f"(Warning)~  Unable to collect non-essential log file { xArtifact }")
+
+    # Determine what to do with the dir of logs
     if entrypoint in ["AWS", "AWSTesting"]:
         # import the required modules
         try:
