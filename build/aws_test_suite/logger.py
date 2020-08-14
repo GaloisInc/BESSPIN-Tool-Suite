40a68348627f3b06e78d2a5acca5e9481c3d4110
Ethan Lew <elew@galois.com>
2020-08-14 10:48:47 -0700

    revert to Logger class and change log_assertion_fails to use it

--- build/aws_test_suite/logger.py
+++ build/aws_test_suite/logger.py
@@ -1,5 +1,6 @@
 import functools
-import logging
+from termcolor import cprint
+from datetime import datetime
 
 
 def log_assertion_fails(func):
@@ -11,76 +12,97 @@ def log_assertion_fails(func):
         try:
             ret = func(*args, **kwargs)
         except AssertionError as exc:
-            log.exception(f"{func.__name__} failed an assertion <{exc}>")
+            log.error(f"{func.__name__} failed an assertion <{exc}>")
             raise exc
         except Exception as exc:
-            log.exception(f"{func.__name__} failed with non-assertion <{exc}>")
+            log.error(f"{func.__name__} failed with non-assertion <{exc}>")
             raise exc
         return ret
     return wrappedFn
 
 
-class LevelFilter(logging.Filter):
-    def __init__(self, low, high):
-        self._low = low
-        self._high = high
-        logging.Filter.__init__(self)
+class Logger:
+    def __init__(self, log_fname="aws-test-suite.log", results_fname="results.txt"):
+        """
+        Global logger setup
+        :param log_fname: Output log filename, defaults to 'aws-test-suite.log'
+        :type log_fname: str, optional
+        :param level: Output information level, defaults to 'info'
+        :type level: str, optional
+        """
 
-    def filter(self, record):
-        if self._low <= record.levelno <= self._high:
-            return True
-        return False
+        # Store the name of the log files
+        self.log_fname = log_fname
+        self.results_fname = results_fname
 
+        # Empty out log files
+        open(log_fname, "w").close()
+        open(results_fname, "w").close()
 
-class SuiteLogger(logging.Logger):
-    """AWS Test Suite Logger Configuration
-    take the base logger in logging, configure it with handlers, and set log levels"""
-    RESULT_LEVEL_NUM = 100
-    def __init__(self, *args,
-                 log_filepath="aws-test-suite.log",
-                 name="aws-test-suite",
-                 results_filepath="results.txt",
-                 filehandler_loglevel=logging.INFO,
-                 **kwargs):
-        super().__init__(name, *args, **kwargs)
+    def log_out(self, message=None, level="Info"):
+        """
+        Prints message to console with the specified level and outputs to log accordingly
+        :param message: Message to print
+        :type message: str
+        :param level: Level of message
+        :type level: str
+        """
 
-        # add a new log level for split filehandler
-        logging.addLevelName(self.RESULT_LEVEL_NUM, "RESULTS")
+        if message is None:
+            pass
 
-        # setup file logging -- accept log level
-        formatter = logging.Formatter('%(asctime)s: (%(levelname)s)  %(message)s', datefmt='%I:%M:%S %p')
-        results_formatter = logging.Formatter('%(asctime)s: %(message)s', datefmt='%I:%M:%S %p')
+        levels = {
+            "Debug": "yellow",
+            "Error": "red",
+            "Info": "white",
+            "Warning": "yellow",
+            "Results": "cyan",
+        }
 
-        # empty out target files
-        open(log_filepath, 'w').close()
-        open(results_filepath, 'w').close()
+        # Check log level in levels.
+        assert level in list(
+            levels.keys()
+        ), f"{level} is not a valid level, must be one of {', '.join(levels.keys())}"
 
-        # setup file logging
-        self.filehandler = logging.FileHandler(log_filepath)
-        self.filehandler.setFormatter(formatter)
-        self.filehandler.addFilter(LevelFilter(filehandler_loglevel, 99))
+        # Print all but debug (to file) and info (to default color) to screen
+        if level.lower() != "debug" and level.lower() != "info":
+            # Print to screen in color
+            cprint(f"({level})~ {message}", levels[level])
 
-        # results filepath
-        self.results_filehandler = logging.FileHandler(results_filepath)
-        self.results_filehandler.setFormatter(results_formatter)
-        self.results_filehandler.addFilter(LevelFilter(self.RESULT_LEVEL_NUM, self.RESULT_LEVEL_NUM))
+        if level.lower() == "info":
+            print(f"({level})~ {message}")
 
-        # setup console logging -- set to info log level
-        self.streamhandler = logging.StreamHandler()
-        self.streamhandler.setFormatter(formatter)
-        self.streamhandler.setLevel(logging.INFO)
+        # Write results to results file
+        if level.lower() == "results":
+            with open(self.results_fname, "a") as f:
+                f.write(f"[ { datetime.now().strftime('%Y/%m/%d') } ] : { message }\n")
 
-        # add the two handlers to csaf logging
-        self.addHandler(self.filehandler)
-        self.addHandler(self.streamhandler)
-        self.addHandler(self.results_filehandler)
+        # Write log message
+        with open(self.log_fname, "a") as f:
+            f.write(
+                f"[ { datetime.now().strftime('%Y/%m/%d @ %H:%M:%S:%f') } ] : ({level})~ {message}\n"
+            )
 
-    def results(self, message, *args, **kwargs):
-        """new logging category -- logs to RESULTS log level and at file handle for results"""
-        if self.isEnabledFor(self.RESULT_LEVEL_NUM):
-            self._log(self.RESULT_LEVEL_NUM, message, args, **kwargs)
+        if level.lower() == "error":
+            cprint("(Error)~ Exiting.", "red")
+            exit(0)
+
+    def debug(self, message):
+        self.log_out(message, "Debug")
+
+    def error(self, message):
+        self.log_out(message, "Error")
+
+    def info(self, message):
+        self.log_out(message, "Info")
+
+    def warning(self, message):
+        self.log_out(message, "Warning")
+
+    def results(self, message):
+        self.log_out(message, "Results")
 
 
 ## Global Package Logger
-log = SuiteLogger()
+log = Logger()
 
