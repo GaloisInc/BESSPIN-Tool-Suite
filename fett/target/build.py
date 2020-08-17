ea085206ce3b4003cb74411e5ca1e742e313aea2
rtadros <rtadros@galois.com>
2020-08-17 22:05:09 +0000

    only use Clang with LLD

--- fett/target/build.py
+++ fett/target/build.py
@@ -276,9 +276,19 @@ def cleanDirectory (xDir,endsWith='.o'):
 @decorate.debugWrap
 @decorate.timeWrap
 def crossCompileUnix(directory,extraString=''):
+    if (isEqSetting('binarySource','SRI-Cambridge')):
+        if (not isEqSetting('cross-compiler','Clang')):
+            warnAndLog (f"Compiling using <{getSetting('cross-compiler')}> for <{getSetting('binarySource')}> is not supported."
+                " Compiling using <Clang> instead.")
+            setSetting('cross-compiler','Clang')
+        if (not isEqSetting('linker','LLD')):
+            warnAndLog (f"Linking using <{getSetting('linker')}> for <{getSetting('binarySource')}> is not supported."
+                " Linking using <LLD> instead.")
+            setSetting('linker','LLD')
+
     #cross-compiling sanity checks
     if ((not isEqSetting('cross-compiler','Clang')) and isEqSetting('linker','LLD')):
-        warnAndLog (f"Linking using <{getSetting('linker')}> while cross-compiling with <{getSetting('cross-compiler')} is not supported. Linking using <GCC> instead.>.")
+        warnAndLog (f"Linking using <{getSetting('linker')}> while cross-compiling with <{getSetting('cross-compiler')} is not supported. Linking using <GCC> instead.")
         setSetting('linker','GCC')
 
     printAndLog (f"Cross-compiling {extraString}...")
