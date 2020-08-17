1e7ac55fe963eb7e6d3044c993487fd4e2dd40ee
rtadros <rtadros@galois.com>
2020-08-17 22:17:41 +0000

    makefile for SRI-Ucam

--- fett/target/utils/defaultEnvLinux.mk
+++ fett/target/utils/defaultEnvLinux.mk
@@ -1,15 +1,24 @@
 # This file has the necessary includes to cross-compile files for Linux
 
-ARCH_ABI = -march=rv64imafdc -mabi=lp64d
+ifeq ($(BIN_SOURCE),SRI_Cambridge)
+	ARCH_ABI := -march=rv64imafdcxcheri -mabi=l64pc128d
+else
+	ARCH_ABI := -march=rv64imafdc -mabi=lp64d
+endif
 FETT_DEFS := -DfettOnLinux -DfettOn$(OS_IMAGE) -DfettOn$(TARGET) 
 FETT_DEFS += -DtestgenOnLinux -DtestgenOn$(OS_IMAGE) -Dtestgen$(TARGET)
 
 CFLAGS := $(ARCH_ABI) -Wall -O0 $(FETT_DEFS)
-PREFIX_FreeBSD := riscv64-unknown-freebsd12.1
-PREFIX_Debian := riscv64-unknown-linux-gnu
 
+PREFIX_FreeBSD := riscv64-unknown-freebsd12.1
+ifeq ($(BIN_SOURCE),SRI_Cambridge)
+	PREFIX_FreeBSD := riscv64-unknown-freebsd13.0
+else
+	PREFIX_FreeBSD := riscv64-unknown-freebsd12.1
+endif
+PREFIX_Debian := riscv64-unknown-linux-gnu
 CFLAGS_FreeBSD_CLANG := -Wno-error=sign-compare -mno-relax
-CFLAGS += ${CFLAGS_$(OS_IMAGE)_$(COMPILER)} ${BIN_SOURCE_$(BIN_SOURCE)}
+CFLAGS += ${CFLAGS_$(OS_IMAGE)_$(COMPILER)} -DBIN_SOURCE_$(BIN_SOURCE)
 
 LD_FLAGS_FreeBSD := -L$(PREFIX_FreeBSD)-ld -lpam -lrt
 LD_FLAGS_Debian := -lpam -lpam_misc -lrt -lkeyutils 
@@ -19,7 +28,11 @@ LDFLAGS := $(ARCH_ABI) ${LD_FLAGS_$(OS_IMAGE)}
 
 CC_GCC := ${PREFIX_$(OS_IMAGE)}-gcc
 SYSROOT_Debian := $(shell $(CC_GCC) -print-sysroot)
-SYSROOT_FreeBSD := $(FETT_GFE_FREEBSD_SYSROOT)
+ifeq ($(BIN_SOURCE),SRI_Cambridge)
+	SYSROOT_FreeBSD := /opt/cheri/sdk/sysroot
+else
+	SYSROOT_FreeBSD := $(FETT_GFE_FREEBSD_SYSROOT)
+endif
 CC_CLANG := clang -target ${PREFIX_$(OS_IMAGE)} --sysroot=${SYSROOT_$(OS_IMAGE)}
 
 LD_GCC_GCC := $(CC_GCC)
