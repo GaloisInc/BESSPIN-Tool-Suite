ce912808e348d8442bef535a84e097e74f9a75ec
rtadros <rtadros@galois.com>
2020-08-24 21:35:28 +0000

    update test 476 to the new structure

--- fett/cwesEvaluation/tests/resourceManagement/vulClassTester.py
+++ fett/cwesEvaluation/tests/resourceManagement/vulClassTester.py
@@ -13,7 +13,8 @@ testsInfo = {
     395 : { 'unix' : 2, 'FreeRTOS' : 2 },
     415 : { 'unix' : 2, 'FreeRTOS' : 2 },
     416 : { 'unix' : 2, 'FreeRTOS' : 2 },
-    468 : { 'unix' : 2, 'FreeRTOS' : 2 }
+    468 : { 'unix' : 2, 'FreeRTOS' : 2 },
+    476 : { 'unix' : 3, 'FreeRTOS' : 2 }
 }
 
 class vulClassTester(testgenTargetCompatabilityLayer):
