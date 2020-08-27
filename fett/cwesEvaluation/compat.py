b31da1cff22c52f6bc740a5cf517ca36a18f3064
rtadros <rtadros@galois.com>
2020-08-27 00:21:55 +0000

    pythonizing the compat layer

--- fett/cwesEvaluation/compat.py
+++ fett/cwesEvaluation/compat.py
@@ -1,7 +1,5 @@
 from fett.base.utils.misc import *
 
-# TODO: Many functions are still missing here
-
 class testgenTargetCompatabilityLayer:
     """
     This class wraps a FETT target and presents a testgen target interface
@@ -33,116 +31,23 @@ class testgenTargetCompatabilityLayer:
 
         self.backend = ('fpga' if isEqSetting('target', 'aws')
                                else getSetting('target'))
+        self.osImage = getSetting("osImage")
 
-    @property
-    def onlySsh(self):
-        return self.target.onlySsh
+    def __getattr__ (self,attr):
+        val = getattr(self.target,attr)
+        return val
 
-    @property
-    def osImage(self):
-        return getSetting("osImage")
-
-    @property
-    def isCurrentUserRoot(self):
-        return self.target.isCurrentUserRoot
-
-    @isCurrentUserRoot.setter
-    def isCurrentUserRoot(self, value):
-        self.target.isCurrentUserRoot = value
-
-    @property
-    def userName(self):
-        return self.target.userName
-
-    @userName.setter
-    def userName(self, value):
-        self.target.userName = value
-
-    @property
-    def userPassword(self):
-        return self.target.userPassword
-
-    @userPassword.setter
-    def userPassword(self, value):
-        self.target.userPassword = value
-
-    @property
-    def rootPassword(self):
-        return self.target.rootPassword
-
-    @property
-    def sshRetries(self):
-        return self.target.sshRetries
-
-    @sshRetries.setter
-    def sshRetries(self, value):
-        self.target.sshRetries = value
-
-    @property
-    def sshLimitRetries(self):
-        return self.target.sshLimitRetries
-
-    @sshLimitRetries.setter
-    def sshLimitRetries(self, value):
-        self.target.sshLimitRetries = value
+    def __setattr__ (self,name,value):
+        if (name=='target'): # exception for the special 'target' member
+            self.__dict__[name] = value
+        else:
+            setattr(self.target, name, value)
 
     def typCommand(self, command):
         # TODO: Better endsWith (will this even work for freebsd?)
         return self.target.runCommand(command,shutdownOnError=False)[1]
 
-    def runCommand(self, command, **kwargs):
-        # TODO: Are these interfaces compatable enough?
-        return self.target.runCommand(command, **kwargs)
-
     # TODO: The real reportAndExit takes a bunch of optional params that we
     # should support
     def reportAndExit(self, message, **kwargs):
         logAndExit(message, **kwargs)
-
-    def switchUser(self):
-        self.target.switchUser()
-
-    def getGdbOutput(self):
-        return self.target.getGdbOutput()
-
-    def closeSshConn(self):
-        self.target.closeSshConn()
-
-    def killSshConn(self):
-        self.target.killSshConn()
-
-    def openSshConn(self, **kwargs):
-        return self.target.openSshConn(**kwargs)
-
-    def activateEthernet(self):
-        return self.target.activateEthernet()
-
-    @property
-    def ipTarget(self):
-        return self.target.ipTarget
-
-    @property
-    def portTarget(self):
-        return self.target.portTarget
-
-    @property
-    def ipHost(self):
-        return self.target.ipHost
-
-    @property
-    def portHost(self):
-        return self.target.portHost
-
-    def readFromTarget(self, *args, **kwargs):
-        # TODO: Are these interfaces compatable enough?
-        return self.target.readFromTarget(*args, **kwargs)
-
-    def getDefaultEndWith(self):
-        return self.target.getDefaultEndWith()
-
-    def enableSshOnRoot (self):
-        return self.target.enableSshOnRoot()
-
-    def retartSshService (self):
-        return self.target.retartSshService()
-
