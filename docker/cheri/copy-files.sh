63c28f4747d7c62f1ffb66cd1003fc7cef1ad127
rtadros-Galois <rtadros@galois.com>
2020-08-28 23:25:40 +0000

    auxiliary scripts for the Ucam dockerfile

--- docker/cheri/copy-files.sh
+++ docker/cheri/copy-files.sh
@@ -1,8 +1,22 @@
 #! /usr/bin/env bash
+set -x
 
 sourceVariants=('' '-purecap' '-temporal')
+
+mntPath=/mnt/cheridisk
+sudo mkdir -p $mntPath
 
 for idx in ${!sourceVariants[@]}; do
     cp ../../SSITH-FETT-Binaries/SRI-Cambridge/osImages/qemu/kernel-cheri${sourceVariants[$idx]}.elf .
     cp ../../SSITH-FETT-Binaries/SRI-Cambridge/osImages/common/disk-image-cheri${sourceVariants[$idx]}.img.zst .
+    diskImage=./disk-image-cheri${sourceVariants[$idx]}.img
+
+    echo "Extracting sysroot from ${diskImage}"
+    unzstd ${diskImage}.zst
+    devLoop=$(sudo losetup -f --show -P ${diskImage})
+    sudo mount -t ufs -o ufstype=ufs2,loop,ro ${devLoop}p1 $mntPath
+    mkdir sysroot${sourceVariants[$idx]}
+    sudo cp -r $mntPath/lib $mntPath/usr sysroot${sourceVariants[$idx]} 
+    sudo umount $mntPath
+    sudo losetup -d $devLoop
 done
