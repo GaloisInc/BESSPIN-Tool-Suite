350e99f40d73337495893475ad8feb0c8ba23a2d
lyellread <lyellread@gmail.com>
2020-09-01 06:40:30 -0700

    Revert "Add region flag"
    This reverts commit 0a2d77b1756f4bb1c918537d567ef8e28b6d7b94.
    
    Commit 0a2d77b1756f4bb1c918537d567ef8e28b6d7b94 is moved to the branch
    aws-test-suite-enhancements-regions for progress to take place there
    and then be merged back into this branch when done.

--- ci/aws-testing-ci.py
+++ ci/aws-testing-ci.py
@@ -59,7 +59,7 @@ def main(args):
     a = AWSCredentials.from_env_vars()
 
     # Make sure that the region and format are written in ~/.aws/config
-    AWSConfig.check_write_aws_config(region=args.region)
+    AWSConfig.check_write_aws_config()
 
     log.info(f"{h}AWSCredentials gathered!")
 
@@ -196,13 +196,6 @@ if __name__ == "__main__":
         type=int,
         help="How many complete runs of all targets to make. Default 1.",
         default=1,
-    )
-    parser.add_argument(
-        "-re",
-        "--region",
-        choices=["us-east-1", "us-east-2", "us-west-1", "us-west-2", "af-south-1", "ap-east-1", "ap-south-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-north-1", "me-south-1", "sa-east-1"],
-        help="AWS Region",
-        default="us-west-2"
     )
 
     main(parser.parse_args())
