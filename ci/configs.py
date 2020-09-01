b6184794a41771a5cddb1eb6418cd9e054f75724
rtadros <rtadros@galois.com>
2020-09-01 17:34:36 +0000

    fetch the AMI ID for CI from git

--- ci/configs.py
+++ ci/configs.py
@@ -7,8 +7,6 @@
     Each "values" should be a tuple. Please note that a 1-element tuple should be: ('element',)
 """
 
-fettTargetAMI = 'ami-0ce567dfc18da2d2d' #fett-target-centos-080320-eb048a4 - ami-0ce567dfc18da2d2d / FETT AMI eb048a49a41540da619eafab3aa9d9b38defd899
-
 ciAWSqueue = 'https://sqs.us-west-2.amazonaws.com/845509001885/ssith-fett-target-ci-develop-pipeline-PipelineSQSQueue-1IOF3D3BU1MEP.fifo'
 ciAWSbucket = 'ssith-fett-target-ci-develop'
 ciAWSqueueTesting = 'https://sqs.us-west-2.amazonaws.com/363527286999/aws-test-suite-queue.fifo'
