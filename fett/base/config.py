63f7fe6afac2917f9be53616916a2163559f5cea
Ramy Tadros <rtadros@galois.com>
2020-09-04 10:36:51 -0700

    enable P3s

--- fett/base/config.py
+++ fett/base/config.py
@@ -53,18 +53,16 @@ def loadConfiguration(configFile):
         loadSecurityEvaluationConfiguration(xConfig,configData)
 
     # Get the XLEN and processor flavor
-    if (getSetting('processor') in ['chisel_p1', 'bluespec_p1']):
-        setSetting('xlen',32)
-    elif (getSetting('processor') in ['chisel_p2', 'bluespec_p2']):
-        setSetting('xlen',64)
+    procMatch = re.match(r"^(?P<procFlavor>chisel|bluespec)_p(?P<pType>[1-3])$",getSetting('processor'))
+    if (procMatch):
+        setSetting('procFlavor', procMatch.group('procFlavor'))
+        if (procMatch.group('pType') == '1'):
+            setSetting('xlen',32)
+        else: # 2-3
+            setSetting('xlen',64)
     else:
-        logAndExit(f"Failed to determine xlen from <{getSetting('processor')}>.",exitCode=EXIT.Dev_Bug)
-    if (getSetting('processor') in ['chisel_p1', 'chisel_p2']):
-        setSetting('procFlavor', 'chisel')
-    elif (getSetting('processor') in ['bluespec_p1', 'bluespec_p2']):
-        setSetting('procFlavor', 'bluespec')
-    else:
-        logAndExit(f"Failed to determine the processor flavor <chisel or bluespec>.",exitCode=EXIT.Dev_Bug)
+        logAndExit(f"Failed to determine the processor flavor and xlen for <{getSetting('processor')}>.",exitCode=EXIT.Dev_Bug)
+
     # Create an isUnix setting
     setSetting('isUnix',getSetting('osImage') in ['debian', 'FreeBSD', 'busybox'])
 
