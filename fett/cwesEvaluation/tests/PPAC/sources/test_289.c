eb8c3c6e3f0af3558de4e9f4b8b9bf561761843c
Ramy Tadros <rtadros@galois.com>
2020-09-17 10:35:50 -0700

    modify the PPAC tests to follow the network updates

--- fett/cwesEvaluation/tests/PPAC/sources/test_289.c
+++ fett/cwesEvaluation/tests/PPAC/sources/test_289.c
@@ -27,6 +27,9 @@ NONE
     
     void main (void) {
         BaseType_t funcReturn;
+
+        startNetwork();
+
         funcReturn = xTaskCreate(vTask289, "main:task289", configMINIMAL_STACK_SIZE * STACKSIZEMUL, NULL, xMainPriority, NULL);
         if (funcReturn != pdPASS) {
             onPrintf ("<INVALID> Failed to create <main:task289>. [ret=%d].\n",funcReturn);
@@ -35,7 +38,6 @@ NONE
             onPrintf (">>> [main:] Created <main:task289>.\n");
         }
 
-        vTaskStartScheduler(); //Hang the function
         return;
     }
 
@@ -46,14 +48,6 @@ NONE
         xMainTask = xTaskGetCurrentTaskHandle();
 
         // -------------------------------- START NETWORK ------------------------------------------
-        funcReturn = xTaskCreate(vStartNetwork, "taskTest:startNetwork", configMINIMAL_STACK_SIZE * STACKSIZEMUL, NULL, xMainPriority, NULL);
-        if (funcReturn != pdPASS) {
-            onPrintf ("<INVALID> [main:] Failed to create <taskTest:startNetwork>. [ret=%d].\n",funcReturn);
-            vEXIT(1);
-        } else {
-            onPrintf (">>> [main:] Created <taskTest:startNetwork>.\n");
-        }
-
         recvNotification = NOTIFY_FAIL;
         funcReturn = xTaskNotifyWait(0xffffffff, 0, &recvNotification, pdMS_TO_TICKS(20000)); //it usually takes 10-15 seconds
         if ((funcReturn != pdPASS) || (recvNotification != NOTIFY_SUCCESS)) {
@@ -62,6 +56,9 @@ NONE
         } else {
             onPrintf (">>> [main:] Network started successfully!\n");
         }
+
+        onPrintf ("\n<NTK-READY>\n");
+        vTaskDelay(pdMS_TO_TICKS(3000)); //give time to the host to ping
 
         #ifdef USE_TLS_OVER_TCP 
             // ------------------------------- START WOLFSSL -------------------------------------------
