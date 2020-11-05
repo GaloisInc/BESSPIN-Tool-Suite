29ea8c840a37e7e2cda3f0d42dd689f45622e343
rtadros <rtadros@galois.com>
2020-11-05 08:13:20 -0800

    updates to accommodate for busybox

--- fett/target/vcu118.py
+++ fett/target/vcu118.py
@@ -380,6 +380,10 @@ class vcu118Target (fpgaTarget, commonTarget):
     @decorate.timeWrap
     def buildSmokeElfForUartSearch(self,hwId):
         freeRTOSBuildChecks(targetId=self.targetId)
+        if (not doesSettingExist('buildDir',targetId=self.targetId)): #In case of busybox for instance
+            buildDir = os.path.join(getSetting('workDir'), f'build{self.targetSuffix}')
+            mkdir(buildDir)
+            setSetting('buildDir',buildDir,targetId=self.targetId)
         buildDir = os.path.join(getSetting('buildDir',targetId=self.targetId),'buildSmokeElfForUart')
         mkdir(buildDir)
 
