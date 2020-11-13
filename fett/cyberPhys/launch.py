d02ec9247bb96de210fcea4888a6541023b9ac43
rtadros <rtadros@galois.com>
2020-11-13 10:27:07 -0800

    create a new threadControl file + always create the locks and semaphores

--- fett/cyberPhys/launch.py
+++ fett/cyberPhys/launch.py
@@ -7,27 +7,12 @@ from fett.base.utils.misc import *
 import fett.target.launch
 import fett.cyberPhys.interactive
 import fett.cyberPhys.run
+from fett.base.threadControl import ftQueueUtils
 import threading, queue
 
 @decorate.debugWrap
 @decorate.timeWrap
 def startCyberPhys():
-    # Create a network lock to protect network operations while multithreading
-    setSetting('networkLock',threading.Lock())
-    # Create the locks needed for vcu118 board(s)
-    """
-    For VCU118, we're using a conservative approach for the sake of robustness;
-    The following operations are all protected by a single lock (openocdLock):
-    - clear flash
-    - program bitfile
-    - start an openocd process
-    - gdb connect to openocd [(gdb) target remote localhost:<gdbPort>]
-    """
-    setSetting('setupUartLock',threading.Lock())
-    setSetting('openocdLock',threading.Lock())
-    # Create a lock for using the FreeRTOS submodule directory or FreeRTOS general settings
-    setSetting('FreeRTOSLock',threading.Lock())
-
     printAndLog (f"Launching FETT <cyberPhys mode>...")
     # start/prepareEnv/Launch
     runThreadPerTarget(fett.target.launch.startFett)
@@ -124,22 +109,6 @@ def runThreadPerTarget(func, tArgs=(), tKwargs=None, addTargetIdToKwargs=True,
             xThread.join()
 
     return xThreads
-
-@decorate.debugWrap
-@decorate.timeWrap
-def ftQueueUtils(queueName,xQueue,method,timeout=None,itemToPut=True,exitFunc=logAndExit):
-    if(method=='put'):
-        try:
-            return xQueue.put(itemToPut, block=True, timeout=timeout)
-        except Exception as exc:
-            exitFunc(f"Failed to put <{itemToPut}> to <{queueName}>.",exc=exc,exitCode=EXIT.Dev_Bug)
-    elif(method=='get'):
-        try:
-            return xQueue.get(block=True, timeout=timeout)
-        except Exception as exc:
-            exitFunc(f"Failed to get from <{queueName}>.",exc=exc,exitCode=EXIT.Dev_Bug)
-    else:
-        exitFunc(f"<ftQueueUtils> not implemented for method<{method}>.",exitCode=EXIT.Implementation)
 
 @decorate.debugWrap
 def startUartPiping(targetId):
