bd95d62203763a097c33ac7ec04f63feb7c4178b
Ramy Tadros <rtadros@galois.com>
2020-11-16 16:30:13 -0800

    first implementation of test 1252

--- fett/cwesEvaluation/hardwareSoC/cweTests/test_1252.py
+++ fett/cwesEvaluation/hardwareSoC/cweTests/test_1252.py
@@ -9,17 +9,63 @@ def test_1252 (target,binTest):
     if (not target.hasGdbAccess()):
         target.terminateAndExit(f"<test_{testNum}> requires GDB access!",exitCode=EXIT.Dev_Bug)
 
+    outLog = ""
     with vulClassTester.inGdbTest(target):
-        if (target.xlen==32):
-            outLog = ""
-        elif (target.xlen==64):
-            outLog = ""
+        allRegs = target.getAllRegsValues()
 
-    if (target.osImage in ['debian','FreeBSD']):
-        outLog += target.runCommand(" ")[1]
-    elif (target.osImage == 'FreeRTOS'):
-        outLog += target.endFreeRTOSTest()
-    else:
-        target.terminateAndExit(f"<test_{testNum}> is not implemented for <{target.osImage}>.",exitCode=EXIT.Dev_Bug)
+        #Check that we now have all the PMP regs values
+        pmpRegs = set([f'pmpcfg{iCfg}' for iCfg in range(4)] + [f'pmpaddr{iCfg}' for iCfg in range(16)])
+        if (len(pmpRegs - set(allRegs))>0): #missing regs
+            errorAndLog("test_1252: Failed to find the values of the following regs: "
+                f"<{','.join(pmpRegs-set(allRegs))}>")
+            outLog += "<INVALID> Missing registers values."
+        else:
+            bCfgPerAddr = {} #this dict will carry the 8-bits CFGs of the pmpaddr0-15
+            if (target.xlen==32):
+                for iCfg in range(4):
+                    mask = int(0xff)
+                    for iByte in range(4):
+                        bCfgPerAddr[iCfg*4+iByte] = (allRegs[f'pmpcfg{iCfg}'] & mask) >> (iByte*8)
+                        mask <<= 8
+            elif (target.xlen==64):
+                for iCfg in range(2):
+                    mask = int(0xff)
+                    for iByte in range(8):
+                        bCfgPerAddr[iCfg*8+iByte] = (allRegs[f'pmpcfg{iCfg*2}'] & mask) >> (iByte*8)
+                        mask <<= 8
+
+            for iCfg in range(16): #looping through the 16 possible regions
+                outLog += f"<cfg{iCfg}>: "
+                
+                A = (int(0x18) & bCfgPerAddr[iCfg]) >> 3
+                X = (int(0x4) & bCfgPerAddr[iCfg]) >> 2
+                W = (int(0x2) & bCfgPerAddr[iCfg]) >> 1
+                R = int(0x1) & bCfgPerAddr[iCfg]
+
+                if (A==0):
+                    outLog += f"<Disabled>\n"
+                    continue
+                elif (A==1): #TOR (Top Of Range)
+                    if (    ((iCfg==0) and (allRegs[f'pmpaddr{iCfg}']>0)) or
+                            ((iCfg>0) and (allRegs[f'pmpaddr{iCfg-1}'] < allRegs[f'pmpaddr{iCfg}']))
+                        ):
+                        outLog += f"<TOR> - "
+                    else:
+                        outLog += f"<TOR> - <Bad-Range>\n"
+                        continue
+                elif (A==2):
+                    outLog += f"<NA4> - "
+                elif (A==3):
+                    outLog += f"<NAPOT> - "
+
+                if (X==0):
+                    outLog += "<EXECUTION>\n"
+                elif (W==0):
+                    outLog += "<WRITE>\n"
+                elif (R==0):
+                    outLog += "<READ>\n"
+                else:
+                    outLog += "<Non-Protected>\n"
 
     return outLog
+
