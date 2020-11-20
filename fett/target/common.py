2cebde50b185c1e04960dea57a0b14d7444091c7
rtadros <rtadros@galois.com>
2020-11-20 22:46:14 +0000

    gdb on connectal too

--- fett/target/common.py
+++ fett/target/common.py
@@ -1328,7 +1328,7 @@ class commonTarget():
     def hasGdbAccess (self):
         return (
             (self.target=='vcu118') or
-            ((self.target=='awsf1') and (self.pvAWS=='firesim'))
+            ((self.target=='awsf1') and (self.pvAWS in ['firesim', 'connectal']))
             )
 
     @decorate.debugWrap
