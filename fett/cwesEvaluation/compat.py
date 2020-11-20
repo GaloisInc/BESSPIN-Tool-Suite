839a1108e63537ca0427c9ae08a9aaf1231713ba
Ramy Tadros <rtadros@galois.com>
2020-11-20 10:38:49 -0800

    BUG: the 'target' member was used as two different members

--- fett/cwesEvaluation/compat.py
+++ fett/cwesEvaluation/compat.py
@@ -5,20 +5,20 @@ class testgenTargetCompatibilityLayer:
     This class wraps a FETT target and presents a testgen target interface
     """
     def __init__(self, target):
-        self.target = target
+        self.objTarget = target
         self.osImage = getSetting("osImage")
 
     def __getattr__ (self,attr):
-        return getattr(self.target,attr)
+        return getattr(self.objTarget,attr)
 
     def __setattr__ (self,name,value):
-        if (name=='target'): # exception for the special 'target' member
+        if (name=='objTarget'): # exception for the special 'objTarget' member
             self.__dict__[name] = value
         else:
-            setattr(self.target, name, value)
+            setattr(self.objTarget, name, value)
 
     def typCommand(self, command): #Note that this returns the `textBack`
-        return self.target.runCommand(command,exitOnError=False)[1]
+        return self.objTarget.runCommand(command,exitOnError=False)[1]
 
     def isTestInfoEnabled (self,testName,infoName):
         return isTestInfoEnabled(self.vulClass,testName,infoName)
