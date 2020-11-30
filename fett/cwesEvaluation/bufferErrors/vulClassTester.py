be44682534c53afffce6e0c0590fb85a2609958b
Brett Boston <bboston7@users.noreply.github.com>
2020-11-30 09:05:32 -0800

    Trap SIGSEGV in debian bof error tool (#882)
    Closes #869
    
    This change registers a signal handler for SIGSEGV in the debian buffer
    error tests to prevent interleaving of OS debug messages with test
    output.  The signal handler causes the test to exit with return code 11
    (the signal number for SIGSEGV) on a segmentation fault, which fett then
    detects and augments the test output with the string "Segmentation
    fault" so that the test is properly scored as TRAPPED.

--- fett/cwesEvaluation/bufferErrors/vulClassTester.py
+++ fett/cwesEvaluation/bufferErrors/vulClassTester.py
@@ -3,6 +3,7 @@
 This file has the custom bufferError methods to run tests on qemu|fpga.
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # """
 
+from fett.base.utils.misc import *
 from fett.cwesEvaluation.compat import testgenTargetCompatibilityLayer
 
 class vulClassTester(testgenTargetCompatibilityLayer):
@@ -12,4 +13,15 @@ class vulClassTester(testgenTargetCompatibilityLayer):
         return
 
     def executeTest(self, binTest):
-        return self.typCommand(f"./{binTest}")
+        if isEqSetting("osImage", "debian"):
+            # Run test, and if it returns 11, then echo "Segmentation fault".
+            # This is needed because the debian buffer errors tests catch
+            # SIGSEGV to prevent the kernel from printing debug info that can
+            # interleave with test output.  A side effect of catching SIGSEGV
+            # is that the normal "Segmentation fault" message is not printed,
+            # so we must print it as part of the run command.
+            return self.typCommand(f"./{binTest} ; "
+                                   "[ $? -eq 11 ] && "
+                                   "echo 'Segmentation fault'")
+        else:
+            return self.typCommand(f"./{binTest}")
