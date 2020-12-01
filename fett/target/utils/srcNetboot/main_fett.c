00c625b1c7007521a70c2256e3de084a10d30ec6
Ramy Tadros <rtadros@galois.com>
2020-12-01 14:13:27 -0800

    why did I add this function? -- there is no need if we include the right header

--- fett/target/utils/srcNetboot/main_fett.c
+++ fett/target/utils/srcNetboot/main_fett.c
@@ -5,31 +5,7 @@
 
 void main_fett (void);
 extern void main_netboot(void);
-uint32_t ulApplicationGetNextSequenceNumber(uint32_t ulSourceAddress,
-                                            uint16_t usSourcePort,
-                                            uint32_t ulDestinationAddress,
-                                            uint16_t usDestinationPort);
 
 void main_fett () {
     main_netboot();
-}
-
-/*
---- This function is called by FreeRTOS_TCP_IP.c. This is a dirty implementation
- * Callback that provides the inputs necessary to generate a randomized TCP
- * Initial Sequence Number per RFC 6528.  THIS IS ONLY A DUMMY IMPLEMENTATION
- * THAT RETURNS A PSEUDO RANDOM NUMBER SO IS NOT INTENDED FOR USE IN PRODUCTION
- * SYSTEMS.
- */
-uint32_t ulApplicationGetNextSequenceNumber(uint32_t ulSourceAddress,
-                                            uint16_t usSourcePort,
-                                            uint32_t ulDestinationAddress,
-                                            uint16_t usDestinationPort)
-{
-    (void)ulSourceAddress;
-    (void)usSourcePort;
-    (void)ulDestinationAddress;
-    (void)usDestinationPort;
-
-    return uxRand();
 }
