7737ea396683170c960edbedd78a05de619f8766
Ramy Tadros <rtadros@galois.com>
2020-12-01 13:31:39 -0800

    update the srcNetboot to work on the upstream mirror

--- fett/target/utils/srcNetboot/envFett.mk
+++ fett/target/utils/srcNetboot/envFett.mk
@@ -4,15 +4,16 @@
 CFLAGS += -DFETT_APPS
 INCLUDES += -I$(INC_FETT_APPS)
 
-# main_netboot specific files
+# main_netboot specific additions
 PORT_ASM += demo/netboot.S
 INCLUDES += $(FREERTOS_IP_INCLUDE)
 FREERTOS_SRC += $(FREERTOS_IP_SRC)
+configCPU_CLOCK_HZ=100000000
+OPT = -O2
+CFLAGS += -DNETBOOT
 
 # C Source files
 DEMO_SRC = main.c \
 	$(INC_FETT_APPS)/main_fett.c \
 	demo/main_netboot.c
 
-configCPU_CLOCK_HZ=100000000
-OPT = -O2
