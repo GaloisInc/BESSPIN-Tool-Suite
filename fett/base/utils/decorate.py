f79435ba69f03a223bb28d4f6b6f92a585480269
rtadros <rtadros@galois.com>
2020-12-08 09:43:46 -0800

    targetId info in the debugging wrappers if possible

--- fett/base/utils/decorate.py
+++ fett/base/utils/decorate.py
@@ -3,6 +3,9 @@
 import logging, time, sys
 from functools import wraps
 
+def getTargetInfo (targetId=None, **kwargs):
+    return '' if (targetId is None) else f"<target{targetId}>: "
+
 def debugWrap (func):
     @wraps(func)
     def wrappedFn(*args, **kwargs):
@@ -10,10 +13,10 @@ def debugWrap (func):
             caller = sys._getframe(1).f_code.co_name
         except:
             caller = 'unknown-caller'
-        logging.debug(f"Entering <{func.__name__}>. [called from <{caller}>]")
-        #logging.debug(f">>>> args={args}, kwargs={kwargs}") #super-duper debug
+        logging.debug(f"{getTargetInfo(**kwargs)}Entering <{func.__name__}>. [called from <{caller}>]")
+        #logging.debug(f"{getTargetInfo(**kwargs)}>>>> args={args}, kwargs={kwargs}") #super-duper debug
         ret = func(*args, **kwargs)
-        logging.debug(f"Exitting <{func.__name__}>.")
+        logging.debug(f"{getTargetInfo(**kwargs)}Exitting <{func.__name__}>.")
         return ret
     return wrappedFn
 
@@ -23,6 +26,7 @@ def timeWrap(func):
         startTime = time.time()
         ret = func(*args, **kwargs)
         minutes, seconds = divmod(time.time() - startTime, 60)
-        logging.debug("[Time Elapsed in <{}>]={:0>2}:{:0>2}".format(func.__name__,int(minutes),int(seconds)))
+        logging.debug("{}[Time Elapsed in <{}>]={:0>2}:{:0>2}".format(getTargetInfo(**kwargs),func.__name__,
+                                                                    int(minutes),int(seconds)))
         return ret
     return wrappedFn
