e33c285fbe71ff71fcf41561db869582b9c2f5f5
Brett Boston <boston@galois.com>
2020-12-09 15:29:10 -0800

    Consistently use getSettingDict in buffer errors test generator

--- fett/cwesEvaluation/bufferErrors/generateTests/generateTests.py
+++ fett/cwesEvaluation/bufferErrors/generateTests/generateTests.py
@@ -23,21 +23,20 @@ def parseBytes(sz):
                    exitCode=EXIT.Configuration)
 
 def generateTests(outdir):
-    settings = getSetting('bufferErrors')
     # Check that nTests is large enough to avoid score instability
     minNTests = 40 if isEqSetting('osImage', 'FreeRTOS') else 100
-    if settings['nTests'] < minNTests:
+    if getSettingDict('bufferErrors', 'nTests') < minNTests:
         warnAndLog(f"<generateTests> <nTests> must be at least <{minNTests}> "
                    f"for <{getSetting('osImage')}>.  Changing <nTests> to "
                    f"<{minNTests}>.")
-        settings['nTests'] = minNTests
+        setSettingDict('bufferErrors', 'nTests', minNTests)
 
     bufferErrorsDir = os.path.join(getSetting("repoDir"),
                                    "fett",
                                    "cwesEvaluation",
                                    "bufferErrors")
-    modelPath = (settings['pathToCustomErrorModel'] if
-                 settings['useCustomErrorModel'] else
+    modelPath = (getSettingDict('bufferErrors', 'pathToCustomErrorModel') if
+                 getSettingDict('bufferErrors', 'useCustomErrorModel') else
                  os.path.join(bufferErrorsDir, "BufferErrors.cfr"))
     try:
         model = featureModelUtil.loadFM(modelPath)
@@ -49,7 +48,8 @@ def generateTests(outdir):
     # Prune out other vulnerability classes
     model = featureModelUtil.splitFM(model, ["BufferErrors_Test"])[0]
     instancePath = os.path.join(bufferErrorsDir, "CachedInstances.json")
-    if settings['useCachedInstances'] and not settings['useCustomErrorModel']:
+    if (getSettingDict('bufferErrors', 'useCachedInstances') and
+        not getSettingDict('bufferErrors', 'useCustomErrorModel')):
         printAndLog("<generateTests> Loading cached instances "
                     f"<{instancePath}>")
         enumeratedFM = safeLoadJsonFile(instancePath)
@@ -59,31 +59,33 @@ def generateTests(outdir):
         # Make sure that the buffer error test is actually enabled
         model = featureModelUtil.addConstraints(model, ["BufferErrors_Test"])
         enumeratedFM = featureModelUtil.enumerateFM(model)
-        if not settings['useCustomErrorModel']:
+        if not getSettingDict('bufferErrors', 'useCustomErrorModel'):
             printAndLog(f"<generateTests> Caching instances to <{instancePath}>")
             safeDumpJsonFile(enumeratedFM, instancePath)
         printAndLog("<generateTests> Done generating instances")
     instances = [BofInstance(model, x) for x in enumeratedFM]
-    heapSize = parseBytes(settings['heapSize'])
-    stackSize = parseBytes(settings['stackSize'])
-    seed = (settings['seed'] if settings['useSeed']
-                             else random.randrange(sys.maxsize))
+    heapSize = parseBytes(getSettingDict('bufferErrors', 'heapSize'))
+    stackSize = parseBytes(getSettingDict('bufferErrors', 'stackSize'))
+    seed = (getSettingDict('bufferErrors', 'seed') if
+            getSettingDict('bufferErrors', 'useSeed') else
+            random.randrange(sys.maxsize))
     printAndLog(f"<generateTests> Using seed {seed}")
 
     tgs = [ (i, BofTestGen(i, heapSize, stackSize)) for i in instances ]
     rnd = random.Random(seed)
 
-    printAndLog(f"<generateTests> Generating {settings['nTests']} tests "
+    printAndLog("<generateTests> Generating "
+                f"{getSettingDict('bufferErrors', 'nTests')} tests "
                 f"with seed={seed}, heap-size={heapSize}, "
                 f"stack-size={stackSize}")
-    if settings["nSkip"]:
-        printAndLog("<generateTests> Skipping {settings['nSkip']} instances")
-        for i in range(settings["nSkip"]):
+    if getSettingDict('bufferErrors', "nSkip"):
+        printAndLog("<generateTests> Skipping {getSettingDict('bufferErrors', 'nSkip')} instances")
+        for i in range(getSettingDict('bufferErrors', "nSkip")):
             tg = rnd.choice(tgs)
             tg.genInstance(rnd, drop=True)
 
     selector = InstanceSelector(tgs, rnd)
-    for i in range(settings['nTests']):
+    for i in range(getSettingDict('bufferErrors', 'nTests')):
         # Name tests test_buffer_error_<i>.c to avoid conflicting with the
         # test_<i>.c tests in the other vulnerability classes
         out   = os.path.join(outdir, f"test_buffer_errors_{i}.c")
