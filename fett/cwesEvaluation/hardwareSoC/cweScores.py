0af6087e885701b00ef56d1495c7e7e5e5018921
rtadros <rtadros@galois.com>
2020-12-09 16:26:44 -0800

    implement the self assessment flow

--- fett/cwesEvaluation/hardwareSoC/cweScores.py
+++ fett/cwesEvaluation/hardwareSoC/cweScores.py
@@ -3,25 +3,8 @@ scoring function for hardwareSoC
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # """
 
 from fett.base.utils.misc import *
-from fett.cwesEvaluation.scoreTests import SCORES, adjustToCustomScore
-from fett.cwesEvaluation.hardwareSoC.customCweScores import *
-from fett.cwesEvaluation.utils.scoringAux import defaultRecommendationText
+from fett.cwesEvaluation.utils.scoringAux import defaultSelfAssessmentScoreAllTests
 
 @decorate.debugWrap
 def scoreAllTests(logs):
-    ret = []
-    for name, log in logs:
-        dispName = name.replace('_','-').upper()
-        if (isTestInfoEnabled("hardwareSoC",name,"recommendation")):
-            ret.append(defaultRecommendationText(dispName, log))
-            continue
-        #else: custom score
-        logText = ftReadLines(log,splitLines=False)
-        try:
-            testScorerFunc = getattr(globals()[name],name)
-        except Exception as exc:
-            errorAndLog(f"scoreAllTests-hardwareSoC: Could not locate the scorer function for <{name}>",exc=exc)
-            ret.append([dispName, SCORES.FAIL, "Failed to Score!"])
-            continue
-        ret.append(testScorerFunc(dispName,logText))
-    return ret
+    return defaultSelfAssessmentScoreAllTests("hardwareSoC", logs)
--- fett/cwesEvaluation/hardwareSoC/vulClassTester.py
+++ fett/cwesEvaluation/hardwareSoC/vulClassTester.py
@@ -4,70 +4,11 @@ This file has the custom hardwareSoC methods for runTests
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # """
 
 from fett.cwesEvaluation.compat import testgenTargetCompatibilityLayer
-import fett.cwesEvaluation.hardwareSoC.cweTests as cweTests
+
 from fett.base.utils.misc import *
-import pexpect
 
 class vulClassTester(testgenTargetCompatibilityLayer):
     def __init__(self, target):
         super().__init__(target)
         self.vulClass = "hardwareSoC"
         return
-
-    def executeTest (self,binTest):
-        outLog = ''
-        try:
-            testName = binTest.split('.')[0]
-            testNum = int(testName.split('_')[1])
-        except Exception as exc:
-            self.terminateAndExit (f"executeTest: Failed to parse <{binTest}>.",exc=exc,exitCode=EXIT.Dev_Bug)
-
-        outLog = "\n" + '*'*30 + f" TEST {testNum} " + '*'*30 + "\n\n"
-        outLog += f"<OSIMAGE={self.osImage}>\n"
-        outLog += f"<XLEN={self.xlen}>\n"
-
-        if (self.isTestInfoEnabled(testName,'gdb') and (not self.hasGdbAccess())):
-            warnAndLog (f"Test <{testNum}> requires GDB access!")
-            outLog += f"<NOGDB>\n"
-            return outLog
-
-        if (self.isTestInfoEnabled(testName,'recommendation')):
-            outLog += self.recommendationTest (testName)
-        elif (self.isTestInfoEnabled(testName,'hasMethod')):
-            if (hasattr(cweTests,testName)):
-                outLog += getattr(getattr(cweTests,testName),testName)(self,binTest)
-            else:
-                self.terminateAndExit (f"Calling unknown method <{testName}>.",exitCode=EXIT.Dev_Bug)
-        else:
-            outLog += self.defaultTest(testNum, binTest)
-
-        return outLog
-
-    def endFreeRTOSTest (self):
-        textBack,wasTimeout,idxReturn = self.expectFromTarget([">>>End of Fett<<<", pexpect.EOF],
-                    "End FreeRTOS Test", exitOnError=False, timeout=getSetting('FreeRTOStimeout'))
-        if (idxReturn == 1):
-            self.terminateAndExit("<runTests> Unexpected EOF during test run.", exitCode=EXIT.Dev_Bug)
-        elif (wasTimeout):
-            textBack += "\n<TIMEOUT>\n"
-
-        return textBack
-
-class inGdbTest: 
-    def __init__(self,target):
-        self.target = target
-          
-    def __enter__(self): 
-        # Pause the OS
-        self.target.interruptGdb()
-        return self
-      
-    def __exit__(self, exc_type, exc_value, exc_traceback): 
-        # Resume the OS
-        self.target.continueGdb()
-        if (self.target.osImage in ['debian','FreeBSD']):
-            self.target.runCommand(" ")
-        elif (self.target.osImage == 'FreeRTOS'):
-            self.target.endFreeRTOSTest()
-        else:
-            self.target.terminateAndExit(f"<inGdbTest> is not implemented for <{self.target.osImage}>.",exitCode=EXIT.Dev_Bug)
