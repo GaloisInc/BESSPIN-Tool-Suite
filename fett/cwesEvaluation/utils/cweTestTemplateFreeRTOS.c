a09accea779ddb7048111ce521ee8d275cb5d4a1
Ramy Tadros <rtadros@galois.com>
2020-12-23 08:30:25 -0800

    run CWE tests on FreeRTOS in a task instead of pre-scheduler

--- fett/cwesEvaluation/utils/cweTestTemplateFreeRTOS.c
+++ fett/cwesEvaluation/utils/cweTestTemplateFreeRTOS.c
@@ -5,6 +5,13 @@
 
 #include <stdio.h>
 
+#ifdef testgenFPGA
+    #include <FreeRTOS.h>
+    #include <task.h>
+
+    void vMain (void *pvParameters);
+#endif
+
 void main_fett (void);
 
 extern MAIN_DECLARATION;
@@ -13,7 +20,41 @@ void main_fett (void)
 {
     printf (">>>Beginning of Fett<<<\n");
 
-    main_TEST_NAME ();
+    #ifdef testgenFPGA
+        // Execute tests within a task to accommodate for security features that gets enabled with the scheduler
+        BaseType_t funcReturn = xTaskCreate(vMain,
+                                            "main:vMain",
+                                            configMINIMAL_STACK_SIZE * 10,
+                                            NULL,
+                                            tskIDLE_PRIORITY + 1,
+                                            NULL);
 
-    printf (">>>End of Fett<<<\n");
+        if (funcReturn == pdPASS)
+        {
+            printf ("(INFO)~  main_fett: vMain task created.\n");
+        } else {
+            printf ("<INVALID>\n(ERROR)~ main_fett: Failed to create vMain task.\n");
+        }
+    #elif testgenQEMU
+        main_TEST_NAME ();
+
+        printf (">>>End of Fett<<<\n");
+    #endif
+    
+    return;    
 }
+
+#ifdef testgenFPGA
+    //This is the entry point task
+    void vMain (void *pvParameters) {
+        (void) pvParameters;
+
+        printf ("(INFO)~ Beginning of Main Task.\n");
+
+        main_TEST_NAME ();
+
+        printf (">>>End of Fett<<<\n");
+
+        vTaskDelete(NULL);
+    }
+#endif
