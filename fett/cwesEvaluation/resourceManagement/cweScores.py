1932036b35b6dd5e3dd10bd7dba8d14869bd22c5
Ramy Tadros <rtadros@galois.com>
2020-12-24 13:09:09 -0800

    add neg scoring to RM cweScores

--- fett/cwesEvaluation/resourceManagement/cweScores.py
+++ fett/cwesEvaluation/resourceManagement/cweScores.py
@@ -65,33 +65,44 @@ def defaultScoreTest(testName, logLines, testsInfoSection):
 
     scoreOptions = [SCORES.CALL_ERR, SCORES.HIGH, SCORES.MED, SCORES.LOW, SCORES.NONE]
 
-    def scoreToKey(xScore):
-        return f"{str(xScore).lower()}Keywords"
+    def scoreToKey(xScore, negate=False):
+        if (negate):
+            return f"NEG-{str(xScore).lower()}Keywords"
+        else:
+            return f"{str(xScore).lower()}Keywords"
 
     # Collect goldenKeywords
     goldenKeywords = defaultdict(list)
-    for xScore in scoreOptions:
-        key = scoreToKey(xScore)
-        if doesSettingExistDict(VULCLASS,[testsInfoSection,testName,key]):
-            curDict = deepcopy(getSettingDict(VULCLASS,[testsInfoSection,testName,key]))
-            for setting in ["osImage", "target"]:
-                if (getSetting(setting) in curDict):
-                    curDict = curDict[getSetting(setting)]
-                elif ("any" in curDict):
-                    curDict = curDict["any"]
-                else:
-                    logAndExit(f"defaultScoreTest: Failed to parse the <{setting}> keywords for <{testName}>.",exitCode=EXIT.Dev_Bug)
-            goldenKeywords[key] = curDict
+    for negate in [False, True]:
+        for xScore in scoreOptions:
+            key = scoreToKey(xScore,negate=negate)
+            if doesSettingExistDict(VULCLASS,[testsInfoSection,testName,key]):
+                curDict = deepcopy(getSettingDict(VULCLASS,[testsInfoSection,testName,key]))
+                for setting in ["osImage", "target"]:
+                    if (getSetting(setting) in curDict):
+                        curDict = curDict[getSetting(setting)]
+                    elif ("any" in curDict):
+                        curDict = curDict["any"]
+                    else:
+                        logAndExit(f"defaultScoreTest: Failed to parse the <{setting}> keywords for <{testName}>.",exitCode=EXIT.Dev_Bug)
+                goldenKeywords[key] = curDict
 
     goldenKeywords[scoreToKey(SCORES.CALL_ERR)] += ["<INVALID>"] #INVALID is common
     relvKeywords =  [levelKeyword for levelKeywords in goldenKeywords.values() for levelKeyword in levelKeywords]
     scoredKeywords = doKeywordsExistInText('\n'.join(logLines),relvKeywords)
 
     def scorePart(partLogLines):
-        for xScore in scoreOptions:
-            for goldenKeyword in goldenKeywords[scoreToKey(xScore)]:
-                if (scoredKeywords[goldenKeyword]):
-                    return xScore
+        """
+        for each score, starting with the worst --> best; do
+            if one of the golden keywords exist --> return this score
+            if one of the golden NEG-keywors does not exist --> return this score
+        done
+        """
+        for xScore in scoreOptions:   
+            for negate in [False, True]:
+                for goldenKeyword in goldenKeywords[scoreToKey(xScore,negate=negate)]:
+                    if (negate ^ scoredKeywords[goldenKeyword]):
+                        return xScore
         return SCORES.FAIL
 
     if (nParts == 1):
