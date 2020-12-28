1b5a27efd3f3b803fcc2f0f8f5dfdbe01c6eb8bd
Ramy Tadros <rtadros@galois.com>
2020-12-28 12:08:27 -0800

    add test 467

---
+++ fett/cwesEvaluation/resourceManagement/sources/test_467.c
@@ -0,0 +1,66 @@
+#include "testsParameters.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define MALLOC_SIZE_MAX 0x00FF
+#define MALLOC_SIZE_MIN 0x0000
+
+#if (defined(testgenOnFreeRTOS) && defined(testgenFPGA))
+    #include "FreeRTOS.h"
+    #define MALLOC pvPortMalloc
+    #define FREE vPortFree
+#else
+    #define MALLOC malloc
+    #define FREE free
+#endif
+
+static void sizeofPtr (void); 
+
+// --------------- FreeRTOS Test ---------------
+#ifdef testgenOnFreeRTOS
+    #define NUM_OF_TEST_PARTS 1
+
+    void main() {
+        printf("\n<OSIMAGE=FreeRTOS>\n");
+        #if TESTGEN_TEST_PART == 1
+            sizeofPtr();
+            printf("\n<END-OF-MAIN>\n");
+        #else
+            printf("\n<INVALID> Part[%d] not in [1,%d].\n",TESTGEN_TEST_PART,NUM_OF_TEST_PARTS);
+        #endif
+        return;
+    }
+
+// --------------- Debian && FreeBSD test ---------------
+#elif (defined(testgenOnDebian) || defined(testgenOnFreeBSD))
+    int main(void);
+
+    int main() {
+        sizeofPtr();
+        printf("\n<END-OF-MAIN>\n");
+        return 0;
+    }
+
+#endif //ifdef testgenOnFreeRTOS
+
+static void sizeofPtr() {
+    long unsigned int xInt, *pInt;
+    size_t xSizeToMalloc;
+    char xChar, *pChar;
+
+    xSizeToMalloc = (size_t) ((rand() % (MALLOC_SIZE_MAX-MALLOC_SIZE_MIN+1)) + MALLOC_SIZE_MIN);
+    xChar = (char) (rand()%(256*sizeof(xChar)));
+    pChar = (char *) MALLOC(xSizeToMalloc * sizeof(pChar));
+    memset (pChar, xChar, xSizeToMalloc * sizeof(pChar));
+
+    xSizeToMalloc = (size_t) ((rand() % (MALLOC_SIZE_MAX-MALLOC_SIZE_MIN+1)) + MALLOC_SIZE_MIN);
+    xInt = (long unsigned int) (rand()%(256*sizeof(xInt)));
+    pInt = (long unsigned int *) MALLOC(xSizeToMalloc * sizeof(pInt));
+    memset (pInt, xInt, xSizeToMalloc * sizeof(pInt));
+
+    FREE(pChar);
+    FREE(pInt);
+    return;
+}
