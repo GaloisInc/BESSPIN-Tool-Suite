d5423b57e9cf7a3f97e3643b796b682af37b00be
Daniel M. Zimmerman <dmz@galois.com>
2021-01-04 13:45:29 -0800

    Some more fleshed out functions.

--- build/cyberphys/infotainment-server/source/infotainment_server.c
+++ build/cyberphys/infotainment-server/source/infotainment_server.c
@@ -17,9 +17,9 @@
 #include <sys/types.h> 
 #include <sys/socket.h> 
 
+#include "can.h"
 #include "canlib.h"
 #include "cyberphys.h"
-#include "j1939.h"
 
 #include "infotainment_defs.h"
 #include "infotainment_debug.h"
@@ -93,8 +93,10 @@ int main_loop(void) {
     // initialize socket address and buffer data structures
     struct sockaddr_in broadcast_address;
     struct sockaddr_in receive_address;
-    uint8_t message[128];
+    socklen_t receive_address_len;
+    uint8_t message[MESSAGE_BUFFER_SIZE];
     size_t message_len;
+    can_frame *frame;
 
     // broadcast all outgoing packets to the world (but our world is small)
     broadcast_address.sin_family = AF_INET;
@@ -104,17 +106,65 @@ int main_loop(void) {
     debug("socket number is %d\n", udp_socket());
     
     while (the_state.S == RUNNING) {
+        // zero out the buffer
+        memset(&message, 0, MESSAGE_BUFFER_SIZE);
+
         // receive a packet
-        uint8_t len = recv_can_message(udp_socket(), &receive_address, 
-                                       &message, &message_len);
+        int32_t bytes_received = 
+            canframe_recvfrom(udp_socket(), &message, MESSAGE_BUFFER_SIZE, 0, // no flags
+                              &receive_address, &receive_address_len);
 
         // decode the packet
-        if (len < 0) {
+        if (bytes_received < 0) {
             debug("CAN message receive failed\n");
             continue;
+        } else if (bytes_received == 0) {
+            debug("Received empty CAN message\n");
+            continue;
         }
-        // change our state based on the packet
-        // broadcast the new state
+
+        debug("received %i bytes\n", bytes_received);
+        
+        // attempt to decode the frame and see if it is for us
+        frame = (can_frame *) &message[0];
+        if (bytes_received <= sizeof(can_frame)) {
+            // extract data field
+            message_len = frame->can_dlc;
+            if (message_len != bytes_received - 5) {
+                debug("received CAN message with invalid length\n");
+                continue;
+            }
+            // check for CAN ID that matches ours
+            if (!is_relevant(frame->can_id)) {
+                debug("received CAN message with irrelevant ID %x\n", frame->can_id);
+                continue;
+            }
+        } else {
+            debug("received J1939 message, ignoring\n");
+        }
+
+        // change our state based on the received CAN frame
+
+        debug("processing CAN message with ID %x\n", frame->can_id);
+        bool position_updated = false;
+        switch (frame->can_id) {
+            case CAN_ID_CAR_X:
+            case CAN_ID_CAR_Y:
+            case CAN_ID_CAR_Z:
+                position_updated = update_position(frame);
+                break;
+            
+            case CAN_ID_BUTTON_PRESSED:
+                handle_button_press(frame);
+        }
+
+        // broadcast the new state; we always broadcast the current music state, 
+        // and we also broadcast any position state that has been updated
+
+        broadcast_music_state();
+        if (position_updated) {
+            broadcast_updated_position(frame->can_id);
+        }
     }
 
     debug("stop signal received, cleaning up\n");
@@ -123,6 +173,72 @@ int main_loop(void) {
     close(udp_socket());
 
     return 0;
+}
+
+bool is_relevant(canid_t can_id) {
+    bool result = false;
+
+    switch (can_id) {
+        case CAN_ID_BUTTON_PRESSED:
+        case CAN_ID_CAR_X:
+        case CAN_ID_CAR_Y:
+        case CAN_ID_CAR_Z:
+            result = true;
+    }
+
+    return result;
+}
+
+bool update_position(can_frame *frame) {
+    // make sure the frame is an appropriate type
+    if (frame->can_id != CAN_ID_CAR_X && frame->can_id != CAN_ID_CAR_Y &&
+        frame->can_id != CAN_ID_CAR_Z) {
+        error("attempt to update position from invalid frame, this should never happen");
+    }
+
+    // interpret the payload as a float
+    float *position;
+    float *old_position;
+    position = (float *) frame->data;
+    
+    bool changed = false;
+    const char *dimension = "";
+    switch (frame->can_id) {
+        case CAN_ID_CAR_X:
+            dimension = "x";
+            old_position = &the_state.pos_x;
+            break;
+        case CAN_ID_CAR_Y:
+            dimension = "y";
+            old_position = &the_state.pos_y;
+            break;
+        case CAN_ID_CAR_Z:
+            dimension = "z";
+            old_position = &the_state.pos_z;
+            break;
+    }
+
+    if (*old_position == *position) {
+        debug("%s-coordinate update (%f) results in no change\n", dimension, *position);
+    } else {
+        *old_position = *position;
+        changed = true;
+        debug("updated %s-coordinate to %f\n", dimension, *position);
+    }
+
+    return changed;
+}
+
+bool handle_button_press(can_frame *frame) {
+    return false;
+}
+
+void broadcast_music_state() {
+
+}
+
+void broadcast_updated_position(canid_t can_id) {
+
 }
 
 void stop(void) {
--- build/cyberphys/infotainment-server/source/infotainment_server.h
+++ build/cyberphys/infotainment-server/source/infotainment_server.h
@@ -13,6 +13,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "can.h"
+
+// the size of the message receive buffer
+#define MESSAGE_BUFFER_SIZE 128
+
 // initialize the server state
 bool initialize(void);
 
@@ -21,6 +26,23 @@ int udp_socket(void);
 
 // the main event loop
 int main_loop(void);
+
+// return true if the CAN ID is relevant to us, false otherwise
+bool is_relevant(canid_t can_id);
+
+// update our position based on the CAN frame; return true if the position
+// changed, false otherwise
+bool update_position(can_frame *frame);
+
+// update our state based on the CAN frame; return true if the state
+// changed, false otherwise
+bool handle_button_press(can_frame *frame);
+
+// broadcast the current music state
+void broadcast_music_state();
+
+// broadcast an updated position state for the dimension specified in the CAN ID
+void broadcast_updated_position(canid_t can_id);
 
 // stop the server (e.g., on a Ctrl-C)
 void stop(void);
