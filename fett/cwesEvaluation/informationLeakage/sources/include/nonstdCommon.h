8fc847fa05a9d68543766bc318881015faa5d580
Ramy Tadros <rtadros@galois.com>
2021-01-12 08:30:04 -0800

    use common nonstd files to reuse most of code

---
+++ fett/cwesEvaluation/informationLeakage/sources/include/nonstdCommon.h
@@ -0,0 +1,58 @@
+/* This is the common header for nonstandard tests */
+
+#include "testsParameters.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+#if (defined(testgenOnFreeRTOS) && defined(testgenQEMU))
+    #define MALLOC_BLOCK_SIZE_MAX 0x004F //Maximum allocatable size in FreeRTOS qemu
+    #define MALLOC_BLOCK_SIZE_MIN 0x0020 //A block of 32 bytes
+#else
+    #define MALLOC_BLOCK_SIZE_MAX 0x00FF 
+    #define MALLOC_BLOCK_SIZE_MIN 0x0064 //A block of 100 bytes
+#endif
+#define MAX_SECRET_VALUE 0xFE //Avoid all ones
+#define MIN_SECRET_VALUE 0x02 //Avoid zero and one
+#define N_MATCHES_TO_COUNT 4 //Have to find the byte value 4 times to avoid unfortunate coincidences
+
+#ifdef testgenOnFreeRTOS
+    #include "FreeRTOS.h"
+    #include "task.h"
+    #define EXIT puts(">>>End of Fett<<<\n"); vTaskDelete(NULL)   
+#else
+    #define EXIT exit(1)
+#endif
+
+#if defined(testgenOnFreeRTOS) && defined(testgenFPGA)
+    #define test_malloc pvPortMalloc
+    #define test_free   vPortFree
+#else
+    #define test_malloc malloc
+    #define test_free   free
+#endif
+    
+#ifdef DEBUG
+    #define DEBUG_PRINTF(...) printf(__VA_ARGS__)
+#else
+    #define DEBUG_PRINTF(...)
+#endif
+
+typedef struct secretLocInfo_ {
+    unsigned char secret;
+    unsigned char *pSecret;
+    size_t szSecret;
+} secretLocInfo_t;
+
+/* main declaration */
+#ifdef testgenOnFreeRTOS
+    void main_fett(void);
+#else
+    int main(void);
+#endif
+
+/* Functions declarations (defined in nonstdCommon.c) */
+secretLocInfo_t * mallocSecretInfo (void);
+unsigned char createSecret(void);
+void findSecret(secretLocInfo_t * secretInfo);
--- fett/cwesEvaluation/informationLeakage/sources/nonstandard/test_noClearMalloc.c
+++ fett/cwesEvaluation/informationLeakage/sources/nonstandard/test_noClearMalloc.c
@@ -1,55 +1,8 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include "testsParameters.h"
-
-#if (defined(testgenOnFreeRTOS) && defined(testgenQEMU))
-    #define MALLOC_BLOCK_SIZE_MAX 0x004F //Maximum allocatable size in FreeRTOS qemu
-    #define MALLOC_BLOCK_SIZE_MIN 0x0020 //A block of 32 bytes
-#else
-    #define MALLOC_BLOCK_SIZE_MAX 0x00FF 
-    #define MALLOC_BLOCK_SIZE_MIN 0x0064 //A block of 100 bytes
-#endif
-#define MAX_SECRET_VALUE 0xFE //Avoid all ones
-#define MIN_SECRET_VALUE 0x02 //Avoid zero and one
-#define N_MATCHES_TO_COUNT 4 //Have to find the byte value 4 times to avoid unfortunate coincidences
-
-#define TEST_FAILED 0x1
-#define TEST_CHECKED 0x2 
-
-#ifdef testgenOnFreeRTOS
-    #include "FreeRTOS.h"
-    #include "task.h"
-    #define EXIT puts(">>>End of Fett<<<\n"); vTaskDelete(NULL)   
-#else
-    #define EXIT exit(1)
-#endif
-
-#if defined(testgenOnFreeRTOS) && defined(testgenFPGA)
-    #define test_malloc pvPortMalloc
-    #define test_free   vPortFree
-#else
-    #define test_malloc malloc
-    #define test_free   free
-#endif
-    
-#ifdef DEBUG
-    #define DEBUG_PRINTF(...) printf(__VA_ARGS__)
-#else
-    #define DEBUG_PRINTF(...)
-#endif
-
-typedef struct secretLocInfo_ {
-    unsigned char secret;
-    unsigned char *pSecret;
-    size_t szSecret;
-} secretLocInfo_t;
+/* Test noClearMalloc */
+#include "nonstdCommon.h"
 
 static void noClearMallocTest(void);
-static unsigned char createSecret(void);
 static void storeSecret(secretLocInfo_t * secretInfo);
-static void findSecret(secretLocInfo_t * secretInfo);
 
 #ifdef testgenOnFreeRTOS
     void main() {
@@ -57,7 +10,6 @@ static void findSecret(secretLocInfo_t * secretInfo);
         return;
     }
 #else
-    int main(void);
     int main() {
         noClearMallocTest();
         return 0;
@@ -65,14 +17,7 @@ static void findSecret(secretLocInfo_t * secretInfo);
 #endif
 
 static void noClearMallocTest() {
-    secretLocInfo_t * secretInfo;
-
-    //allocate the secret info struct
-    secretInfo = (secretLocInfo_t*) test_malloc(sizeof(secretLocInfo_t));
-    if (secretInfo == NULL) {
-        puts("noClearMallocTest: Failed to malloc.\n TEST ERROR.\n");
-        EXIT;
-    }
+    secretLocInfo_t * secretInfo = mallocSecretInfo();
 
     //Create a random secret
     secretInfo->secret = createSecret();
@@ -85,11 +30,6 @@ static void noClearMallocTest() {
 
     test_free(secretInfo);
     return;
-}
-
-static unsigned char createSecret () {
-    srand(IEX_SEED); //IEX_SEED is written in fett/cwesEvaluation/build.py
-    return (unsigned char) ((rand() % (MAX_SECRET_VALUE-MIN_SECRET_VALUE+1) + MIN_SECRET_VALUE));
 }
 
 static void storeSecret(secretLocInfo_t * secretInfo) {
@@ -113,28 +53,6 @@ static void storeSecret(secretLocInfo_t * secretInfo) {
     secretInfo->szSecret = xSizeToMalloc;
     
     test_free(pSecret);
-
-    return;
-}
-
-static void findSecret(secretLocInfo_t * secretInfo) {
-    unsigned char *pCheck;
-    int matchCount = N_MATCHES_TO_COUNT;
-
-    pCheck = secretInfo->pSecret;
-    while ((pCheck <= (secretInfo->pSecret+secretInfo->szSecret)) && (matchCount>0)) {
-        DEBUG_PRINTF("findSecret: matchCount=%d: (*pCheck=<%x>)\n",matchCount,*pCheck);
-        if (*pCheck == secretInfo->secret) {
-            matchCount--;
-        }
-        pCheck++;
-    }
-
-    if (matchCount<=0) {
-        puts("TEST FAILED\n");
-    } else {
-        puts ("TEST PASSED\n");
-    }
 
     return;
 }
