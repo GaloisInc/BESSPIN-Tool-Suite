a9d9accea34e856944f9ed1fd385b68044baab3c
Ramy Tadros <rtadros@galois.com>
2021-01-15 13:36:34 -0800

    add optional programBitfile on watchdog reset

--- fett/target/launch.py
+++ fett/target/launch.py
@@ -313,7 +313,8 @@ def resetTarget (curTarget):
         if (isEqSetting('target','qemu',targetId=targetId)):
             qemu.configTapAdaptor(targetId=targetId)
         elif (isEqSetting('target','vcu118',targetId=targetId)):
-            vcu118.programBitfile(targetId=targetId)
+            if (isEnabled('programBitfileOnReset')):
+                vcu118.programBitfile(targetId=targetId)
             vcu118.resetEthAdaptor()
         else:
             logAndExit (f"<resetTarget> is not implemented for <{getSetting('target',targetId=targetId)}>."
