e9ce13e30eb99ca3b832275763b8d0aaa7b8281c
Michal Podhradsky <mpodhradsky@galois.com>
2021-01-23 14:45:33 -0800

    Current test commit

--- build/cyberphys/scripts/tmux-start.sh
+++ build/cyberphys/scripts/tmux-start.sh
@@ -6,9 +6,11 @@ tmux has-session -t $SESSIONNAME &> /dev/null
 if [ $? != 0 ] 
  then
     tmux new-session -s $SESSIONNAME -n script -d
-    tmux send-keys -t $SESSIONNAME "ip a" C-m 
+    tmux send-keys -t $SESSIONNAME "bash" C-m
+    tmux send-keys -t $SESSIONNAME "ip a" C-m
 
     tmux split-window -h -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "bash" C-m
     tmux send-keys -t $SESSIONNAME 'nix-shell' C-m
     tmux send-keys -t $SESSIONNAME './fett.py -d -c build/cyberphys/configs/config-cyberphys.ini' C-m
 fi
