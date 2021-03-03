b56ece168428927dda3f911e303ab4979bc3c2b9
rtadros <rtadros@galois.com>
2021-03-03 13:56:01 -0800

    use custom PPAC tests

--- fett/cwesEvaluation/PPAC/vulClassTester.py
+++ fett/cwesEvaluation/PPAC/vulClassTester.py
@@ -5,10 +5,25 @@ This file has the custom PPAC methods to for runTests
 from fett.cwesEvaluation.compat import testgenTargetCompatibilityLayer
 
 from fett.base.utils.misc import *
+from fett.cwesEvaluation.PPAC import cweTests
 
 class vulClassTester(testgenTargetCompatibilityLayer):
     def __init__ (self,target):
         super().__init__(target)
         self.vulClass = "PPAC"
         return
+
+    def executeTest (self,binTest):
+        outLog = ''
+        testName = binTest.split('.')[0]
+        
+        outLog += "\n" + '*'*30 + f" {testName.upper().replace('_',' ')} " + '*'*30 + "\n\n"
+
+        if (hasattr(cweTests,testName)):
+            outLog += getattr(getattr(cweTests,testName),testName)(self)
+        else:
+            self.terminateAndExit(f"Failed to find method <{testName}>.",exitCode=EXIT.Dev_Bug)
+        
+        return outLog
+
 
