159a2e31091428316452454cd3d71bcfce240a15
rtadros <rtadros@galois.com>
2021-03-03 13:55:24 -0800

    enhance openSsh for special testing

--- fett/target/common.py
+++ fett/target/common.py
@@ -1247,13 +1247,15 @@ class commonTarget():
     @decorate.debugWrap
     @decorate.timeWrap
     def openSshConn (self,userName='root',endsWith=None,timeout=60,specialTest=False):
-        def returnFail (message,exc=None):
+        def returnFail (message,exc=None,returnSpecial=False):
             self.killSshConn()
+            if (returnSpecial):
+                return 'BLOCKED_IP'
             warnAndLog (message,doPrint=False,exc=exc)
             extraMsg = ' Trying again...' if (self.sshRetries < self.sshLimitRetries-1) else ''
-            warnAndLog(f"openSshConn: Failed to open an SSH connection for <{userName}>.{extraMsg}")
+            warnAndLog(f"openSshConn: Failed to open an SSH connection for <{userName}>.{extraMsg}",doPrint=(not specialTest))
             self.sshRetries += 1
-            return self.openSshConn (userName=userName, timeout=timeout)
+            return self.openSshConn (userName=userName, endsWith=endsWith, timeout=timeout, specialTest=specialTest)
 
         if (self.osImage not in ['FreeBSD','debian']):
             self.terminateAndExit(f"<openSshConn> is not implemented for <{self.osImage}>.",exitCode=EXIT.Dev_Bug)
@@ -1293,15 +1295,10 @@ class commonTarget():
             retYes = self.runCommand("yes",endsWith=passwordPrompt+blockedIpResponse+[pexpect.EOF],
                         timeout=timeout,exitOnError=False,issueInterrupt=False)
             if (retYes[3] not in [0,1]): #No password prompt
-                if (specialTest and (retYes[3] in [2,3,4,5])):
-                    return 'BLOCKED_IP'
-                else:
-                    return returnFail(f"openSshConn: Unexpected outcome when responding <yes> to the ssh process.")
+                returnFail(f"openSshConn: Unexpected outcome when responding <yes> to the ssh process.",
+                    returnSpecial=(specialTest and (retYes[3] in [2,3,4,5])))
         elif (retExpect[2] in [2,3,4,6]): #the ip was blocked or connection refused
-            if specialTest:
-                return 'BLOCKED_IP'
-            else:
-                return returnFail(f"openSshConn: Unexpected response when spawning the ssh process.")
+            return returnFail(f"openSshConn: Unexpected response when spawning the ssh process.",returnSpecial=specialTest)
         retPassword = self.runCommand(sshPassword,endsWith=endsWith,timeout=timeout,
                         exitOnError=False,issueInterrupt=False)
         if (not retPassword[0]):
