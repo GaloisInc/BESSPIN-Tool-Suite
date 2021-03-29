6fff94de325b5addd5f7142425f5655db4eadb5b
rtadros <rtadros@galois.com>
2021-03-29 16:11:43 -0700

    all macros are capitalized

--- besspin/target/utils/defaultEnvUnix.mk
+++ besspin/target/utils/defaultEnvUnix.mk
@@ -5,37 +5,35 @@ ifeq ($(BIN_SOURCE),SRI_Cambridge)
 else
 	ARCH_ABI := -march=rv64imafdc -mabi=lp64d
 endif
-BESSPIN_DEFS += -DBESSPIN_UNIX -DtestgenOn$(OS_IMAGE)
-ifeq ($(TARGET),QEMU)
-	BESSPIN_DEFS += -DBESSPIN_QEMU
-else
-	BESSPIN_DEFS += -DBESSPIN_FPGA -Dtestgen$(TARGET)
+BESSPIN_DEFS += -DBESSPIN_UNIX -DBESSPIN_$(OS_IMAGE) -DBESSPIN_$(TARGET)
+ifeq ($(TARGET),$(filter $(TARGET),VCU118 AWSF1))
+	BESSPIN_DEFS += -DBESSPIN_FPGA 
 endif
 
 CFLAGS := $(ARCH_ABI) -Wall -O0 $(BESSPIN_DEFS)
 
 ifeq ($(BIN_SOURCE),SRI_Cambridge)
-	PREFIX_FreeBSD := riscv64-unknown-freebsd13.0
+	PREFIX_FREEBSD := riscv64-unknown-freebsd13.0
 else
-	PREFIX_FreeBSD := riscv64-unknown-freebsd12.1
+	PREFIX_FREEBSD := riscv64-unknown-freebsd12.1
 endif
 ifeq ($(BIN_SOURCE),LMCO)
 	#In the docker image, this is v8.3.0
-	PREFIX_Debian := riscv64-unknown-elf
+	PREFIX_DEBIAN := riscv64-unknown-elf
 else
-	PREFIX_Debian := riscv64-unknown-linux-gnu
+	PREFIX_DEBIAN := riscv64-unknown-linux-gnu
 endif
-CFLAGS_FreeBSD_CLANG := -Wno-error=sign-compare -mno-relax
+CFLAGS_FREEBSD_CLANG := -Wno-error=sign-compare -mno-relax
 CFLAGS += ${CFLAGS_$(OS_IMAGE)_$(COMPILER)} -DBIN_SOURCE_$(BIN_SOURCE)
 
 LDFLAGS := $(ARCH_ABI) ${LD_FLAGS_$(OS_IMAGE)}
 
 CC_GCC := ${PREFIX_$(OS_IMAGE)}-gcc
-SYSROOT_Debian = $(shell $(CC_GCC) -print-sysroot)
+SYSROOT_DEBIAN = $(shell $(CC_GCC) -print-sysroot)
 ifeq ($(BIN_SOURCE),SRI_Cambridge)
-	SYSROOT_FreeBSD := /opt/cheri/sdk/sysroot-$(SOURCE_VARIANT)
+	SYSROOT_FREEBSD := /opt/cheri/sdk/sysroot-$(SOURCE_VARIANT)
 else
-	SYSROOT_FreeBSD := $(BESSPIN_GFE_FREEBSD_SYSROOT)
+	SYSROOT_FREEBSD := $(BESSPIN_GFE_FREEBSD_SYSROOT)
 endif
 CLANG ?= clang
 SYSROOT ?= $(SYSROOT_$(OS_IMAGE))
@@ -46,8 +44,8 @@ LD_CLANG_GCC := $(LD_GCC_GCC)
 LD_CLANG_LLD := $(CC_CLANG)
 
 GCC_LIB_DIR = $(shell dirname $(shell $(CC_GCC) -print-libgcc-file-name))
-LDFLAGS_Debian_LLD = -L$(GCC_LIB_DIR) -B$(GCC_LIB_DIR)
-LDFLAGS_FreeBSD_LLD := -fuse-ld=lld
+LDFLAGS_DEBIAN_LLD = -L$(GCC_LIB_DIR) -B$(GCC_LIB_DIR)
+LDFLAGS_FREEBSD_LLD := -fuse-ld=lld
 
 LDFLAGS += ${LDFLAGS_$(OS_IMAGE)_$(LINKER)}
 
