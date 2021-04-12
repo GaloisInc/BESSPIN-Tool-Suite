fb4e99c7933c779155d84b32691fa98e46946278
rtadros <rtadros@galois.com>
2021-04-12 09:18:10 -0700

    add brief explanations to load and unload FreeRTOS disk images

--- utils/loadFreertosDiskImage.sh
+++ utils/loadFreertosDiskImage.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
+
+# Mounts `workDir/osImages/FreeRTOS.img` onto `/loopfs`
+
 sudo losetup -fP workDir/osImages/FreeRTOS.img
 sudo mkdir /loopfs
 sudo mount -o loop /dev/loop0 /loopfs
