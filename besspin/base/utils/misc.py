3556d8da0e698e5942ecb7afe72d6425a42e443d
Michal Podhradsky <mpodhradsky@galois.com>
2021-04-22 11:51:19 -0700

    Add try block around multicast IP calculation

--- besspin/base/utils/misc.py
+++ besspin/base/utils/misc.py
@@ -707,7 +707,10 @@ def computeMd5ForFile (filepath):
 
 @decorate.debugWrap
 def multicastFromHostIp(hostIp: str):
-    hostIp = hostIp.split('.')
-    hostIp[-1] = 255
-    multicastIp = '.'.join([str(octet) for octet in hostIp])
+    try:
+        hostIp = hostIp.split('.')
+        hostIp[-1] = 255
+        multicastIp = '.'.join([str(octet) for octet in hostIp])
+    except Exception as exc:
+        logAndExit(f"Failed to compute multicast IP from <{hostIp}>.", exc=exc, exitCode=EXIT.Unspecified)
     return multicastIp
