8f7b7715039b00369620dfcb13dc86d4fd220229
rtadros <rtadros@galois.com>
2021-04-22 13:39:50 -0700

    there will always be dirty files -- remove the assert

--- ci/besspin-ci.py
+++ ci/besspin-ci.py
@@ -227,9 +227,6 @@ def main(xArgs):
                 assert targetBranch == branches[0], "Failed branch check for Target."
             if not branches[1] == "None":
                 assert binariesBranch == branches[1], "Failed branch check for Binaries"
-                assert (
-                    binariesRepo.status() == {}
-                ), "Binaries Branch was not pulled successfully."
 
         except Exception as exc:
             # Prepare, upload to S3 and send SQS without TargetLogs, as nothing ran.
