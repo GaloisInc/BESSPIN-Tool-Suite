44a16c0bc450c5c6caa1aabf88d36d2537c71e7c
Steven Osborn <steven@lolsborn.com>
2021-05-29 20:53:47 -0700

    ability to switch backdrop images / states

--- besspin/cyberPhys/ui/can-display/src/App.vue
+++ besspin/cyberPhys/ui/can-display/src/App.vue
@@ -1,19 +1,13 @@
 <template>
-  <div id="app">
+  <div id="app" :class="[state]">
     <div id="can-legend">
       <table>
         <tr><th>CAN ID</th><th>Description</th></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
-        <tr><td>AA123453</td><td>Some Description of subsystem</td></tr>
+        <tr><td>AAF01000</td><td>Gear Selection</td></tr>
+        <tr><td>AAF01A00</td><td>Throttle Input</td></tr>
+        <tr><td>AAF01B00</td><td>Brake Input</td></tr>
+        <tr><td>AAF01D00</td><td>Steering Angle</td></tr>
+        <tr><td>AAFEAA00</td><td>Fuel remaining</td></tr>
       </table>
     </div>
     <Console/>
@@ -27,6 +21,16 @@ export default {
   name: 'App',
   components: {
     Console
+  },
+  data() {
+    return {
+      state: "normal"
+    }
+  },
+  methods: {
+    changeState(state) {
+      this.state = state;
+    }
   }
 }
 </script>
@@ -36,7 +40,8 @@ export default {
 html, body {
     background-color: #222;
     overflow: hidden;
-    background-image: url('/canBus_normal.png');
+    margin: 0;
+    padding: 0;
 }
 #ssith-status {
   width: 600px;
@@ -54,17 +59,19 @@ html, body {
   top: 380px;
   position: absolute;
 }
+#can-legend td {
+  width: 140px;
+  font-family: 'Roboto Mono', monospace;
+}
 #can-legend th {
   padding: 0;
   text-align: left;
-  border-bottom: 3px solid;
+  font-weight: bold;
 }
-#can-legend tr :first-child {
-  border-right: 1px solid;
-}
+
 #can-legend td, #can-legend th {
-    padding: 5px;
-}
+    padding: 2px;
+} 
 #can-legend table {
   border-collapse: collapse;
   border-spacing: 0;
@@ -78,6 +85,19 @@ h1 {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #ddd;
-  margin-top: 60px;
+  background-image: url('/canBus_normal.png');
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
 }
+#app.normal {
+  background-image: url('/canBus_normal.png');
+}
+#app.hacked {
+  background-image: url('/canBus_hacked.png');
+}
+#app.ssith {
+  background-image: url('/canBus_protected.png');
+}
+
 </style>
