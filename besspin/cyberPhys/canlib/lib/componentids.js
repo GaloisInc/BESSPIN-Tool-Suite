f33ed6641985d98b89bffc8f9f1761eb2af20905
Michal Podhradsky <mpodhradsky@galois.com>
2021-06-02 13:40:14 -0700

    Update Can and CMD settings and specs

--- besspin/cyberPhys/canlib/lib/componentids.js
+++ besspin/cyberPhys/canlib/lib/componentids.js
@@ -4,13 +4,13 @@
     * Name: lib/componentids.js
     * Author: Steven Osborn <steven@lolsborn.com>, Kristofer Dobelstein, Ethan Lew <elew@galois.com>
     * Michal Podhradsky <mpodhradsky@galois.com>
-    * Date: 28 May 2021
+    * Date: 02 June 2021
     * This file was created by BESSPIN-Tool-Suite/besspin/cyberPhys/canlib/make_can_spec.py
     */
 
  const SCENARIO_BASELINE = 0x11
  const SCENARIO_SECURE_ECU = 0x12
- const SCENARIO_SECURE_ECU = 0x13
+ const SCENARIO_SECURE_INFOtAINMENT = 0x13
  const BESSPIN_TOOL = 0x20
  const TARGET_1 = 0x21
  const TARGET_2 = 0x22
--- besspin/cyberPhys/cyberphyslib/cyberphyslib/canlib/canspecs.py
+++ besspin/cyberPhys/cyberphyslib/cyberphyslib/canlib/canspecs.py
@@ -3,7 +3,7 @@
     Name: ../cyberphyslib/cyberphyslib/canlib/canspecs.py
     Author: Steven Osborn <steven@lolsborn.com>, Kristofer Dobelstein, Ethan Lew <elew@galois.com>
     Michal Podhradsky <mpodhradsky@galois.com>
-    Date: 28 May 2021
+    Date: 02 June 2021
     This file was created by BESSPIN-Tool-Suite/besspin/cyberPhys/canlib/make_can_spec.py
     """
 
