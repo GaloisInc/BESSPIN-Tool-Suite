0647cc037c5cbd1d5cbbfed4291b2a4bc83149b8
Ethan Lew <elew@galois.com>
2021-06-09 15:44:48 -0700

    add state entry methods

--- besspin/cyberPhys/cyberphyslib/cyberphyslib/kiosk/director.py
+++ besspin/cyberPhys/cyberphyslib/cyberphyslib/kiosk/director.py
@@ -12,6 +12,12 @@ from transitions.extensions import GraphMachine as Machine
 from transitions import State
 
 
+def slide(f):
+    """decorator for slide action methods"""
+    print(f.__name__)
+    return f
+
+
 class KioskDirector:
     """
     Kiosk Director implements the desired state flow for the hacker kiosk experience
@@ -60,13 +66,14 @@ class KioskDirector:
         {'transition': ('slide17', 'slide20'), 'conditions': 'input_next'},
         {'transition': ('slide19', 'slide20'), 'conditions': 'input_next'},
         {'transition': ('slide20', 'slide21'), 'conditions': 'input_restart'},
-        {'transition': ('slide21', 'slide3')}
+        {'transition': ('slide21', 'slide3'), 'conditions': 'input_next'}
     ]
 
     def __init__(self):
         """kiosk state machine"""
         self.states = None
         self.transitions = None
+        self.inputs = None
         self.machine = self.prepare_state_machine()
 
     @property
@@ -74,14 +81,20 @@ class KioskDirector:
         """state machine termination condition"""
         return False
 
-    def run(self):
-        """start the state machine, and keep it transitioning until termination"""
-        try:
-            while not self.is_finished:
-                # advance the state machine
-                self.next_state()
-        except KeyboardInterrupt:
+    def default_inputs(self):
+        """set all button inputs to false"""
+        for inp in self.inputs:
+            setattr(self, f'{inp}', False)
+
+    def submit_button(self, button_name):
+        """activate a button input and send the kiosk to the next state"""
+        self.default_inputs()
+        if hasattr(self, f'input_{button_name}'):
+            setattr(self, f'input_{button_name}', True)
+        else:
+            # TODO raise warning
             pass
+        self.next_state()
 
     def prepare_state_machine(self):
         """expand state machine description and create pytransitions machine"""
@@ -94,14 +107,17 @@ class KioskDirector:
 
         # create transition objects from static transition description
         self.transitions  = []
+        self.inputs = set()
         for tn in self.transition_names:
             base_dict = {'trigger': 'next_state',
                          'source': get_full_name(tn['transition'][0]),
                          'dest': get_full_name(tn['transition'][1])}
             if 'conditions' in tn:
                 base_dict['conditions'] = tn['conditions']
+                self.inputs |= {tn['conditions']}
             if 'unless' in tn:
                 base_dict['unless'] = tn['unless']
+                self.inputs |= {tn['unless']}
             self.transitions.append(base_dict)
 
         return Machine(self, states=self.states, transitions=self.transitions, initial='slide2_kiosk_setup', show_conditions=True)
@@ -113,3 +129,79 @@ class KioskDirector:
         NOTE: you will need to install graphviz (with dot)
         """
         self.machine.get_graph().draw(fname, prog='dot')
+
+    @slide
+    def slide2_kiosk_setup_enter(self):
+        """timer choice selected"""
+        pass
+
+    @slide
+    def slide3_introduction_enter(self):
+        """introduction
+        1. send TX_CMD_ACTIVE_SCENARIO(BASELINE)
+        2. send TX_CMD_HACK_ACTIVE(0x0)
+        TODO: restart inactive components?
+        """
+        pass
+
+    @slide
+    def slide4_enter(self):
+        pass
+
+    @slide
+    def slide5_enter(self):
+        pass
+
+    @slide
+    def slide6a_hack_ota_server_enter(self):
+        """
+        1. hack OTA server
+        """
+        pass
+
+    @slide
+    def slide6b_hack_ota_server_enter(self):
+        """
+        1. hack
+        """
+        pass
+
+    @slide
+    def slide7_infotainment_hacks_enter(self):
+        pass
+
+    @slide
+    def slide9_enter(self):
+        pass
+
+    @slide
+    def slide10_hack_critical_systems_enter(self):
+        pass
+
+    @slide
+    def slide15_ssith_intro_enter(self):
+        pass
+
+    @slide
+    def slide16_secure_infotainment_enter(self):
+        pass
+
+    @slide
+    def slide17_enter(self):
+        pass
+
+    @slide
+    def slide18_secure_ecu_enter(self):
+        pass
+
+    @slide
+    def slide19_enter(self):
+        pass
+
+    @slide
+    def slide20_everything_is_hackable_enter(self):
+        pass
+
+    @slide
+    def slide21_ssith_is_the_solution_enter(self):
+        pass
