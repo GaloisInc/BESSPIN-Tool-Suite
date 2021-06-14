GaloisInc/BESSPIN-Tool-Suite

commit 808b0eef1825e8c6d2bcac071dd12f00e2b2d3e6

Various cyberphys fixes

besspin/target/build.py
@@ -353,7 +353,7 @@ def importImage(targetId=None):
     targetIdInfo = f'<target{targetId}>: ' if (targetId) else ''
     imagePaths = selectImagePaths(targetId=targetId)
     for ip in imagePaths:
-        cp (ip, getSetting('osImagesDir',targetId=targetId))
+        cp (ip, f"{getSetting('osImagesDir',targetId=targetId)}/{getSetting('osImage',targetId=targetId)}.elf")
     if (isEqSetting('target', 'vcu118', targetId=targetId)):
         # Fix the FreeBSD IP
         if (isEqSetting('osImage','FreeBSD',targetId=targetId) and isEqSetting('binarySource','GFE',targetId=targetId)):

besspin/cyberPhys/launch.py
@@ -94,9 +94,9 @@ def startCyberPhys():
             runThreadPerTarget(startTtyLogging)
 
         # Start relay manager
-        in_socks, _ = getComponentPorts("relayManager")
+        in_socks, out_socks = getComponentPorts("relayManager")
         components.append(
-            besspin.cyberPhys.relaymanager.RelayManager("relayManager", in_socks, []))
+            besspin.cyberPhys.relaymanager.RelayManager("relayManager", in_socks, out_socks))
         components.append(besspin.cyberPhys.commander.Commander())
 
         for c in components:

besspin/target/vcu118.py
@@ -842,8 +842,10 @@ def resetEthAdaptor ():
         #get the name and check configuration if this is the first time called
         if (not doesSettingExist('ethAdaptor')):
             ethAdaptor= getSetting('vcu118EthAdaptorName')
-            if (getAddrOfAdaptor(ethAdaptor,'MAC') != getSetting('vcu118EthAdaptorMacAddress')):
-                logAndExit(f"checkEthAdaptorConfiguration: <{ethAdaptor}> does not have the expected mac address <{getSetting('vcu118EthAdaptorMacAddress')}>. Please check the network configuration.",exitCode=EXIT.Network)
+            # Don't worry about MAC in cyberphys mode
+            if not (isEqSetting('mode','cyberPhys')):
+                if (getAddrOfAdaptor(ethAdaptor,'MAC') != getSetting('vcu118EthAdaptorMacAddress')):
+                    logAndExit(f"checkEthAdaptorConfiguration: <{ethAdaptor}> does not have the expected mac address <{getSetting('vcu118EthAdaptorMacAddress')}>. Please check the network configuration.",exitCode=EXIT.Network)
             #Set the adaptor's name
             setSetting('ethAdaptor',ethAdaptor)
             printAndLog (f"<{getSetting('ethAdaptor')}> exists and its MAC address is properly configured.",doPrint=False)

commit b8da7aea8c27c8099440f976598328cdc8c3ded8

fix the importImage bug caused by 808b0eef1825e8c6d2bcac071dd12f00e2b2d3e6

besspin/target/build.py
@@ -323,7 +323,12 @@ def prepareBusybox(targetId=None):
 @decorate.debugWrap
 def selectImagePaths(targetId=None):
     if isEnabled('useCustomOsImage',targetId=targetId):
-        return [getSetting('pathToCustomOsImage',targetId=targetId)]
+        # We need to make sure the name is right
+        tempPath = os.path.join(getSetting('workDir'),f'tmp{targetId}')
+        mkdir (tempPath,exitIfExists=False)
+        tempImagePath = os.path.join(tempPath,os.path.basename(getSetting('osImageElf',targetId=targetId)))
+        cp (getSetting('pathToCustomOsImage',targetId=targetId), tempImagePath)
+        return [tempImagePath]
     else:
         imageType = getSetting('target',targetId=targetId) if (getSetting('target',targetId=targetId)!='awsf1') else getSetting('pvAWS',targetId=targetId)
         if isEqSetting('binarySource','GFE',targetId=targetId):
@@ -353,7 +358,7 @@ def importImage(targetId=None):
     targetIdInfo = f'<target{targetId}>: ' if (targetId) else ''
     imagePaths = selectImagePaths(targetId=targetId)
     for ip in imagePaths:
-        cp (ip, f"{getSetting('osImagesDir',targetId=targetId)}/{getSetting('osImage',targetId=targetId)}.elf")
+        cp (ip, getSetting('osImagesDir',targetId=targetId))
     if (isEqSetting('target', 'vcu118', targetId=targetId)):
         # Fix the FreeBSD IP
         if (isEqSetting('osImage','FreeBSD',targetId=targetId) and isEqSetting('binarySource','GFE',targetId=targetId)):