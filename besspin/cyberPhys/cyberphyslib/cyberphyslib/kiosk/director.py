51efbc141df2d3b62460eb4379e20ff3aa6ed6be
Michal Podhradsky <mpodhradsky@galois.com>
2021-06-16 16:25:42 -0700

    Somplified state machine

--- besspin/cyberPhys/cyberphyslib/cyberphyslib/kiosk/director.py
+++ besspin/cyberPhys/cyberphyslib/cyberphyslib/kiosk/director.py
@@ -17,8 +17,8 @@ import threading
 import zmq
 
 
-def slide(f):
-    """decorator for slide action methods"""
+def page(f):
+    """decorator for page action methods"""
     def inner(*args, **kwargs):
         """TODO: make this log instead of print"""
         print(f.__name__)
@@ -26,86 +26,87 @@ def slide(f):
     return inner
 
 
-class KioskDirector:
+class HackerKiosk:
     """
     Kiosk Director implements the desired state flow for the hacker kiosk experience
 
     TODO: given the similar interfaces with IgnitionDirector, make a state machine base class?
     """
 
+    ZMQ_PORT = 5091
+    ZMQ_POLL_TIMEOUT = 0.1
+
     # full name of the states
-    state_names = ["slide2_kiosk_setup",
-                   "slide3_introduction",
-                   "slide4",
-                   "slide5",
-                   "slide6a_hack_ota_server",
-                   "slide6b_hack_ota_server",
-                   "slide7_infotainment_hacks",
-                   "slide9",
-                   "slide10_hack_critical_systems",
-                   "slide15_ssith_intro",
-                   "slide16_secure_infotainment",
-                   "slide17",
-                   "slide18_secure_ecu",
-                   "slide19",
-                   "slide20_everything_is_hackable",
-                   "slide21_ssith_is_the_solution"]
+    state_names = ["reset",
+                   "hack02_kiosk_intro",
+                   "hack05_info_attempt",
+                   "hack06_info_exploit",
+                   "hack06_info_exploit_attemp_hack",
+                   "hack08_critical_exploit",
+                   "hack09_protect",
+                   "hack10_protect_info_attempt",
+                   "hack10_info_exploit_attempt_hack",
+                   "hack12_protect_critical",
+                   "hack12_critical_exploit"]
 
     # this is a brief description of the state transitions that is expanded at runtime
     # into pytransitions transitions
     transition_names = [
-        {'transition': ('slide2', 'slide3'), 'conditions': 'input_timer'},
-        {'transition': ('slide3', 'slide4'), 'conditions': 'input_hack_car'},
-        {'transition': ('slide4', 'slide5'), 'conditions': 'input_exploit_ota'},
-        {'transition': ('slide5', 'slide6a'), 'conditions': 'input_hack_infotainment'},
-        {'transition': ('slide6a', 'slide6b'), 'conditions': 'input_infotainment_hacked'},
-        {'transition': ('slide6b', 'slide7'), 'conditions': 'input_next'},
-        {'transition': ('slide7', 'slide7'), 'conditions': 'input_hack'},
-        {'transition': ('slide7', 'slide9'), 'conditions': 'input_next', 'unless': 'input_hack'},
-        {'transition': ('slide9', 'slide10'), 'conditions': 'input_next'},
-        {'transition': ('slide10', 'slide10'), 'conditions': 'input_hack'},
-        {'transition': ('slide10', 'slide15'), 'conditions': 'input_next', 'unless': 'input_hack'},
-        {'transition': ('slide15', 'slide16'), 'conditions': 'input_hack_infotainment'},
-        {'transition': ('slide15', 'slide18'), 'conditions': 'input_hack_critical', 'unless': 'input_hack_infotainment'},
-        {'transition': ('slide16', 'slide16'), 'conditions': 'input_hack'},
-        {'transition': ('slide18', 'slide18'), 'conditions': 'input_hack'},
-        {'transition': ('slide16', 'slide17'), 'conditions': 'input_next', 'unless': 'input_hack'},
-        {'transition': ('slide18', 'slide19'), 'conditions': 'input_next', 'unless': 'input_hack'},
-        {'transition': ('slide17', 'slide20'), 'conditions': 'input_next'},
-        {'transition': ('slide19', 'slide20'), 'conditions': 'input_next'},
-        {'transition': ('slide20', 'slide21'), 'conditions': 'input_restart'},
-        {'transition': ('slide21', 'slide3'), 'conditions': 'input_next'}
+        {'transition': ('reset', 'hack02_kiosk_intro'), 'conditions': 'reset_complete'},
+        {'transition': ('hack02_kiosk_intro', 'hack05_info_attempt'), 'conditions': 'button_pressed_next'},
+        {'transition': ('hack05_info_attempt', 'hack06_info_exploit'), 'conditions': 'button_pressed_next'},
+        {'transition': ('hack06_info_exploit', 'hack06_info_exploit_attemp_hack'), 'conditions': 'button_pressed_info_exploit'},
+        {'transition': ('hack06_info_exploit_attemp_hack', 'hack06_info_exploit'), 'conditions': 'exploit_complete'},
+        {'transition': ('hack06_info_exploit', 'hack08_critical_exploit'), 'conditions': 'button_pressed_critical_exploit'},
+        {'transition': ('hack08_critical_exploit', 'hack06_info_exploit'), 'conditions': 'exploit_complete'},
+        {'transition': ('hack06_info_exploit', 'hack09_protect'), 'conditions': 'button_pressed_next'},
+        {'transition': ('hack09_protect', 'hack10_protect_info_attempt'), 'conditions': 'button_pressed_ssith_infotainment'},
+        {'transition': ('hack10_protect_info_attempt', 'hack10_info_exploit_attempt_hack'), 'conditions': 'button_pressed_info_exploit'},
+        {'transition': ('hack10_info_exploit_attempt_hack', 'hack10_protect_info_attempt'), 'conditions': 'input_next', 'unless': 'exploit_complete'},
+        {'transition': ('hack09_protect', 'hack12_protect_critical'), 'conditions': 'button_pressed_ssith_ecu'},
+        {'transition': ('hack12_protect_critical', 'hack12_critical_exploit'), 'conditions': 'button_pressed_critical_exploit'},
+        {'transition': ('hack12_critical_exploit', 'hack12_protect_critical'), 'conditions': 'exploit_complete'},
+        {'transition': ('hack02_kiosk_intro', 'reset'), 'conditions': 'button_pressed_reset'},
+        {'transition': ('hack05_info_attempt', 'reset'), 'conditions': 'button_pressed_reset'},
+        {'transition': ('hack06_info_exploit', 'reset'), 'conditions': 'button_pressed_reset'},
+        {'transition': ('hack09_protect', 'reset'), 'conditions': 'button_pressed_reset'},
+        {'transition': ('hack10_protect_info_attempt', 'reset'), 'conditions': 'button_pressed_reset'},
+        {'transition': ('hack12_protect_critical', 'reset'), 'conditions': 'button_pressed_reset'},
     ]
 
     @classmethod
     def for_besspin(cls, dnc):
         """argument free constructor"""
-        kiosk = kkiosk.HackerKiosk(dnc)
-        # TODO: FIXME: add this
-        #client = kclient.HackOtaClient()
-        client = None
-        return cls(client, kiosk)
+        return cls(None, None)
 
-    def __init__(self, ota_client: kclient.HackOtaClient, kiosk: kkiosk.HackerKiosk):
+    def __init__(self):
         """kiosk state machine"""
         self.states = None
         self.transitions = None
         self.inputs = None
         self.machine = self.prepare_state_machine()
-        self.client: kclient.HackOtaClient = ota_client
-        self.kiosk: kkiosk.HackerKiosk = kiosk
         self.state_arg = None
 
         self.stop_evt = threading.Event()
-        self.cmd_thread = threading.Thread(target=self.cmd_loop, args=[], daemon=True)
+
+        # ZMQ init
+        self.zmq_port = HackerKiosk.ZMQ_PORT
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind(f"tcp://*:{self.zmq_port}")
+
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
+
+        print(f"<{self.__class__.__name__}> Listening on ZMQ_PORT {self.zmq_port}")
 
     def run(self):
-        self.cmd_thread.start()
         while not self.stopped:
-            msgs = dict(self.kiosk.poller.poll(timeout=kkiosk.HackerKiosk.ZMQ_POLL_TIMEOUT))
-            if self.kiosk.socket in msgs and msgs[self.kiosk.socket] == zmq.POLLIN:
-                # recv there
-                self.kiosk.serve()
+            msgs = dict(self.poller.poll(HackerKiosk.ZMQ_POLL_TIMEOUT))
+            if self.socket in msgs and msgs[self.socket] == zmq.POLLIN:
+                req = self.socket.recv_json()
+                print(f"<{self.__class__.__name__}> Got request: {req}")
+
 
     def stop(self):
         self.stop_evt.set()
@@ -116,7 +117,6 @@ class KioskDirector:
 
     def exit(self):
         self.stop()
-        #self.join() # NOTE: not a thread
 
     def cmd_loop(self):
         while True:
@@ -150,10 +150,6 @@ class KioskDirector:
 
     def prepare_state_machine(self):
         """expand state machine description and create pytransitions machine"""
-        def get_full_name(n):
-            map = {s.split('_')[0]: s for s in self.state_names}
-            return map[n]
-
         # create state objects from state name
         self.states = [State(name=s, on_enter=f'{s}_enter') for s in self.state_names]
 
@@ -162,8 +158,8 @@ class KioskDirector:
         self.inputs = set()
         for tn in self.transition_names:
             base_dict = {'trigger': 'next_state',
-                         'source': get_full_name(tn['transition'][0]),
-                         'dest': get_full_name(tn['transition'][1]),
+                         'source': tn['transition'][0],
+                         'dest': tn['transition'][1],
                          'before': 'set_arg'}
             if 'conditions' in tn:
                 base_dict['conditions'] = tn['conditions']
@@ -173,7 +169,7 @@ class KioskDirector:
                 self.inputs |= {tn['unless']}
             self.transitions.append(base_dict)
 
-        return Machine(self, states=self.states, transitions=self.transitions, initial='slide2_kiosk_setup', show_conditions=True)
+        return Machine(self, states=self.states, transitions=self.transitions, initial='hack02_kiosk_intro', show_conditions=True)
 
 
     def draw_graph(self, fname: str):
@@ -183,125 +179,58 @@ class KioskDirector:
         """
         self.machine.get_graph().draw(fname, prog='dot')
 
-    def status_send(self, canid, argument):
-        pass
 
-    @slide
-    def slide2_kiosk_setup_enter(self, arg):
-        """timer choice selected"""
-        pass
-
-    @slide
-    def slide3_introduction_enter(self, arg):
-        """introduction
-        1. send TX_CMD_ACTIVE_SCENARIO(BASELINE)
-        2. send TX_CMD_HACK_ACTIVE(0x0)
-        TODO: restart inactive components?
-        """
-        self.status_send(canlib.CAN_ID_CMD_ACTIVE_SCENARIO, canlib.SCENARIO_BASELINE)
-        self.status_send(canlib.CAN_ID_CMD_HACK_ACTIVE, 0x0)
-        pass
-
-    @slide
-    def slide4_enter(self, arg):
+    @page
+    def reset_enter(self, arg):
         """nothing"""
         pass
 
-    @slide
-    def slide5_enter(self, arg):
+    @page
+    def hack02_kiosk_intro_enter(self, arg):
         """nothing"""
         pass
 
-    @slide
-    def slide6a_hack_ota_server_enter(self, arg):
-        """
-        1. hack OTA server
-        """
-        #success = self.client.hack_server()
-        # TODO: error handling if not successful?
-        self.kiosk.hack_ota()
-
-    @slide
-    def slide6b_hack_ota_server_enter(self, arg):
-        """
-        1. upload OTA payload file
-        """
-        # TODO: is this the correct call?
-        #success, _ = self.client.change_secret_key()
-        pass
-
-    @slide
-    def slide7_infotainment_hacks_enter(self, arg):
-        """infotainment server hacks are calls on the python backend (?)"""
-        pass
-
-    @slide
-    def slide9_enter(self, arg):
-        """
-        1. attempt to hack the critical systems?
-        """
-        # TODO: what goes here?
-        pass
-
-    @slide
-    def slide10_hack_critical_systems_enter(self, arg):
-        """
-        1. Send TX_CMD_HACK_ACTIVE(hack-number)
-        - select correct precompiled binary
-        - color button red / green based on selection
-        """
-        #self.status_send(canlib.CAN_ID_CMD_HACK_ACTIVE, "TODO")
-        # FIXME: TODO: how to get hack number
-        self.kiosk.switch_active_scenario("TODO?")
-
-    @slide
-    def slide15_ssith_intro_enter(self, arg):
-        """
-        1. restart baseline scenario components using CMD_RESTART
-        2. Send TX_CMD_ACTIVE_SECNARIO(SCENARIO_SECURE_ECU)
-        3. Send TX_CMD_HACK_ACTIVE(0x0)
-        """
-        #self.status_send(canlib.CAN_ID_CMD_ACTIVE_SCENARIO, canlib.SCENARIO_SECURE_ECU)
-        #self.status_send(canlib.CAN_ID_CMD_HACK_ACTIVE, 0x0)
-        self.kiosk.switch_active_scenario(canlib.SCENARIO_SECURE_ECU)
-        self.kiosk.send_hack_active_message(0x0)
-
-    @slide
-    def slide16_secure_infotainment_enter(self, arg):
-        """
-        1. Send TX_CMD_ACTIVE_SCENARIO(SCENARIO_SECURE_INFOTAINMENT)
-        - All hacks fail with an error message
-        - OTA server crashes on SSITH P2 when a hack is attempted
-        """
-        #self.status_send(canlib.CAN_ID_CMD_ACTIVE_SCENARIO, canlib.SCENARIO_SECURE_INFOTAINMENT)
-        self.kiosk.switch_active_scenario(canlib.SCENARIO_SECURE_INFOTAINMENT)
-
-    @slide
-    def slide17_enter(self, arg):
+    @page
+    def hack05_info_attempt_enter(self, arg):
         """nothing"""
         pass
 
-    @slide
-    def slide18_secure_ecu_enter(self, arg):
-        """
-        1. Send TX_CMD_ACTIVE_SCENARIO(SCENARIO_SECURE_ECU)
-        - appropriate precompiled binary is selected
-        - will be a short unavailability of the ECU (buttons will indicate that)
-        """
-        #self.status_send(canlib.CAN_ID_CMD_ACTIVE_SCENARIO, canlib.SCENARIO_SECURE_ECU)
-        self.kiosk.switch_active_scenario(canlib.SCENARIO_SECURE_ECU)
-
-    @slide
-    def slide19_enter(self, arg):
+    @page
+    def hack06_info_exploit_enter(self, arg):
         """nothing"""
         pass
 
-    @slide
-    def slide20_everything_is_hackable_enter(self, arg):
-        """nothign"""
+    @page
+    def hack06_info_exploit_attemp_hack_enter(self, arg):
+        """nothing"""
         pass
 
-    @slide
-    def slide21_ssith_is_the_solution_enter(self, arg):
+    @page
+    def hack08_critical_exploit_enter(self, arg):
+        """nothing"""
+        pass
+
+    @page
+    def hack09_protect_enter(self, arg):
+        """nothing"""
+        pass
+
+    @page
+    def hack10_protect_info_attempt_enter(self, arg):
+        """nothing"""
+        pass
+
+    @page
+    def hack10_info_exploit_attempt_hack_enter(self, arg):
+        """nothing"""
+        pass
+
+    @page
+    def hack12_protect_critical_enter(self, arg):
+        """nothing"""
+        pass
+
+    @page
+    def hack12_critical_exploit_enter(self, arg):
         """nothing"""
         pass
