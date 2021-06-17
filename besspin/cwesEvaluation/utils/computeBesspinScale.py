5b558d2c98353ca1ed52375d6044f1ec87e662ac
rtadros <rtadros@galois.com>
2021-06-17 08:14:34 -0700

    checking explicitly if score not in the dict is better

--- besspin/cwesEvaluation/utils/computeBesspinScale.py
+++ besspin/cwesEvaluation/utils/computeBesspinScale.py
@@ -9,8 +9,7 @@ normalizingScoresTable = {
     SCORES.MED : 1/3,
     SCORES.LOW : 2/3,
     SCORES.NONE : 1.0,
-    SCORES.DETECTED : 1.0,
-    SCORES.NOT_IMPLEMENTED : None #Intentionally a non-numerical value to cause errors if goes unnoticed
+    SCORES.DETECTED : 1.0
 }
 
 besspinCoeffsFactors = {
