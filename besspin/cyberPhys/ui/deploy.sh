c3aa5f8483029dc21dd6a61a0ee04deeea0feb1c
Steven Osborn <steven@lolsborn.com>
2021-06-21 09:33:36 -0700

    ui app deployment script

---
+++ besspin/cyberPhys/ui/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE=`dirname $0`/$1/dist_electron/$1-0.1.0-armv7l.AppImage
+
+if [ -z ${1+x} ]; then
+    echo "Usage: deploy.sh <app-name>"
+    exit
+fi
+
+if test -f "$FILE"; then
+    echo "Deploying $FILE" to pi@$1
+    scp $FILE pi@$1:/home/pi/deploy
+else
+    echo "File does not exist: $FILE"
+    echo "Did you run: npm run build:arm?"
+fi
+
