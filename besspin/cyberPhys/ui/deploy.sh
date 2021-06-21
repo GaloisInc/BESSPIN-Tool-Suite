036e728865c360d1d543f54917cf1da4df6b3897
Steven Osborn <steven@lolsborn.com>
2021-06-21 10:10:39 -0700

    deploy script fix

--- besspin/cyberPhys/ui/deploy.sh
+++ besspin/cyberPhys/ui/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILE=`dirname $0`/$1/dist_electron/$1-0.1.0-armv7l.AppImage
+DEST=/home/pi/deploy/$1-0.1.0-armv7l.AppImage
 
 if [ -z ${1+x} ]; then
     echo "Usage: deploy.sh <app-name>"
@@ -9,7 +10,7 @@ fi
 
 if test -f "$FILE"; then
     echo "Deploying $FILE" to pi@$1
-    scp $FILE pi@$1:/home/pi/deploy
+    scp $FILE pi@$1:$DEST
 else
     echo "File does not exist: $FILE"
     echo "Did you run: npm run build:arm?"
