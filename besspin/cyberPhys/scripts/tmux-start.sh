7300bca07cc907f05ec56e17d497668ad38e88b7
Michal Podhradsky <mpodhradsky@galois.com>
2021-08-05 13:35:25 -0700

    Update startup tmux script to monitor components from the Adim PC

--- besspin/cyberPhys/scripts/tmux-start.sh
+++ besspin/cyberPhys/scripts/tmux-start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 SESSIONNAME="cyberphys"
 tmux has-session -t $SESSIONNAME &> /dev/null
 
-if [ $? != 0 ] 
+if [ $? != 0 ]
  then
     tmux new-session -s $SESSIONNAME -n script -d
     tmux send-keys -t $SESSIONNAME "bash" C-m
@@ -16,4 +15,43 @@ if [ $? != 0 ]
     tmux send-keys -t $SESSIONNAME './besspin.py -d -c besspin/cyberPhys/configs/config-cyberphys.ini' C-m
 fi
 
-tmux attach -t $SESSIONNAME
+SESSIONNAME="hacker"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]
+ then
+    tmux new-session -s $SESSIONNAME -n script -d
+    tmux send-keys -t $SESSIONNAME "bash" C-m
+    tmux send-keys -t $SESSIONNAME "sleep 60" C-m
+    tmux send-keys -t $SESSIONNAME "ssh pi@hacker-kiosk" C-m
+    tmux send-keys -t $SESSIONNAME "sudo systemctl restart hacker-ui" C-m
+    tmux send-keys -t $SESSIONNAME "/home/pi/mouse-config.sh" C-m
+
+    tmux split-window -h -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "bash" C-m
+    tmux send-keys -t $SESSIONNAME "sleep 60" C-m
+    tmux send-keys -t $SESSIONNAME "ssh pi@hacker-kiosk" C-m
+    tmux send-keys -t $SESSIONNAME "sudo systemctl stop hacker-kiosk" C-m
+    tmux send-keys -t $SESSIONNAME "cd BESSPIN-Tool-Suite/besspin/cyberPhys/ui/hacker-kiosk/" C-m
+    tmux send-keys -t $SESSIONNAME "python3 kiosk-backend.py" C-m
+fi
+
+SESSIONNAME="display"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]
+ then
+    tmux new-session -s $SESSIONNAME -n script -d
+    tmux send-keys -t $SESSIONNAME "bash" C-m
+    tmux send-keys -t $SESSIONNAME "sleep 60" C-m
+    tmux send-keys -t $SESSIONNAME "ssh pi@can-display" C-m
+    tmux send-keys -t $SESSIONNAME "sudo systemctl restart can-ui" C-m
+
+    tmux split-window -h -t $SESSIONNAME
+    tmux send-keys -t $SESSIONNAME "bash" C-m
+    tmux send-keys -t $SESSIONNAME "sleep 60" C-m
+    tmux send-keys -t $SESSIONNAME "ssh pi@can-display" C-m
+    tmux send-keys -t $SESSIONNAME "sudo systemctl stop can-display" C-m
+    tmux send-keys -t $SESSIONNAME "cd BESSPIN-Tool-Suite/besspin/cyberPhys/ui/can-display/" C-m
+    tmux send-keys -t $SESSIONNAME "python3 can-display.py" C-m
+fi
