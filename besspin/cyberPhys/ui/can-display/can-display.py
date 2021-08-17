6f496471225bea21862b71eca135f6f2d1eeec96
Michal Podhradsky <mpodhradsky@galois.com>
2021-08-17 16:26:23 -0700

    Update can-display backend, implement hearbeat response

--- besspin/cyberPhys/ui/can-display/can-display.py
+++ besspin/cyberPhys/ui/can-display/can-display.py
@@ -20,7 +20,12 @@ import zmq
 from enum import Enum
 import threading
 
-import cyberphyslib.canlib as ccan
+from can import Message
+
+import cyberphyslib.canlib as canlib
+from cyberphyslib.demonstrator import config
+
+DEFAULT_NETWORK_CONFIG_PATH = "/home/pi/BESSPIN-Tool-Suite/besspin/base/utils/setupEnv.json"
 
 # make CAN module less noisy
 logging.getLogger("can").setLevel(logging.WARNING)
@@ -30,8 +35,6 @@ STATE_HACKED = "hacked"
 STATE_SSITH = "ssith"
 
 class CanDisplay(threading.Thread):
-    CMD_PORT = 5041
-    ZMQ_PORT = 5091
     ZMQ_POLL_TIMEOUT = 0.1
 
     def __init__(self):
@@ -40,16 +43,12 @@ class CanDisplay(threading.Thread):
         self.stop_evt = threading.Event()
 
         # CMD network init
-        self.cmd_port = CanDisplay.CMD_PORT
-        self.host = f"10.88.88.5:{self.cmd_port}" # Can display
-        self.nodes = [f"10.88.88.1:{self.cmd_port}",
-                      f"10.88.88.2:{self.cmd_port}",
-                      f"10.88.88.3:{self.cmd_port}",
-                      f"10.88.88.4:{self.cmd_port}"]
-        self.canbus = ccan.TcpBus(self.host, self.nodes)
+        net_conf = config.DemonstratorNetworkConfig.from_setup_env(DEFAULT_NETWORK_CONFIG_PATH)
+        cmd_host, cmd_subscribers = net_conf.getCmdNetworkNodes("CanDisplay")
+        self.cmd_bus = canlib.TcpBus(cmd_host, cmd_subscribers)
 
         # ZMQ init
-        self.zmq_port = CanDisplay.ZMQ_PORT
+        self.zmq_port = net_conf.port_network_ipcPort
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind(f"tcp://*:{self.zmq_port}")
@@ -57,10 +56,10 @@ class CanDisplay(threading.Thread):
         self.poller = zmq.Poller()
         self.poller.register(self.socket, zmq.POLLIN)
 
-        self.scenario = ccan.SCENARIO_BASELINE
+        self.scenario = canlib.SCENARIO_BASELINE
         self.state = STATE_NORMAL
 
-        print(f"<{self.__class__.__name__}> Listening on CAN_PORT {self.cmd_port}, and ZMQ_PORT {self.zmq_port}")
+        print(f"<{self.__class__.__name__}> Listening on CAN_PORT {net_conf.port_network_commandPort}, and ZMQ_PORT {self.zmq_port}")
 
         self.cmd_thread = threading.Thread(target=self.cmdLoop, args=[], daemon=True)
 
@@ -96,7 +95,7 @@ class CanDisplay(threading.Thread):
 
     def cmdLoop(self):
         while True:
-            msg = self.canbus.recv()
+            msg = self.cmd_bus.recv()
             if msg:
                 self.processCmdMsg(msg)
 
@@ -104,29 +103,38 @@ class CanDisplay(threading.Thread):
         """
         process CMD message
         """
-        cid, data = msg.arbitration_id, msg.data
-        data = int.from_bytes(data, byteorder='big', signed=False)
+        cid, bytedata = msg.arbitration_id, msg.data
+        data = int.from_bytes(bytedata, byteorder='big', signed=False)
 
         try:
-            if cid == ccan.CAN_ID_CMD_ACTIVE_SCENARIO:
+            if cid == canlib.CAN_ID_CMD_ACTIVE_SCENARIO:
                 print(f"<{self.__class__.__name__}> CAN_ID_CMD_ACTIVE_SCENARIO: {hex(data)}")
-                if data == ccan.SCENARIO_BASELINE or\
-                   data == ccan.SCENARIO_SECURE_ECU or\
-                   data == ccan.SCENARIO_SECURE_INFOTAINMENT:
+                if data == canlib.SCENARIO_BASELINE or\
+                   data == canlib.SCENARIO_SECURE_ECU or\
+                   data == canlib.SCENARIO_SECURE_INFOTAINMENT:
                    self.scenario = data
                 else:
                     print(f"<{self.__class__.__name__}> Unknown scenario ID: {hex(data)}")
-                if self.scenario == ccan.SCENARIO_BASELINE:
+                if self.scenario == canlib.SCENARIO_BASELINE:
                     self.state = STATE_NORMAL
                 else:
                     self.state = STATE_SSITH
-            elif cid == ccan.CAN_ID_CMD_HACK_ACTIVE:
+            elif cid == canlib.CAN_ID_CMD_HACK_ACTIVE:
                 print(f"<{self.__class__.__name__}> CAN_ID_CMD_HACK_ACTIVE: {hex(data)}")
-                if self.scenario == ccan.SCENARIO_BASELINE:
-                    if data == ccan.HACK_NONE:
+                if self.scenario == canlib.SCENARIO_BASELINE:
+                    if data == canlib.HACK_NONE:
                         self.state = STATE_NORMAL
                     else:
                         self.state = STATE_HACKED
+            elif cid == canlib.CAN_ID_HEARTBEAT_REQ:
+                print(f"<{self.__class__.__name__}> CAN_ID_HEARTBEAT_REQ: {hex(data)}")
+                # respond with a heartbeat response
+                # Component ID / sender IP address (uint32_t) | heartbeat request number (uint32_t)
+                heartbeat_ack = Message(arbitration_id=canlib.CAN_ID_HEARTBEAT_ACK,
+                            is_extended_id=True,
+                            data=list(canlib.HACKER_KIOSK.to_bytes(4, byteorder = 'big'), bytedata))
+                self.cmd_bus.send(heartbeat_ack)
+
             print(f"State: {self.scenario}")
         except Exception as exc:
             print(f"<{self.__class__.__name__}> Error processing message: {msg}: {exc}")
