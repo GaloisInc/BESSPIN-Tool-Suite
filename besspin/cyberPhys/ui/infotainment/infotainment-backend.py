6e105fea469b0a858188b13d783a6a36d8ff4bdd
Michal Podhradsky <mpodhradsky@galois.com>
2021-08-31 10:40:29 -0700

    De-noise heartbeat responses

--- besspin/cyberPhys/ui/infotainment/infotainment-backend.py
+++ besspin/cyberPhys/ui/infotainment/infotainment-backend.py
@@ -106,7 +106,6 @@ class InfotainmentBackend(threading.Thread):
         try:
             if cid == canlib.CAN_ID_HEARTBEAT_REQ:
                 req_number = struct.unpack(canlib.CAN_FORMAT_HEARTBEAT_REQ, msg.data)[0]
-                print(f"<{self.__class__.__name__}> CAN_ID_HEARTBEAT_REQ: {hex(req_number)}")
                 heartbeat_ack = Message(arbitration_id=canlib.CAN_ID_HEARTBEAT_ACK,
                                         dlc=canlib.CAN_DLC_HEARTBEAT_ACK,
                                         data=struct.pack(canlib.CAN_FORMAT_HEARTBEAT_ACK, canlib.INFOTAINMENT_THIN_CLIENT, req_number))
