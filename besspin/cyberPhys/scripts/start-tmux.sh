8d21454ba506de3c5c604bddcd66de4e995ec804
Michal Podhradsky <mpodhradsky@galois.com>
2021-09-20 15:51:00 -0700

    Remove non-source code files, they are moved into https://github.com/GaloisInc/BESSPIN-Demonstrator

--- besspin/cyberPhys/scripts/start-tmux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Move to /opt/ and schedule to run after reboot with `crontab -e`
-# Set to your BESSPIN-Tool-Suite dir, and proper tmux start script
-cd /home/galoisuser/BESSPIN-Tool-Suite
-/bin/bash ./besspin/cyberPhys/scripts/tmux-start-adminpc.sh
