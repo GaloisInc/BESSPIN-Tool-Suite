1dbed79c0c8345d6878be668628375fe3c1726f3
Michal Podhradsky <mpodhradsky@galois.com>
2021-09-21 10:28:22 -0700

    Simplify udp health monitor

--- besspin/cyberPhys/cyberphyslib/cyberphyslib/demonstrator/healthmonitor.py
+++ besspin/cyberPhys/cyberphyslib/cyberphyslib/demonstrator/healthmonitor.py
@@ -87,7 +87,8 @@ class PingMonitor(HealthMonitor):
         self.addr = addr
 
     def run_health_test(self) -> bool:
-        retCode = subprocess.call(["ping","-w","1",self.addr])
+        retCode = subprocess.call(["ping","-w","1",self.addr],
+                                  stdout=subprocess.DEVNULL)
         if retCode == 0:
             return True
         else:
@@ -144,124 +145,7 @@ class OtaMonitor(HttpMonitor):
 
 
 
-
-HeartbeatResponseType = typing.Tuple[float, float]
-
-class BusHeartbeatMonitor(HealthMonitor):
-    """
-    Heartbeat monitor is responsible for
-
-        1. sending out the heartbeat requests (REQ)
-        2. listening for heartbeat acknowledgements (ACK)
-        3. determining system health from ACK patterns
-    """
-    # length of response buffers (time horizon of ACKs)
-    maxlen = 5
-    wait_period = 10.0
-
-    @staticmethod
-    def form_heartbeat_req_msg(req_number: int):
-        """form REQ can packet"""
-        msg = can.Message(arbitration_id=canspecs.CAN_ID_HEARTBEAT_REQ,
-                          data=struct.pack(canspecs.CAN_FORMAT_HEARTBEAT_REQ, req_number))
-        return msg
-
-    def __init__(self, client_ids: typing.Sequence[int], bus: typing.Union[TcpBus, UdpBus]):
-        self.curr_req_number = 0
-        self.bus = bus
-        self.response_buffer: typing.Dict[int, typing.Deque[float]] = {
-            c: collections.deque(maxlen=self.maxlen) for c in client_ids
-        }
-        self.start_time = time.time()
-
-    def submit_response(self, client_id: int, response: HeartbeatResponseType):
-        """submit a response from client_id to the ACK buffer"""
-        if client_id not in self.response_buffer:
-            # TODO: FIXME: add fault location
-            idmap = {v: k for k, v in cids.__dict__.items() if isinstance(v, int)}
-            cname = idmap.get(client_id, "<UNKNOWN>")
-            health_logger.warning(f"<{self.__class__.__name__}> Received unanticipated response {client_id} ({cname})")
-        else:
-            if response is not None:
-                self.response_buffer[client_id].append(time.time())
-
-    def send_heartbeat_req_msg(self, req_number: int):
-        """set REQ can packet over the TCP bus"""
-        m = self.form_heartbeat_req_msg(req_number)
-        self.bus.send_msg(m)
-
-    def send_req_nodes(self):
-        """send REQ to nodes"""
-        self.send_heartbeat_req_msg(self.curr_req_number)
-        self.curr_req_number += 1
-
-    def run_health_test(self) -> bool:
-        """implement the heartbeat health:
-            for a given history of component with component id cid, if the maximum value is
-            within MAXLEN of the current REQ number, the component is considered healthy
-        """
-        return all(self.run_health_tests())
-
-    def run_health_tests(self) -> typing.Dict[typing.Hashable, bool]:
-        """return health result for each component entered in the response buffer"""
-        outcome = {k: True for k in self.response_buffer.keys()}
-        for cid, buff in self.response_buffer.items():
-            if len(buff) > 0:
-                ltime = max(buff)
-                delta = time.time() - ltime
-                if delta > self.maxlen:
-                    health_logger.debug(f"ERROR: Component with ID {cid} Has Failed Heartbeat Health Test!")
-                    outcome[cid] = False
-            else:
-                if time.time() - self.start_time > self.wait_period:
-                    health_logger.debug(f"ERROR: Component with ID {cid} Has Failed Heartbeat Health Test (No Submissions)!")
-                    outcome[cid] = False
-        return outcome
-
-class HeartbeatTaskComponent(cycomp.ComponentPoller):
-    """
-    HeartbeatTaskComponent extends ComponentPoller to have the desired functionality of a heartbeat
-    element, hiding implementation from other tasks that the component is responsible for (polling
-    task + message handling)
-
-    Examples:
-        ```
-        myc = heartbeat_component("myc", {("msg-in", 5006)}, {"msg-out", 5007})
-        myc.register_heartbeat_bus(bus_obj, [0x45])
-        myc.start()
-        ```
-    """
-    def __init__(self, *args, heartbeat_sample_period=1.0, **kwargs):
-        super(HeartbeatTaskComponent, self).__init__(*args, **kwargs)
-        # poller properties
-        # NOTE: should sample period be larger?
-        self.heartbeat_thread = cycomp.ThreadExiting(name=f"{self.name}-heartbeat",
-                                                     sample_frequency=1 / heartbeat_sample_period)
-        self.heartbeat_thread.on_poll = self.on_heartbeat_poll
-        self.heartbeat_thread.on_start = self.on_heartbeat_start
-        self.heartbeat_thread.on_exit = self.on_heartbeat_exit
-        self._heartbeat_call_started = False
-
-    def on_start(self):
-        if not self._heartbeat_call_started:
-            self.heartbeat_thread.start()
-        super().on_start()
-
-    def on_exit(self):
-        super().exit()
-        if self._heartbeat_call_started:
-            self.heartbeat_thread.exit()
-
-    def on_heartbeat_start(self):
-        pass
-
-    def on_heartbeat_exit(self):
-        pass
-
-    def on_heartbeat_poll(self, t):
-        pass
-
-class HeartbeatMonitorComponent(HeartbeatTaskComponent):
+class HeartbeatMonitorComponent(cycomp.ComponentPoller):
     """
     Heartbeat Monitor Component (component to check the health of the clients)
 
@@ -275,29 +159,28 @@ class HeartbeatMonitorComponent(HeartbeatTaskComponent):
         myc.start()
         ```
     """
-    def __init__(self, *args, **kwargs):
-        super(HeartbeatMonitorComponent, self).__init__(*args, **kwargs)
-        self._heartbeat_monitor_udp: typing.Optional[BusHeartbeatMonitor] = None
+    def __init__(self, bus: UdpBus):
+        super().__init__("hbmonitor", [], [], sample_frequency=1.0)
+        self.udp_bus = bus
+        self.req_number = 0
 
-    def register_can_heartbeat_bus(self, bus: UdpBus, client_ids):
-        self._heartbeat_monitor_udp: BusHeartbeatMonitor = BusHeartbeatMonitor(client_ids, bus)
+    @staticmethod
+    def form_heartbeat_req_msg(req_number: int):
+        """form REQ can packet"""
+        msg = can.Message(arbitration_id=canspecs.CAN_ID_HEARTBEAT_REQ,
+                          data=struct.pack(canspecs.CAN_FORMAT_HEARTBEAT_REQ, req_number))
+        return msg
 
-    def on_heartbeat_poll(self, t):
-        if self._heartbeat_monitor_udp is not None:
-            hm: BusHeartbeatMonitor = self._heartbeat_monitor_udp
-            hm.send_req_nodes()
-
-    def run_health_test(self) -> bool:
-        udp_result = True
-        if self._heartbeat_monitor_udp is not None:
-            hm: BusHeartbeatMonitor = self._heartbeat_monitor_udp
-            udp_result = hm.run_health_test()
-        return udp_result
+    def on_poll_poll(self, t):
+        # send heartbeat request
+        self.req_number += 1
+        m = self.form_heartbeat_req_msg(self.req_number)
+        self.udp_bus.send_msg(m)
 
     @property
     def is_healthy(self) -> bool:
         """health property"""
-        return self.run_health_test()
+        return False
 
     @property
     def is_unhealthy(self) -> bool:
@@ -307,23 +190,7 @@ class HeartbeatMonitorComponent(HeartbeatTaskComponent):
     def _(self, data):
         """UDP Acknowledge: submit the ack to the monitor"""
         cid, rnum = data
-        if self._heartbeat_monitor_udp is not None:
-            self._heartbeat_monitor_udp.submit_response(cid, (cid, rnum))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print(f"Got response from component {cid} rnum {rnum}")
 
 class HeartbeatMonitor(threading.Thread):
     FREQUENCY = 0.1
@@ -364,7 +231,7 @@ class HeartbeatMonitor(threading.Thread):
             cids.DEBIAN_3 : "10.88.88.31",
         }
 
-        self.component_monitor = HeartbeatMonitorComponent("can_monitor", set(), set())
+        self.component_monitor = None
         self.ping_monitors  = {k: PingMonitor(addr) for k, addr in self.pings.items()}
         self.ota_monitors = {k: OtaMonitor(addr) for k, addr in self.https.items()}
         self.service_monitors = {k: ServiceMonitor(params["service_name"], params["address"],
@@ -374,8 +241,7 @@ class HeartbeatMonitor(threading.Thread):
     
     def start_monitor(self, can_bus):
         """monitor initialization"""
-        can_bus.register(self.component_monitor)
-        self.component_monitor.register_can_heartbeat_bus(can_bus.bus, self.udp_descr.values())
+        self.component_monitor = HeartbeatMonitorComponent(can_bus)
         self.component_monitor.start()
 
     def run(self):
@@ -401,17 +267,6 @@ class HeartbeatMonitor(threading.Thread):
                 ret[k] = hs
                 if not hs:
                     health_logger.debug(f"WARNING! {k} Ping failed health check")
-            
-            if self.component_monitor is not None:
-                # TODO: this monitor access is ugly
-                if self.component_monitor._heartbeat_monitor_udp is not None:
-                    health_logger.debug("Testing UDP")
-                    health_report = self.component_monitor._heartbeat_monitor_udp.run_health_tests()
-                    kmap = {v: k for k,v in self.udp_descr.items()}
-                    ret.update({kmap[k]: v for k, v in health_report.items()})
-                    if not all(health_report.values()):
-                        health_logger.debug(f"Health Status: {health_report}")
-                        health_logger.debug(f"ERROR! UDP Failed")
 
             self._health_report = ret
 
